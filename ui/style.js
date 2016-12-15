7ebc3a28d0d4b5fb438ec88caedafaf6ea6ce38c
M.Halit Karakis <halit.karakis@smartface.io>
2016-12-15 12:29:44 +0300

    Add style class

---
+++ ui/style.js
@@ -0,0 +1,35 @@
+/**
+ * @class Style
+ * 
+ * This class wraps common UI styling options. Instances of Style
+ * class can be set to style property of any UI object. For same
+ * style object different UI objects can behave different. 
+ * 
+ *      @example
+ *      var label = new Label({
+ *          width: "80%",
+ *          height: "10%"
+ *      });
+ *      label.style = new Style({
+ *          borderColor: "#000000",
+ *          borderWidth: 2
+ *      });
+ */
+function Style(params) {
+    /**
+     * Sets/gets border color of bounded view.
+     * 
+     * @member {Color} borderColor Border color
+     */
+    this.borderColor = "#000000";
+
+    /**
+     * Sets/gets border thickness of bounded view. Accepts unsigned
+     * numbers, 0 means no border.
+     * 
+     * @member {Number} borderWidth Border width
+     */
+    this.borderWidth = 0;
+}
+
+module.exports = Style;
