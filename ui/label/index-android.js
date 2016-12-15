80efe2a01e75d70b60bbcb0180b96391df415d5d
M.Halit Karakis <halit.karakis@smartface.io>
2016-12-15 14:21:27 +0300

    Remove comments from implementations file

--- ui/label/index-android.js
+++ ui/label/index-android.js
@@ -1,34 +1,7 @@
-/**
- * @class Label
- *
- *
- * Label is a UI object to display a text on the screen. Label can contain only a single type font.
- *
- * @example
- * const Label = require('sf-core/ui/label');
- * var myLabel = new Label({
- *     text: "This is my label",
- *     visible: true
- * });
- * myLabel.backgroundColor = "#00FFFFFF";
- */
 function Label (options) {
 
     const TextView = android.widget.TextView || require("core-modules/android-mock/android.widget.TextView");
     this.inner = new TextView("Landroid/app/Activity"); 
-
-    /**
-     * Gets/sets HTML text value. This property helps user showing HTML
-     * tagged texts in Label view.
-     * 
-     * @example
-     * // In this example 'This link' text inside Label will shown blue and
-     * // underlined
-     * var label = new Label();
-     * label.htmlText = "<a href='http://smartface.io'>This link</a> will redirect you to Smartface website.";
-     * 
-     * @member {string} htmlText HTML text to display in object
-     */
 
     Object.defineProperty(this, 'htmlText', {
         get: function() {
@@ -39,12 +12,6 @@ function Label (options) {
         }
      });
 
-    /**
-     * Gets/sets font of label view. When set to null label uses system font.
-     * It is set to null by default.
-     * 
-     * @member {Font} font Font of label view.
-     */
      Object.defineProperty(this, 'font', {
         get: function() {
             return this.inner.getTypeface();
@@ -54,13 +21,6 @@ function Label (options) {
         }
      });
 
-    /**
-     * Gets/sets allowing multiple line for label view. If set to true
-     * and text is too long to show in single line label shows text as 
-     * multiline. 
-     * 
-     * @member {boolean} multipleLine Show multiple line in label 
-     */
     Object.defineProperty(this, 'multipleLine', {
         get: function() {
             return this.inner.getLineCount() != 1;
@@ -70,18 +30,8 @@ function Label (options) {
         }
      });
 
-    /**
-     * Gets/sets style of view. 
-     * 
-     * @member {Style} style Style of view
-     */
     this.style = {};
 
-    /**
-     * Gets/sets text inside label view.
-     * 
-     * @member {string} text Text inside label
-     */
      Object.defineProperty(this, 'text', {
         get: function() {
             return this.inner.getText();
@@ -91,16 +41,6 @@ function Label (options) {
         }
      });
 
-    /**
-     * Gets/sets text alignment of label view. UI.TextAlignment constants
-     * can be used.
-     * 
-     * @example
-     * var label = new Label();
-     * label.textAlignment = TextAlignment.CENTER;
-     * 
-     * @member {number} textAlignment Text alignment
-     */
     Object.defineProperty(this, 'textAlignment', {
         get: function() {
             return this.inner.getTextAlignment();
@@ -109,21 +49,16 @@ function Label (options) {
             this.inner.setTextAlignment(textAlignment);
         }
      });
-    /**
-     * Gets/sets text color of view.
-     * 
-     * @member {Color} textColor Text Color
-     */
+
     Object.defineProperty(this, 'color', {
         get: function() {
             return this.inner.getTextColor();
         },
         set: function(color) {
-            this.inner.setTextColor(Color.parseColor(color);
+            this.inner.setTextColor(Color.parseColor(color));
         }
      });
     
-    // Assign properties given in constructor
     if (props) {
         for (var prop in props) {
             this[prop] = props[prop];
