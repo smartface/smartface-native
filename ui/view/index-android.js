55d3a03d8652ad07a6ba8e9bbb78479cc313e2c5
Metehan Toksoy <metehan.toksoy@smartface.com>
2016-12-15 16:49:03 +0200

    implement android view module#2

--- ui/view/index-android.js
+++ ui/view/index-android.js
@@ -6,13 +6,10 @@
  *
  */
 function View(params) {
-    /** 
-     * Defines opacity of view. The value of this property is float number
-     * between 0.0 and 1.0. 0 represents view is completely transparent and 1 
-     * represents view is completely opaque.
-     *
-     * @member {number} alpha Alpha value of view
-     */
+
+    const View = android.widget.View || require("core-modules/android-mock/android.view.View");
+    this.inner = new View("Landroid/app/Activity"); 
+    
     Object.defineProperty(this, 'alpha', {
         get: function() {
             return this.inner.getAlpha();
@@ -22,12 +19,6 @@ function View(params) {
         }
      });
     
-    /**
-     * Gets/sets background color of view. It allows setting background 
-     * color with string or UI.Color properties.
-     * 
-     * @member {Color} backgroundColor Background color
-     */ 
     Object.defineProperty(this, 'backgroundColor', {
         get: function() {
             return this.inner.getBackgroundColor();
@@ -37,34 +28,22 @@ function View(params) {
         }
      });
 
-    /**
-     * Gets/sets height of view. Setting number (as pixel) and string 
-     * (as percentage) is allowed.
-     * 
-     * @property {number} height Height of view
-     */
-    this.height = "0%";
     Object.defineProperty(this, 'height', {
         get: function() {
-            return (this.inner.getHeight() != 0 ? this.inner.getHeight() : this.height);
+            return this.inner.getHeight();
         },
         set: function(height) {
-            this.height = height;
             if(this.inner.getLayoutParams != null){
                 // Needs global layout param setter. Layout params can be number or %
                 // If we can work just pixels except LinearLayout this will enought for setting.
                 // this.inner.getLayoutParams().height
             }
+            else{
+                this.heightInitial = height;
+            }
         }
      });
         
-    /**
-     * Gets/sets id of view. Should be unique number for all objects
-     * inside project.
-     * 
-     * @member {number} id View identifier
-     */
-    this.id = android.view.View.generateViewId()
     Object.defineProperty(this, 'id', {
         get: function() {
             return this.inner.getId();
@@ -73,52 +52,31 @@ function View(params) {
             this.inner.setId(id);
         }
      });
-     
-
-    /**
-     * Gets/sets position X value of view. Setting number (as pixel) and string 
-     * (as percentage) is allowed.
-     * 
-     * @property {number} left Position X value of view
-     */
-    this.left = "0%";
+    
     Object.defineProperty(this, 'left', {
         get: function() {
-            return (this.inner.getLeft() != 0 ? this.inner.getLeft() : this.left);
+            return this.inner.getLeft();
         },
         set: function(left) {
-            this.left = left;
+            this.leftInitial = left;
             if(this.inner.getLayoutParams != null){
                 // Needs global layout param setter. Layout params can be number or %
             }
         }
      });
 
-    /**
-     * Gets/sets position Y value of view. Setting number (as pixel) and string 
-     * (as percentage) is allowed.
-     * 
-     * @property {number} top Position Y value of view
-     */
-    this.top = "0%";
     Object.defineProperty(this, 'top', {
         get: function() {
-            return (this.inner.getTop() != 0 ? this.inner.getTop() : this.top);
+            return this.inner.getTop();
         },
         set: function(top) {
-            this.top = top;
+            this.topInitial = top;
             if(this.inner.getLayoutParams != null){
                 // Needs global layout param setter. Layout params can be number or %
             }
         }
      });
 
-    /**
-     * Gets/sets visibility of view. It is set to true as default.
-     * 
-     * @member {boolean} visible View visibility
-     */
-    this.visible = true;
     Object.defineProperty(this, 'visible', {
         get: function() {
             return this.inner.getVisibility() == android.view.View.VISIBLE;
@@ -131,12 +89,6 @@ function View(params) {
         }
     });
 
-    /**
-     * Enables/disables touches to view. When set to false events
-     * related to touches won't fire. It is set to true as default.
-     * 
-     * @member {boolean} touchEnabled Touch enable
-     */
     Object.defineProperty(this, 'touchEnabled', {
         get: function() {
             return this.inner.getEnabled();
@@ -146,69 +98,60 @@ function View(params) {
         }
      });
 
-
-    /**
-     * Gets/sets width of view. Setting number (as pixel) and string 
-     * (as percentage) is allowed.
-     * 
-     * @property {number} width Width of view
-     */
-    this.width = "0%";
     Object.defineProperty(this, 'width', {
         get: function() {
-            return (this.inner.getLayoutParams().width != 0 ? this.inner.getHeight() : this.width);
+            return this.inner.getWidth();
         },
         set: function(width) {
-            this.width = width;
-            if(this.inner.width != null){
+            if(this.inner.getLayoutParams != null){
                 // Needs global layout param setter. Layout params can be number or %
                 // If we can work just pixels except LinearLayout this will enought for setting.
                 // this.inner.getLayoutParams().width
             }
             else{
-                this.width = width;
+                this.widthInitial = width;
             }
         }
      });
 
-    /**
-     * This method returns all position values in one object.
-     * 
-     * @return {Object} Object with properties:
-     * @return {Number} return.width Width value
-     * @return {Number} return.height Height value
-     * @return {Number} return.left Position X value
-     * @return {Number} return.top Position Y value
-     */
-    this.getPosition = function(){return  {width: 3, height: 5, top: 7, left: 9}; }
+    this.getPosition = function(){
+        return  {
+            width: this.inner.getWidth(), 
+            height: this.inner.getHeight(), 
+            top: this.inner.getTop(), 
+            left: this.inner.getLeft()
+        }; 
+    }
 
-    /**
-     * This method allows setting all position values within one function call.
-     * Using this method will be faster than setting all position values (width,
-     * height etc.) separately.
-     * 
-     * @param {Object} position Object describing position values
-     * @param {Number} [position.width] Width value
-     * @param {Number} [position.height] Height value
-     * @param {Number} [position.left] Position X value
-     * @param {Number} [position.top] Position Y value
-     */
-    this.setPosition = function(position){}
+    this.setPosition = function(position){
+        if(this.inner.getLayoutParams != null){
+            / Needs global layout param setter. Layout params can be number or %
+        }
+        else{
+            widthInitial = position.width;
+            heightInitial = height;
+            topInitial = top;
+            leftInitial = left;
+        }
+    }
 
-    /**
-     * Gets/sets touch event for view. This event fires when touch started.
-     * 
-     * @event {function} onTouch Touch event
-     */
-    this.onTouch = function(){ }
+    Object.defineProperty(this, 'onTouch', {
+        get: function() {
+            return this.onTouchCallback;
+        },
+        set: function(top) {
+            this.onTouchCallback = onTouch;
+        }
+     });
 
-    /**
-     * Gets/sets touch ended event for view. This event fires when touch
-     * finished.
-     * 
-     * @event {function} onTouchEnded Touch ended event
-     */
-    this.onTouchEnded = function(){ }
+    Object.defineProperty(this, 'onTouchEnded', {
+        get: function() {
+            return this.onTouchEndedCallback;
+        },
+        set: function(top) {
+            this.onTouchEndedCallback = onTouch;
+        }
+     });
 }
 
 module.exports = View;
