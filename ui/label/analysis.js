3a34cb4bf74479ae66943d8d94474b24e035f36e
M.Halit Karakis <halit.karakis@smartface.io>
2016-12-15 12:03:11 +0300

    Extend Label from View

--- ui/label/analysis.js
+++ ui/label/analysis.js
@@ -1,3 +1,5 @@
+const View = require('../view');
+const extend = require('js-base/core/extend');
 /**
  * @class Label
  *
@@ -12,84 +14,88 @@
  * });
  * myLabel.backgroundColor = "#00FFFFFF";
  */
-function Label (options) {
-    /**
-     * Gets/sets HTML text value. This property helps user showing HTML
-     * tagged texts in Label view.
-     * 
-     * @example
-     * // In this example 'This link' text inside Label will shown blue and
-     * // underlined
-     * var label = new Label();
-     * label.htmlText = "<a href='http://smartface.io'>This link</a> will redirect you to Smartface website.";
-     * 
-     * @member {string} htmlText HTML text to display in object
-     */
-    this.htmlText = "";
+const Label = extend(View)(
+    function (_super, params) {
+        _super(this);
 
-    /**
-     * Gets/sets font of label view. When set to null label uses system font.
-     * It is set to null by default.
-     * 
-     * @member {Font} font Font of label view.
-     */
-    this.font = null;
+        /**
+         * Gets/sets HTML text value. This property helps user showing HTML
+         * tagged texts in Label view.
+         * 
+         * @example
+         * // In this example 'This link' text inside Label will shown blue and
+         * // underlined
+         * var label = new Label();
+         * label.htmlText = "<a href='http://smartface.io'>This link</a> will redirect you to Smartface website.";
+         * 
+         * @member {string} htmlText HTML text to display in object
+         */
+        this.htmlText = "";
 
-    /**
-     * Gets/sets allowing multiple line for label view. If set to true
-     * and text is too long to show in single line label shows text as 
-     * multiline. 
-     * 
-     * @member {boolean} multipleLine Show multiple line in label 
-     */
-    this.multipleLine = true;
+        /**
+         * Gets/sets font of label view. When set to null label uses system font.
+         * It is set to null by default.
+         * 
+         * @member {Font} font Font of label view.
+         */
+        this.font = null;
 
-    /**
-     * Gets/sets style of view. 
-     * 
-     * @member {Style} style Style of view
-     */
-    this.style = {};
+        /**
+         * Gets/sets allowing multiple line for label view. If set to true
+         * and text is too long to show in single line label shows text as 
+         * multiline. 
+         * 
+         * @member {boolean} multipleLine Show multiple line in label 
+         */
+        this.multipleLine = true;
 
-    /**
-     * Gets/sets text inside label view.
-     * 
-     * @member {string} text Text inside label
-     */
-    this.text = "Text";
+        /**
+         * Gets/sets style of view. 
+         * 
+         * @member {Style} style Style of view
+         */
+        this.style = {};
 
-    /**
-     * Gets/sets text alignment of label view. UI.TextAlignment constants
-     * can be used.
-     * 
-     * @example
-     * var label = new Label();
-     * label.textAlignment = TextAlignment.CENTER;
-     * 
-     * @member {number} textAlignment Text alignment
-     */
-    this.textAlignment = TextAlignment.CENTER;
+        /**
+         * Gets/sets text inside label view.
+         * 
+         * @member {string} text Text inside label
+         */
+        this.text = "Text";
 
-    /**
-     * Gets/sets text color of view.
-     * 
-     * @member {Color} textColor Text Color
-     */
-    this.textColor = "#000000";
+        /**
+         * Gets/sets text alignment of label view. UI.TextAlignment constants
+         * can be used.
+         * 
+         * @example
+         * var label = new Label();
+         * label.textAlignment = TextAlignment.CENTER;
+         * 
+         * @member {number} textAlignment Text alignment
+         */
+        this.textAlignment = TextAlignment.CENTER;
 
-    /**
-     * Sets/gets showing scroll bar when text doesn't fit to label view.
-     * 
-     * @member {Boolean} showScrollBar
-     */
-    this.showScrollBar = true;
-    
-    // Assign properties given in constructor
-    if (props) {
-        for (var prop in props) {
-            this[prop] = props[prop];
+        /**
+         * Gets/sets text color of view.
+         * 
+         * @member {Color} textColor Text Color
+         */
+        this.textColor = "#000000";
+
+        /**
+         * Sets/gets showing scroll bar when text doesn't fit to label view.
+         * 
+         * @member {Boolean} showScrollBar
+         */
+        this.showScrollBar = true;
+        
+        // Assign parameters given in constructor
+        if (params) {
+            for (var param in params) {
+                this[param] = params[param];
+            }
         }
     }
-}
+);
 
 module.exports = Label;
