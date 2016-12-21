276770d092138e4760af6a0315c4c2c7b5fe8f04
M.Halit Karakis <halit.karakis@smartface.io>
2016-12-21 16:48:13 +0300

    Initial color analysis

---
+++ ui/color/analysis.js
@@ -0,0 +1,24 @@
+// Usages
+label.backgroundColor = Color.BLACK;
+label.backgroundColor = Color.create("#FF0000");
+label.backgroundColor = Color.create(10, 20, 30);
+label.backgroundColor = Color.create(10, 20, 30, 40);
+
+var green = Color.green(Color.create("#FF0000"));
+var blue = Color.blue(colorValue);
+var red = Color.red(colorValue);
+var alpha = Color.alpha(colorValue);
+
+// Constants
+Color.BLACK
+Color.BLUE
+Color.CYAN
+Color.DARKGRAY
+Color.GRAY
+Color.GREEN
+Color.LIGHTGRAY
+Color.MAGENTA
+Color.RED
+Color.TRANSPARENT
+Color.YELLOW
+Color.WHITE
