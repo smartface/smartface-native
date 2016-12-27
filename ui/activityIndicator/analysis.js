f5aa02b6e1050ccecc6ce1fa5d4db0f47adfc4e9
Hasret Sariyer <hasret.sariyer@smartface.io>
2016-12-27 10:14:55 +0200

    [COR-1088] Write analysis of activity indicator

---
+++ ui/activityIndicator/analysis.js
@@ -0,0 +1,34 @@
+var ActivityIndicatorStyle = {
+    NORMAL: 0,
+    LARGE: 1
+};
+
+/**
+ * @class ActivityIndicator
+ * @since 0.1
+ * 
+ * ActivityIndicator class shows a progress of some background task.
+ *
+ *     @example
+ *     const ActivityIndicator = require('sf-core/ui/activityIndicator');
+ *     var myActivityIndicator = new ActivityIndicator();
+ *     myActivityIndicator.style = ActivityIndicatorStyle.LARGE;
+ */
+function ActivityIndicator(params) {
+    /**
+     * Gets/sets color of the activity indicator.
+     * 
+     * @property {Color} color Color of the activity indicator
+     */
+    this.color = COLOR.GRAY;
+
+    /**
+     * Gets/sets style of the activity indicator.
+     * 
+     * @property {Number} style Style of the activity indicator
+     */
+    this.style =  ActivityIndicatorStyle.NORMAL;
+
+}
+
+module.exports = {ActivityIndicator: ActivityIndicator, ActivityIndicatorStyle: ActivityIndicatorStyle};
