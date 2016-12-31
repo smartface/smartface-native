2e6d2479ff3d728d682714e41e2acaf931045481
Metehan Toksoy <metehan.toksoy@smartface.com>
2016-12-31 15:23:09 +0200

    [COR-1091] edited ui/index.js for ViewGroup

--- ui/index.js
+++ ui/index.js
@@ -5,7 +5,8 @@ module.exports = {
     TextAlignment : require('./textalignment.js'),
     Font: require('./font'),
     Color: require('./color'),
-    Button : require('./button').Button,
-    StateList : require('./button').StateList,
-    ActivityIndicator: require('./activityindicator')
+    Button: require('./button').Button,
+    StateList: require('./button').StateList,
+    ActivityIndicator: require('./activityindicator'),
+    ViewGroup: require('./viewgroup')
 };
