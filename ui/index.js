941752abf7ced1509bccda3e7b363047f5cb909a
Dogan Ekici <smartface556@users.smartfacecloud.io>
2017-01-03 11:51:09 +0000

    added analysis

--- ui/index.js
+++ ui/index.js
@@ -5,7 +5,8 @@ module.exports = {
     TextAlignment : require('./textalignment.js'),
     Font: require('./font'),
     Color: require('./color'),
-    Switch: require('./switch'),
-    Button : require('./button'),
-    ActivityIndicator: require('./activityindicator')
+    Button: require('./button').Button,
+    StateList: require('./button').StateList,
+    ActivityIndicator: require('./activityindicator'),
+    Switch: require('./switch')
 };
