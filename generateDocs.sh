1caf97eb1cfdcfd643c61ec815e29f042a1a5047
Hasret Sariyer <hasret.sariyer@smartface.io>
2017-01-05 17:51:14 +0200

    Add version of document

--- generateDocs.sh
+++ generateDocs.sh
@@ -5,6 +5,8 @@ cp docs_resource/welcome.html api-docs/
 cp docs_resource/smf.css api-docs/resources/css/
 cp docs_resource/images/logo.png api-docs/resources/images/logo.png
 
+python addVersion.py
+
 cd api-docs
 zip -r ../api-docs.zip *
 cd ..
