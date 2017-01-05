baa000bb68bd27ad2f2fb3161f8a25c7da5b6161
Eray Tuncer <eray.tuncer@smartface.io>
2017-01-05 19:08:35 +0300

    [COR-1093] add Label unit tests.

--- tests/index.js
+++ tests/index.js
@@ -1,14 +1,13 @@
 require("salep");
+
 salep.on("success", function(testCase) {
-    console.log("Case [" + testCase.name + "]...succeeded"); 
 });
 salep.on("fail", function(testCase) {
-    console.log("Case [" + testCase.name + "]...failed: " + testCase.reason); 
+    log("Case [" + testCase.name + "]...failed: " + testCase.reason);
+    console.log("Case [" + testCase.name + "]...failed: " + testCase.reason);
 });
-
-salep.run();
 
 require("./ui");
 
-var result = salep.stop();
-alert(JSON.stringify(result, null, 2));
+var result = salep.getResults();
+alert("Success: " + result.success + ", Fail: " + result.fail + ", Skip: " + result.skip + ", Total: " + result.total);
