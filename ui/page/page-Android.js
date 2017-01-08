7244364ce39c6b13504a7d7efaa63cd5d8e33660
Metehan Toksoy <metehan.toksoy@smartface.io>
2017-01-08 21:15:07 +0000

    [AND-2511] Fixed Page's orientation resizing.

--- ui/page/page-Android.js
+++ ui/page/page-Android.js
@@ -4,39 +4,56 @@ const Color = require("sf-core/ui/color");
 function Page(params) {
     var self = this;
     var activity = Android.getActivity();
-
-    self.height = Device.screenHeight;
-    self.width = Device.screenWidth;
-
+    
+    // self.height and self.width for child views. It can get root layout dimensions from it.
+    var verticalDimention = self.height = Device.screenHeight;
+    var horizontalDimention = self.width = Device.screenWidth;
+    
     var innerLayout = new AbsoluteLayout({
-        height: Device.screenHeight,
-        width: Device.screenWidth,
+        height: verticalDimention,
+        width: horizontalDimention,
         backgroundColor:Color.WHITE
-    })
+    });
     innerLayout.parent = self;
 
-
-    self.nativeObject = android.support.v4.app.Fragment.extend("SFFragment", {
+    self.nativeObject = android.app.Fragment.extend("SFFragment", {
                             onCreateView: function() {
+                                console.log("onCreateView");
                                 return innerLayout.nativeObject;
                             },
                             onViewCreated: function(view, savedInstanceState){
-                                innerLayout.invalidatePosition(self.width, self.height);
+                                console.log("onViewCreated");
+                                self.invalidatePosition();
                                 onShowCallback && onShowCallback();
                             },
                             onConfigurationChanged: function(newConfig){
-                                if(newConfig.orientation == 2){
-                                    innerLayout.setPosition({width: Device.screenHeight, height:Device.screenWidth});
-                                    self.width= Device.screenHeight;
-                                    self.height=Device.screenWidth;
-                                }
-                                else{
-                                    innerLayout.setPosition({width: Device.screenWidth, height:Device.screenHeight});
-                                    self.height = Device.screenHeight;
-                                    self.width = Device.screenWidth;
-                                }
-
+                                self.invalidatePosition(newConfig.orientation);
                             }
+                            // not working for onHide. 
+                            // onDetach: function(){
+                            //     console.log("onDetach");
+                            //     onHideCallback && onHideCallback();
+                            // },
+                            // onHiddenChanged: function(isHidden){
+                            //     console.log("onHiddenChanged");
+                            //     onHideCallback && onHideCallback();
+                            // },
+                            // onPause: function(isHidden){
+                            //     console.log("onPause");
+                            //     onHideCallback && onHideCallback();
+                            // },
+                            // onStop: function(isHidden){
+                            //     console.log("onStop");
+                            //     onHideCallback && onHideCallback();
+                            // },
+                            // onDestroyView: function(isHidden){
+                            //     console.log("onDestroyView");
+                            //     onHideCallback && onHideCallback();
+                            // },
+                            // onDestroy: function(isHidden){
+                            //     console.log("onDestroy");
+                            //     onHideCallback && onHideCallback();
+                            // }
     }, null);
 
     var onShowCallback;
@@ -82,6 +99,29 @@ function Page(params) {
     this.remove = function(view){
         innerLayout.removeChild(view)
     };
+    
+    this.invalidatePosition = function(orientation){
+        if(!orientation){
+            orientation = activity.getResources().getConfiguration().orientation;
+        }
+        if(orientation == 2){
+            self.width = Device.screenHeight;
+            self.height = Device.screenWidth;
+            innerLayout.setPosition({width: Device.screenHeight, height:Device.screenWidth});
+        }
+        else{
+            self.height = Device.screenHeight;
+            self.width = Device.screenWidth;
+            innerLayout.setPosition({width: Device.screenWidth, height:Device.screenHeight});
+        }
+    }
+    
+    // Assign parameters given in constructor
+    if (params) {
+        for (var param in params) {
+            this[param] = params[param];
+        }
+    }
 }
 
 module.exports = Page;
