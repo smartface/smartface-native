ff74e84840ef36ad5016279b77b1ed124167325e
Eray Tuncer <eray.tuncer@smartface.io>
2017-01-09 14:19:28 +0300

    [COR-1123] add unit tests for UI.AlertView.

--- tests/index.js
+++ tests/index.js
@@ -3,8 +3,20 @@ require("salep");
 salep.on("success", function(testCase) {
 });
 salep.on("fail", function(testCase) {
-    log("Case [" + testCase.name + "]...failed: " + testCase.reason);
-    console.log("Case [" + testCase.name + "]...failed: " + testCase.reason);
+    log("Case Failed : [" + testCase.name + "]. Reason : " + testCase.reason);
+    console.log("Case Failed : [" + testCase.name + "]. Reason : " + testCase.reason);
+
+    // UnComment below after salep's testCase.parent.name bug is fixed
+    // log("Case Failed : [" + testCase.name + "] in " + testCase.parent.name + ". Reason : " + testCase.reason);
+    // console.log("Case Failed : [" + testCase.name + "] in " + testCase.parent.name + ". Reason : " + testCase.reason);
+});
+salep.on("skip", function(testCase) {
+    log("Skipped Case [" + testCase.name + "].");
+    console.log("Skipped Case [" + testCase.name + "].");
+
+    // UnComment below after salep's testCase.parent.name bug is fixed
+    // log("Skipped Case [" + testCase.name + "] in " + testCase.parent.name + ".");
+    // console.log("Skipped Case [" + testCase.name + "] in " + testCase.parent.name + ".");
 });
 
 require("./ui");
