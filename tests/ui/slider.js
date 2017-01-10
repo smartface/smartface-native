7b3f69525a07eea9a86cad7ce594dc539eec6ce1
Hasret Sariyer <hasret.sariyer@smartface.io>
2017-01-10 15:00:11 +0200

    [COR-1136] Edit slider.js

--- tests/ui/slider.js
+++ tests/ui/slider.js
@@ -3,48 +3,43 @@ var assert  = require("chai").assert;
 const Slider = require('sf-core/ui/slider');
 const Color = require('sf-core/ui/color');
 
-var mySlider = new Slider();
 
 salep.test("sf-core/ui/slider Unit Test", function () {
-
+    var mySlider = new Slider();
     //Test with default values
-    this.case("[thumbColor] setter/getter with default value.", function () {
+    this.case("[Slider] test defaults values.", function() {
+        // Test thumbColor
         assert.equal(mySlider.thumbColor, Color.GRAY, "thumbColor must be equal to Color.GRAY");
-        assert.notEqual(mySlider.thumbColor, Color.RED, "thumbColor is not equal to Color.RED");
-    });
-
-    this.case("[thumbImage] setter/getter with default value.", function () {
+        assert.notEqual(mySlider.thumbColor, Color.RED, "thumbColor is not equal to Color.RED"); 
+        
+        // Test thumbImage
         assert.equal(mySlider.thumbImage, "images://smartface.png", "thumbImage must be equal to 'images://smartface.png'");
         assert.deepNotEqual(mySlider.thumbImage, "images://icon.png", "thumbImage is not equal to 'images://icon.png'");
-    });
-
-    this.case("[minTrackColor] setter/getter with default value.", function () {
-        assert.equal(mySlider.minTrackColor, Color.BLUE, "minTrackColor must be equal to Color.BLUE");
-        assert.notEqual(mySlider.minTrackColor, Color.RED, "minTrackColor is not equal to Color.RED");
-    });
-
-    this.case("[maxTrackColor] setter/getter with default value.", function () {
+    
+        // Test maxTrackColor
         assert.equal(mySlider.maxTrackColor, Color.GREEN, "maxTrackColor must be equal to Color.GREEN");
         assert.notEqual(mySlider.maxTrackColor, Color.BLUE, "maxTrackColor is not equal to Color.BLUE");
-    });
-
-    this.case("[maxValue] setter/getter with default value.", function () {
+    
+        // Test minTrackColor
+        assert.equal(mySlider.minTrackColor, Color.BLUE, "minTrackColor must be equal to Color.BLUE");
+        assert.notEqual(mySlider.minTrackColor, Color.RED, "minTrackColor is not equal to Color.RED");
+    
+        // Test maxValue
         assert.equal(mySlider.maxValue, 100, "maxValue must be equal to 100");
         assert.notEqual(mySlider.maxValue, 50, "maxValue is not equal to 50");
-    });
 
-    this.case("[minValue] setter/getter with default value.", function () {
+        // Test minValue
         assert.equal(mySlider.minValue, 0, "minValue must be equal to 0");
         assert.notEqual(mySlider.minValue, 100, "minValue is not equal to 100");
-    });
 
-    this.case("[value] setter/getter with default value.", function () {
+        // Test value
         assert.equal(mySlider.value, 30, "value must be equal to 30");
         assert.notEqual(mySlider.value, 100, "value is not equal to 100");
     });
     
     // Test with assigned values 
     this.case("[thumbColor] setter/getter with blue color.", function () {
+        var mySlider = new Slider();
         mySlider.thumbColor = Color.BLUE;
 
         assert.equal(mySlider.thumbColor, Color.BLUE, "thumbColor must be equal to Color.BLUE");
@@ -52,13 +47,15 @@ salep.test("sf-core/ui/slider Unit Test", function () {
     });
 
     this.case("[thumbImage] setter/getter with blue color.", function () {
+        var mySlider = new Slider();
         mySlider.thumbImage = "images://smartface.png";
 
-        assert.deepEqual(mySlider.thumbImage, "images://smartface.png", "thumbImage must be equal to 'images://smartface.png'");
-        assert.deepNotEqual(mySlider.thumbImage, "images://icon.png", "thumbImage is not equal to 'images://icon.png'");
+        assert.equal(mySlider.thumbImage, "images://smartface.png", "thumbImage must be equal to 'images://smartface.png'");
+        assert.notEqual(mySlider.thumbImage, "images://icon.png", "thumbImage is not equal to 'images://icon.png'");
     });
 
     this.case("[minTrackColor] setter/getter with green color.", function () {
+        var mySlider = new Slider();
         mySlider.minTrackColor = Color.GREEN;
 
         assert.equal(mySlider.minTrackColor, Color.GREEN, "minTrackColor must be equal to Color.GREEN");
@@ -66,13 +63,15 @@ salep.test("sf-core/ui/slider Unit Test", function () {
     });
 
     this.case("[maxTrackColor] setter/getter with red color.", function () {
+        var mySlider = new Slider();
         mySlider.maxTrackColor = Color.RED;
 
         assert.equal(mySlider.maxTrackColor, Color.RED, "maxTrackColor must be equal to Color.RED");
         assert.notEqual(mySlider.maxTrackColor, Color.BLUE, "maxTrackColor is not equal to Color.BLUE");
     });
 
-    this.case("[maxValue] setter/getter with assigned to 100.", function () {
+    this.case("[maxValue] setter/getter with assigned to 200.", function () {
+        var mySlider = new Slider();
         mySlider.maxValue = 200;
 
         assert.equal(mySlider.maxValue, 200, "maxValue must be equal to 200");
@@ -87,6 +86,7 @@ salep.test("sf-core/ui/slider Unit Test", function () {
     });
 
     this.case("[value] setter/getter with assigned to 70.", function () {
+        var mySlider = new Slider();
         mySlider.value = 70;
 
         assert.equal(mySlider.value, 70, "value must be equal to 70");
