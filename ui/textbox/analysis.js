8f60656d9783856b60b605ea10b1be0128e8f761
Metehan Toksoy <metehan.toksoy@smartface.com>
2017-01-10 22:54:11 +0200

    [COR-1130] Added events, ios and android specific properties

--- ui/textbox/analysis.js
+++ ui/textbox/analysis.js
@@ -2,6 +2,7 @@ const Label = require('../label');
 const extend = require('js-base/core/extend');
 
 /** @enum {Number} UI.Textbox.KeyboardType
+ s* @static
  * @since 0.1
  * KeyboardType is an enum. It defines keyboard appearance when user focused
  * to the TextBox.
@@ -16,39 +17,44 @@ var KeyboardType = { };
  * @property {Number} DEFAULT
  * Default keyboard appearance.
  * @static
+ * @readonly
  * @since 0.1
  */
 KeyboardType.DEFAULT = 0;
 
 /**
  * @property {Number} DATE
- * @static
- * @since 0.1
  * Date specific keyboard appearance
+ * @static
+ * @readonly
+ * @since 0.1
  */
 KeyboardType.DATE = 1;
 
 /**
  * @property {Number} NEGATIVE
- * @static
- * @since 0.1
  * Numeric specific keyboard appearance
+ * @static
+ * @readonly
+ * @since 0.1
  */
 KeyboardType.NUMBER = 2;
 
 /**
  * @property {Number} DECIMAL
+ * Decimal specific keyboard appearance
  * @static
  * @since 0.1
- * Decimal specific keyboard appearance
+ * @readonly
  */
 KeyboardType.DECIMAL = 3;
 
 /**
  * @property {Number} EMAILADDRESS
+ * Email address specific keyboard appearance
  * @static
  * @since 0.1
- * Email address specific keyboard appearance
+ * @readonly
  */
 KeyboardType.EMAILADDRESS = 4;
 
@@ -95,20 +101,61 @@ const TextBox = extend(Label)(
         this.hint = "";
 
         /**
-         * Gets/sets the color of the hint text.
+         * Gets/sets the color of the hint text. This property will work only for Android.
          *
          *     @example
          *     const TextBox = require('sf-core/ui/textbox').TextBox;
          *     const Color = require('sf-core/ui/color')
          *     var myTextBox = new TextBox({
          *         hint: "Smartface TextBox",
-         *         hintTextColor: Color.RED
+         *         android: {
+         *             hintTextColor: Color.RED
+         *         }
          *     });
          *
          * @property {Color} hintTextColor
          * @since 0.1
          */
-        this.hintTextColor = Color.GRAY;
+        this.android.hintTextColor = Color.LIGHTGRAY;
+
+        /**
+         * Gets/sets adjustment status of TextBox. If enabled text size will be adjusted by
+         * view's width and height. It must be greater than minimumFontSize.
+         * This property will work only for iOS.
+         *
+         *     @example
+         *     const TextBox = require('sf-core/ui/textbox').TextBox;
+         *     var myTextBox = new TextBox({
+         *         hint: "Smartface TextBox",
+         *         ios: {
+         *             adjustFontSizeToFit: true
+         *         }
+         *     });
+         *
+         * @property {Boolean} adjustFontSizeToFit
+         * @since 0.1
+         */
+        this.ios.adjustFontSizeToFit = false;
+
+        /**
+         * Gets/sets minimum font size of TextBox.
+         * This property will work only for iOS.
+         *
+         *     @example
+         *     const TextBox = require('sf-core/ui/textbox').TextBox;
+         *     var myTextBox = new TextBox({
+         *         hint: "Smartface TextBox",
+         *         ios: {
+         *             adjustFontSizeToFit: true,
+         *             minimumFontSize:15
+         *         }
+         *     });
+         *
+         * @property {Number} minimumFontSize
+         * @since 0.1
+         */
+        this.ios.minimumFontSize = 7;
+
 
         /**
          * Gets/sets clear button state. If enabled, clear button will be shown
@@ -157,9 +204,19 @@ const TextBox = extend(Label)(
          */
         this.keyboardType = KeyboardType.DEFAULT;
 
+
+        /**
+         * This method shows keyboard manually.
+         *
+         * @method showKeyboard
+         * @since 0.1
+         */
+        this.showKeyboard = function(){};
+
         // events
         /**
-         * Gets/sets text change event for textbox.
+         * Gets/sets text change event for TextBox. When user insert or delete character
+         * to the TextBox, this event will fire.
          *
          *     @example
          *     const TextBox = require('sf-core/ui/textbox').TextBox;
@@ -175,7 +232,67 @@ const TextBox = extend(Label)(
          * @event onTextChanged
          * @since 0.1
          */
-        this.onTextChanged = function(newText) {};
+        this.onTextChanged = function() {};
+
+        /**
+         * Gets/sets on begin editing event for TextBox. When user starts to insert or delete character
+         * to the TextBox this event will fire.
+         *
+         *     @example
+         *     const TextBox = require('sf-core/ui/textbox').TextBox;
+         *     const Label = require('sf-core/ui/label');
+         *     var myLabel = new Label();
+         *     var myTextBox = new TextBox({
+         *         hint: "Smartface TextBox Password",
+         *         onBeginEditing: function(){
+         *             myLabel.text = myTextBox.text;
+         *         }
+         *     });
+         *
+         * @event onBeginEditing
+         * @since 0.1
+         */
+        this.onBeginEditing = function() {};
+
+        /**
+         * Gets/sets on end editing event for TextBox. When user finishes editing,
+         * the TextBox this event will fire.
+         *
+         *     @example
+         *     const TextBox = require('sf-core/ui/textbox').TextBox;
+         *     const Label = require('sf-core/ui/label');
+         *     var myLabel = new Label();
+         *     var myTextBox = new TextBox({
+         *         hint: "Smartface TextBox Password",
+         *         onBeginEditing: function(){
+         *             myLabel.text = myTextBox.text;
+         *         }
+         *     });
+         *
+         * @event onEndEditing
+         * @since 0.1
+         */
+        this.onEndEditing = function() {};
+
+        /**
+         * Gets/sets on focus event for TextBox. When user clicks to the TextBox and when,
+         * TextBox gains focus this event will fire.
+         *
+         *     @example
+         *     const TextBox = require('sf-core/ui/textbox').TextBox;
+         *     const Label = require('sf-core/ui/label');
+         *     var myLabel = new Label();
+         *     var myTextBox = new TextBox({
+         *         hint: "Smartface TextBox Password",
+         *         onFocus: function(){
+         *             myLabel.text = "TextBox focused.";
+         *         }
+         *     });
+         *
+         * @event onFocus
+         * @since 0.1
+         */
+        this.onFocus = function() {};
     }
 );
 
