67d0ac2d1ac7e2da8c45e38c79c310d4306342fb
smartface10661 <smartface10661@users.smartfacecloud.io>
2017-01-16 15:24:32 +0000

    [story/AND-2559] Add invalidateStatusBar function

--- ui/page/page-Android.js
+++ ui/page/page-Android.js
@@ -155,8 +155,15 @@ function Page(params) {
         }
     }
     
+    this.invalidateStatusBar = function(){
+        console.log("visible = " + _visible);
+        self.statusBar.visible = _visible;
+        self.statusBar.color = _color;
+        
+    }
+    
     // Default values
-    self.statusBar.visible = true;
+    //self.statusBar.visible = true;
     // todo Add color default value after resolving COR-1153.
     
     // Assign parameters given in constructor
