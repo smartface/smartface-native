3f8177465af614cc25b6ef868db5bb2339441d64
smartface10661 <smartface10661@users.smartfacecloud.io>
2017-01-17 12:56:27 +0000

    [AND-2543] Textbox mplemententation for android. Added new requirements

--- ui/textbox/textbox-Android.js
+++ ui/textbox/textbox-Android.js
@@ -7,119 +7,112 @@ const ActionKeyType = require('../actionkeytype');
 const NativeActivity = requireClass("android.app.Activity");
 const NativeContext = requireClass("android.content.Context"); 
 const NativeInputMethodManager = requireClass("android.view.inputmethod.InputMethodManager");
+const NativeEditText = requireClass("android.widget.EditText"); 
+const NativeView = requireClass("android.view.View");
+const NativeTextWatcher = requireClass("android.text.TextWatcher");
+const NativeTextView = requireClass("android.widget.TextView");
 
-// InputType.TYPE_CLASS_TEXT = 1
-// InputType.TYPE_CLASS_NUMBER = 2 
-// InputType.TYPE_CLASS_PHONE = 3
-// InputType.TYPE_CLASS_DATETIME = 4
-// InputType.TYPE_NUMBER_FLAG_DECIMAL = 8192
-// InputType.TYPE_NUMBER_FLAG_SIGNED = 4096
-// InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS = 32
-// InputType.TYPE_TEXT_VARIATION_PASSWORD = 128
-// InputType.TYPE_NUMBER_VARIATION_PASSWORD = 16
-// InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE = 65536
-// InputType.TYPE_TEXT_FLAG_AUTO_CORRECT = 32768
-// InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS = 4096
-// InputType.TYPE_TEXT_FLAG_CAP_SENTENCES = 16384
-// InputType.TYPE_TEXT_FLAG_CAP_WORDS = 8192
-// InputType.TYPE_TEXT_VARIATION_EMAIL_SUBJECT = 48
-// InputType.TYPE_TEXT_VARIATION_LONG_MESSAGE = 80
-// Input.TYPE_TEXT_FLAG_NO_SUGGESTIONS = 524288
-// Input.TYPE_TEXT_VARIATION_PERSON_NAME = 96
-// Input.TYPE_TEXT_VARIATION_SHORT_MESSAGE = 64
-// Input.TYPE_DATETIME_VARIATION_TIME = 32
-// Input.TYPE_TEXT_VARIATION_URI = 16
-var NativeKeyboardType = {
-    DEFAULT: 1,
-    NUMBER: 2,
-    DECIMAL: 2 | 8192,
-    PHONE: 3,
-    URL: 1 | 16,
-    android: {
-        DATETIME: 4,
-        SIGNEDNUMBER: 2 | 4096, 
-        SIGNEDDECIMAL: 2 | 8192 | 4096,
-        TEXTAUTOCOMPLETE: 1 | 65536,
-        TEXTAUTOCORRECT: 1 | 32768,
-        TEXTCAPCHARACTERS: 1 | 4096,
-        TEXTCAPSENTENCES: 1 | 16384,
-        TEXTCAPWORDS: 1 | 8192,
-        TEXTEMAILSUBJECT: 1 | 48,
-        TEXTLONGMESSAGE: 1 | 80,
-        TEXTNOSUGGESTIONS: 1 | 524288,
-        TEXTPERSONNAME: 1 | 96,
-        TEXTSHORTMESSAGE: 1 | 64,
-        TIME: 4 | 32,
-        EMAILADDRESS: 1 | 32,
-    }
-};
+const NativeKeyboardType = [1,  // InputType.TYPE_CLASS_TEXT
+    2,              //InputType.TYPE_CLASS_NUMBER
+    2 | 8192,       // InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL
+    3,              // InputType.TYPE_CLASS_PHONE
+    1 | 16,         // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_URI
+    1,              // InputType.TYPE_CLASS_TEXT
+    1,              // InputType.TYPE_CLASS_TEXT
+    4,              // InputType.TYPE_CLASS_DATETIME
+    2 | 4096,       // InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED
+    2 | 8192 | 4096,// InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_NUMBER_FLAG_SIGNED 
+    1 | 65536,      // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE
+    1 | 32768,      // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_AUTO_CORRECT
+    1 | 4096,       // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS
+    1 | 16384,      // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
+    1 | 8192,       // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_WORDS
+    1 | 48,         // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_SUBJECT
+    1 | 80,         // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_LONG_MESSAGE
+    1 | 524288,     // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+    1 | 96,         // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PERSON_NAME
+    1 | 64,         // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_SHORT_MESSAGE
+    4 | 32,         // InputType.TYPE_CLASS_DATETIME | InputType.TYPE_DATETIME_VARIATION_TIME
+    1 | 32          // InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+]
 
-var NativePasswordType = {
+const NumberInputTypeIndex = [1, 2, 3, 7, 8, 9, 20];
+
+const NativePasswordType = {
     NUMBER: 16,
     TEXT: 128
 };
 
-var NativeKeyEvent  = {
-    DEFAULT: 66,
-    NEXT: 500,
-    GO: 500,
-    SEARCH: 500,
-    SEND: 500
-}
+// NativeActionKeyType corresponds android action key type.
+// The values are taken from android.view.inputmethod.EditorInfo.
+const NativeActionKeyType = [6, 5, 2, 3, 4];
 
 const TextBox = extend(Label)(
     function (_super, params) {
         var self = this;
         var activity = Android.getActivity();
         if(!self.nativeObject){
-            self.nativeObject = new android.widget.EditText(Android.getActivity());
+            self.nativeObject = new NativeEditText(activity);
         }
         _super(this);
 
-        var _hintInitial = "";
+        var _hint = "";
         Object.defineProperty(this, 'hint', {
             get: function() {
-                return _hintInitial;
+                return _hint;
             },
-            set: function(_hintInitial) {
-                self.nativeObject.setHint(_hintInitial);
+            set: function(hint) {
+                _hint = hint;
+                self.nativeObject.setHint(_hint);
             },
             enumerable: true
         });
         
-        var _hintTextColorInitial = Color.LIGHTGRAY;
+        var _hintTextColor = Color.LIGHTGRAY;
         this.android = {}
         Object.defineProperty(this.android, 'hintTextColor', {
             get: function() {
-                return _hintTextColorInitial;
+                return _hintTextColor;
             },
             set: function(hintTextColor) {
-                _hintTextColorInitial = hintTextColor; 
-                self.nativeObject.setHintTextColor(_hintTextColorInitial);
+                _hintTextColor = hintTextColor; 
+                self.nativeObject.setHintTextColor(_hintTextColor);
             },
             enumerable: true
         });
         
-        var _isPasswordInitial = false;
+        var _isPassword = false;
         Object.defineProperty(this, 'isPassword', {
             get: function() {
-                return _isPasswordInitial;
+                return _isPassword;
             },
             set: function(isPassword) {
-                _isPasswordInitial = isPassword;
+                _isPassword = isPassword;
                 setKeyboardType();
             },
             enumerable: true
         });
         
-        var _keyboardTypeInitial = KeyboardType.DEFAULT;
+        var _keyboardType = KeyboardType.DEFAULT;
         Object.defineProperty(this, 'keyboardType', {
             get: function() {
-                return _keyboardTypeInitial;
+                return _keyboardType;
             },
             set: function(keyboardType) {
-                _keyboardTypeInitial = keyboardType; 
+                _keyboardType = keyboardType; 
                 setKeyboardType();
+            },
+            enumerable: true
+        });
+        
+        var _actionKeyType = ActionKeyType.DEFAULT;
+        Object.defineProperty(this, 'actionKeyType', {
+            get: function() {
+                return _actionKeyType;
+            },
+            set: function(actionKeyType) {
+                _actionKeyType = actionKeyType; 
+                self.nativeObject.setImeOptions(NativeActionKeyType[_actionKeyType]);
             },
             enumerable: true
         });
@@ -179,9 +172,18 @@ const TextBox = extend(Label)(
             enumerable: true
         });
     
-        self.nativeObject.addTextChangedListener(android.text.TextWatcher.implement({
+        self.nativeObject.addTextChangedListener(NativeTextWatcher.implement({
+            // todo: Control insertedText after resolving AND-2508 issue.
             onTextChanged: function(charSequence, start, before, count){
-                _onTextChangedCallback && _onTextChangedCallback();
+                var index = Math.abs(start+before);
+                if(count > before){
+                    var insertedText = charSequence.toString().substring(start+before,start+count);
+                    var e = {
+                        location: index,
+                        insertedText: insertedText
+                    }
+                    _onTextChangedCallback && _onTextChangedCallback(e);
+                }
             },
             
             beforeTextChanged: function(charSequence, start, count, after){
@@ -193,143 +195,62 @@ const TextBox = extend(Label)(
             }
         }));
         
-        self.nativeObject.setOnKeyListener(android.view.View.OnKeyListener.implement({
+        self.nativeObject.setOnKeyListener(NativeView.OnKeyListener.implement({
             onKey: function(view, keyCode, event){
-                if (keyCode == NativeKeyEvent.DEFAULT)  {
+                if (keyCode == NativeActionKeyType)  {
                     _onReturnKeyCallback && _onReturnKeyCallback();
                 }
-                return false;
             }
         }));
         
-        self.nativeObject.setOnEditorActionListener(android.widget.TextView.OnEditorActionListener.implement({
+        self.nativeObject.setOnEditorActionListener(NativeTextView.OnEditorActionListener.implement({
             onEditorAction: function(textView, actionId, event){
-                if (actionId == ActionKeyType.DEFAULT)  {
-                    _onActionButtonCallback && _onActionButtonCallback();
+                if (actionId == NativeActionKeyType[_actionKeyType])  {
+                    _onActionButtonCallback && _onActionButtonCallback({actionKeyType: _actionKeyType});
                 }
                 return false;
             }
         }));
-        
-        // editText.setOnEditorActionListener(new OnEditorActionListener() {        
-        //     @Override
-        //     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-        //         if(actionId==EditorInfo.IME_ACTION_DONE){
-        //             //do something
-        //         }
-        //     return false;
-        //     }
-        // });
         
         function setKeyboardType(){
-            switch (_keyboardTypeInitial) {
-                case KeyboardType.DEFAULT:
-                    self.nativeObject.setInputType(NativeKeyboardType.DEFAULT);
-                    break;
-                case KeyboardType.NUMBER:
-                    self.nativeObject.setInputType(NativeKeyboardType.NUMBER);
-                    break;
-                case KeyboardType.DECIMAL:
-                    self.nativeObject.setInputType(NativeKeyboardType.DECIMAL);
-                    break;
-                case KeyboardType.PHONE:
-                    self.nativeObject.setInputType(NativeKeyboardType.PHONE);
-                    break;
-                case KeyboardType.URL:
-                    self.nativeObject.setInputType(NativeKeyboardType.URL);
-                    break;
-                case KeyboardType.android.DATETIME:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.DATETIME);
-                    break;
-                case KeyboardType.android.SIGNEDNUMBER:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.SIGNEDNUMBER);
-                    break;
-                case KeyboardType.android.SIGNEDDECIMAL:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.SIGNEDDECIMAL);
-                    break;
-                case KeyboardType.android.TEXTAUTOCOMPLETE:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TEXTAUTOCOMPLETE);
-                    break;
-                case KeyboardType.android.TEXTAUTOCORRECT:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TEXTAUTOCORRECT);
-                    break;
-                case KeyboardType.android.TEXTCAPCHARACTERS:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TEXTCAPCHARACTERS);
-                    break;
-                case KeyboardType.android.TEXTCAPSENTENCES:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TEXTCAPSENTENCES);
-                    break;
-                case KeyboardType.android.TEXTCAPWORDS:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TEXTCAPWORDS);
-                    break;
-                case KeyboardType.android.TEXTEMAILSUBJECT:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TEXTEMAILSUBJECT);
-                    break;
-                case KeyboardType.android.TEXTLONGMESSAGE:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TEXTLONGMESSAGE);
-                    break;
-                case KeyboardType.android.TEXTNOSUGGESTIONS:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TEXTNOSUGGESTIONS);
-                    break;
-                case KeyboardType.android.TEXTPERSONNAME:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TEXTPERSONNAME);
-                    break;
-                case KeyboardType.android.TEXTSHORTMESSAGE:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TEXTSHORTMESSAGE);
-                    break;
-                case KeyboardType.android.TIME:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.TIME);
-                    break;
-                case KeyboardType.android.EMAILADDRESS:
-                    self.nativeObject.setInputType(NativeKeyboardType.android.EMAILADDRESS);
-                    break;
-                default:
-                    self.nativeObject.setInputType(NativeKeyboardType.DEFAULT);
-                    break;
-            }
+            self.nativeObject.setInputType(NativeKeyboardType[_keyboardType]);
             
-            if(_isPasswordInitial){
+            if(_isPassword){
+                console.log("ISNumberType " + NumberInputTypeIndex.indexOf(_keyboardType));
                 var inputType = self.nativeObject.getInputType();
-                if((_keyboardTypeInitial == KeyboardType.DEFAULT) || 
-                   (_keyboardTypeInitial == KeyboardType.EMAILADDRESS)
-                   ) {
-                    self.nativeObject.setInputType(inputType | NativePasswordType.TEXT);
+                
+                // self.nativeObject.setInputType(inputType | 16);
+                if(NumberInputTypeIndex.indexOf(_keyboardType) >= 0) {
+                    console.log("setInputType " + inputType | NativePasswordType.NUMBER);
+                    self.nativeObject.setInputType(inputType | NativePasswordType.NUMBER);
                 }
                 else {
-                    self.nativeObject.setInputType(inputType | NativePasswordType.NUMBER);
+                    console.log("setInputType " + inputType | NativePasswordType.TEXT);
+                    self.nativeObject.setInputType(inputType | NativePasswordType.TEXT);
                 }
             }
         }
         
         this.showKeyboard = function(){
-          //  self.nativeObject.requestFocus();
+            // todo: getSystemService doesn't work causing by issue COR-1153
+            // getSystemService added in api level 23.
             self.nativeObject.requestFocus();
             var context = activity.getApplicationContext();
             var inputMethodManager = context.getSystemService(NativeActivity.INPUT_METHOD_SERVICE);
-            inputMethodManager.toggleSoftInput(NativeInputMethodManager.HIDE_IMPLICIT_ONLY, 0);
-            
-            // hideSoftKeyboard(editText, this.getApplicationContext());
-            // mEtSearch.requestFocus();
-            // InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
-            // imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+            inputMethodManager.toggleSoftInput(NativeInputMethodManager.SHOW_FORCED, NativeInputMethodManager.HIDE_IMPLICIT_ONLY);
         };
         
         this.hideKeyboard = function(){
+            // todo: getSystemService doesn't work causing by issue COR-1153
+            // getSystemService added in api level 23.
             self.nativeObject.clearFocus();
             var context = activity.getApplicationContext();
             var inputMethodManager = context.getSystemService(NativeContext.INPUT_METHOD_SERVICE);
             inputMethodManager.hideSoftInputFromWindow(self.nativeObject.getWindowToken(), 0); 
-            
-            // hideSoftKeyboard(editText, this.getApplicationContext());
-            // mEtSearch.clearFocus();
-            // InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-            // imm.hideSoftInputFromWindow(mEtSearch.getWindowToken(), 0);
         };
         
         self.hint = "";
-        self.android.hintTextColor = Color.LIGHTGRAY;
-        self.isPassword = false;
-        self.keyboardType = KeyboardType.DEFAULT;
+        self.multipleLine = false;
         
         // Assign parameters given in constructor
         if (params) {
