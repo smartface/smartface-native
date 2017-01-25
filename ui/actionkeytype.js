5b5e95e2d9198abd4a49e12db6ee4a638cae31f0
Eray Tuncer <eray.tuncer@smartface.io>
2017-01-25 10:44:22 +0300

    [COR-1172] refactor UI.ActionKeyType documentation.

--- ui/actionkeytype.js
+++ ui/actionkeytype.js
@@ -1,24 +1,31 @@
 /**
  * @enum {Number} UI.ActionKeyType
  * @static
+ * @readonly
  * @since 0.1
  *
  * ActionKeyType is an enum. It defines soft keyboard action key (return key) type.
  *
  *     @example
- *     const TextBox = require('sf-core/ui/textbox');
- *     const ActionKeyType = require('sf-core/ui/actionkeytype');
+ *     const Color = require('nf-core/ui/color');
+ *     const TextBox = require('nf-core/ui/textbox');
+ *     const ActionKeyType = require('nf-core/ui/actionkeytype');
+ *     
  *     var myTextBox = new TextBox({
- *         hint: "Smartface TextBox",
+ *         hint: "hint",
+ *         width: "50%",
+ *         backgroundColor: Color.create("#7fd0ff"),
  *         actionKeyType: ActionKeyType.DEFAULT
  *     });
+ *     
+ *     myPage.add(myTextBox);
  *
  */
 var ActionKeyType = { };
 
 /**
  * @property {Number} DEFAULT
- * Default colored keyboard appearance. This constant corresponds to the done action.
+ * Done action for soft keyboard.
  * @static
  * @readonly
  * @since 0.1
