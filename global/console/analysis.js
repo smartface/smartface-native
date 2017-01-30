2398a46620e771c7d27b2d7617781040ef42fe91
smartface10661 <smartface10661@users.smartfacecloud.io>
2017-01-30 11:39:23 +0000

    [COR-1180] Define static method outside class

--- global/console/analysis.js
+++ global/console/analysis.js
@@ -4,28 +4,27 @@
  *
  * Outputs a message to the debugging console.
  */
-function console() {
+function console() {}
    
-    /**
-     * Logs a message to the debugging console.
-     * 
-     *     @example
-     *     var text = "Some text";
-     *     console.log(text);
-     * 
-     *     var number = 5432;
-     *     console.log(number);
-     * 
-     *     const Label = require('nf-core/ui/label');
-     *     var myLabel = new Label({
-     *         text: "This is my label"
-     *     }); 
-     *     console.log(myLabel.text); 
-     * 
-     * @param {Object} msg Object to output in the console.
-     * @method log
-     * @static
-     * @since 0.1
-     */
-    this.log = function(msg){}
-}
+/**
+ * Logs a message to the debugging console.
+ * 
+ *     @example
+ *     var text = "Some text";
+ *     console.log(text);
+ * 
+ *     var number = 5432;
+ *     console.log(number);
+ * 
+ *     const Label = require('nf-core/ui/label');
+ *     var myLabel = new Label({
+ *         text: "This is my label"
+ *     }); 
+ *     console.log(myLabel.text); 
+ * 
+ * @param {Object} msg Object to output in the console.
+ * @method log
+ * @static
+ * @since 0.1
+ */
+console.log = function(msg){}
