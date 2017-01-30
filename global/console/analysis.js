986615bd28eef805994f8f6ad7a626fb4174b1ff
smartface10661 <smartface10661@users.smartfacecloud.io>
2017-01-30 08:44:14 +0000

    [COR-1180] Write analysis of console

---
+++ global/console/analysis.js
@@ -0,0 +1,31 @@
+/**
+ * @class console
+ * @since 0.1
+ *
+ * Outputs a message to the debugging console.
+ */
+function console() {
+   
+    /**
+     * Logs a message to the debugging console.
+     * 
+     *     @example
+     *     var text = "Some text";
+     *     console.log(text);
+     * 
+     *     var number = 5432;
+     *     console.log(number);
+     * 
+     *     const Label = require('sf-core/ui/label');
+     *     var myLabel = new Label({
+     *         text: "This is my label"
+     *     }); 
+     *     console.log(myLabel.text); 
+     * 
+     * @param {Object} msg Object to output in the console.
+     * @method log
+     * @static
+     * @since 0.1
+     */
+    this.log = function(msg){}
+}
