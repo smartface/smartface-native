08c355e0b0c182f8b2bf34f3b89703887e2772cc
Eray Tuncer <eray.tuncer@smartface.io>
2017-01-31 11:05:08 +0300

    [COR-1181] add Device.System.

---
+++ device/system/analysis.js
@@ -0,0 +1,162 @@
+/**
+ * @class Device.System
+ * @since 0.1
+ * 
+ * TODO: type definition
+ */
+function System() {}
+
+/**
+ *
+ * TODO: type definition
+ * @property {String} OS
+ * @readonly
+ * @static
+ * @since 0.1
+ */
+System.OS;
+
+/**
+ *
+ * TODO: type definition
+ * @property {String} OSVersion
+ * @readonly
+ * @static
+ * @since 0.1
+ */
+System.OSVersion;
+
+/**
+ *
+ * TODO: type definition
+ * @property {String} firmwareVersion
+ * @readonly
+ * @static
+ * @since 0.1
+ */
+System.firmwareVersion;
+
+/**
+ *
+ * TODO: SET / GET, type definition
+ * @property {Boolean} accelerometerEnabled
+ * @static
+ * @since 0.1
+ */
+System.accelerometerEnabled;
+
+/**
+ *
+ * TODO: type definition
+ * @property {Boolean} menuKeyAvaliable
+ * @readonly
+ * @static
+ * @since 0.1
+ */
+System.menuKeyAvaliable;
+
+/**
+ *
+ * TODO: type definition
+ * @property {Boolean} fingerPrintAvaliable
+ * @readonly
+ * @static
+ * @since 0.1
+ */
+System.fingerPrintAvaliable;
+
+/**
+ *
+ * TODO: type definition
+ * @property {String} clipboard
+ * @static
+ * @since 0.1
+ */
+System.clipboard;
+
+/**
+ *
+ * TODO: type definition
+ * @property {String} secureID
+ * @readonly
+ * @static
+ * @since 0.1
+ */
+System.android.secureID;
+
+/**
+ * TODO: type definition
+ *     
+ *     @example
+ *     System.android.getPackageVersion({
+ *         packageName: "io.smartface.SmartfaceDemo",
+ *         onSuccess: function(e) {
+ *             alert("app version name:" + e.versionName);
+ *         },
+ *         onError: function(e) {
+ *             alert("Package doesn’t exist");
+ *         }
+ *     });
+ * 
+ * @method getPackageVersion
+ * @param {String} packageName
+ * @param {Function} onSuccess
+ * @param {Function} onError
+ * @since 0.1
+ */
+System.android.getPackageVersion ({
+    packageName: null,
+    onSuccess: function(versionName){},
+    onError: function(error){}
+});
+
+/**
+ * TODO: type definition
+ * 
+ *     @example
+ *     System.validateFingerPrint({
+ *         onSuccess: function() {
+ *             alert("valid finger");
+ *         },
+ *         onError: function() {
+ *             alert("invalid finger");
+ *         }
+ *     });
+ * @method validateFingerPrint
+ * @param {Function} onSuccess
+ * @param {Function} onError
+ * @since 0.1
+ */
+System.validateFingerPrint ({
+    onSuccess: function(){},
+    onError: function(){}
+});
+
+/**
+ * TODO: type definition
+ * @method vibrate
+ * @param {Number} milliseconds
+ * @since 0.1
+ */
+System.vibrate = function(milliseconds) {};
+
+/**
+ * TODO: type definition
+ * @method isApplicationInstalled
+ * @param {String} packageName | schemaName
+ * @since 0.1
+ */
+System.isApplicationInstalled = function(packageName) {};
+
+/**
+ * TODO: type definition
+ * 
+ * @since 0.1
+ * @event onAccelerate
+ * @param {Number} x
+ * @param {Number} y
+ * @param {Number} z
+ */
+System.onAccelerate = function onAccelerate(x, y, z){ }
+
+module.exports = System;
