a98fda10af9a4cab75b9635f1a21583282b67a61
Dogan Ekici <smartface556@users.smartfacecloud.io>
2017-02-01 08:14:12 +0000

    Add Hardware

---
+++ device/hardware/hardware-iOS.js
@@ -0,0 +1,27 @@
+function Hardware() {}
+
+Object.defineProperty(Hardware, 'UID', {
+  value: UIDevice.currentDevice().UUID,  
+  writable: false,
+  enumerable: true
+});
+
+Object.defineProperty(Hardware, 'IMEI', {
+  value: -1,  
+  writable: false,
+  enumerable: true
+});
+
+Object.defineProperty(Hardware, 'brandModel', {
+  value: UIDevice.currentDevice().model,  
+  writable: false,
+  enumerable: true
+});
+
+Object.defineProperty(Hardware, 'brandName', {
+  value: UIDevice.currentDevice().name,  
+  writable: false,
+  enumerable: true
+});
+
+module.exports = Hardware;
