795c7016b58c412645c4f5a4d9aadc99efdeaada
Eray Tuncer <eray.tuncer@smartface.io>
2017-02-01 10:40:48 +0000

    [AND-2591] implement Device.Hardware.

---
+++ device/hardware/hardware-Android.js
@@ -0,0 +1,47 @@
+const NativeContext  = requireClass('android.content.Context');
+const NativeSettings = requireClass('android.provider.Settings');
+const NativeBuild    = requireClass('android.os.Build');
+
+const Hardware = {};
+
+Object.defineProperty(Hardware, 'IMEI', {
+    get: function () {
+        var activity = Android.getActivity();
+        var telephonyManager = activity.getSystemService(NativeContext.TELEPHONY_SERVICE);
+        return telephonyManager.getDeviceId();
+    },
+    configurable: false
+});
+
+Object.defineProperty(Hardware, 'UID', {
+    get: function () {
+        var activity = Android.getActivity();
+        var contentResolver = activity.getContentResolver();
+
+        return NativeSettings.Secure.getString(contentResolver, NativeSettings.Secure.ANDROID_ID);
+    },
+    configurable: false
+});
+
+Object.defineProperty(Hardware, 'brandName', {
+    get: function () {
+        return NativeBuild.BRAND;
+    },
+    configurable: false
+});
+
+Object.defineProperty(Hardware, 'brandModel', {
+    get: function () {
+        return NativeBuild.MODEL;
+    },
+    configurable: false
+});
+
+Object.defineProperty(Hardware, 'vendorID', {
+    get: function () {
+        return NativeBuild.SERIAL;
+    },
+    configurable: false
+});
+
+module.exports = Hardware;
