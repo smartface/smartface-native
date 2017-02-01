0fc6fd9e0e74733d4718f2628fc85d17a00dc58e
smartface495 <smartface495@users.smartfacecloud.io>
2017-02-01 06:51:41 +0000

    [AND-2591] implement Device.Screen.

---
+++ device/screen/screen-Android.js
@@ -0,0 +1,74 @@
+const UnitConverter = require('nf-core/util/Android/unitconverter');
+const Image         = require('nf-core/ui/image');
+
+const NativeContext        = requireClass('android.content.Context');
+const NativeBitmap         = requireClass('android.graphics.Bitmap');
+const NativeBitmapDrawable = requireClass('android.graphics.drawable.BitmapDrawable');
+const NativeR              = requireClass('android.R');
+
+const Screen = {};
+
+Object.defineProperty(Screen, 'dpi', {
+    get: function () {
+        var activity = Android.getActivity();
+        var metrics = activity.getResources().getDisplayMetrics();
+        return metrics.densityDpi;
+    },
+    configurable: false
+});
+
+Object.defineProperty(Screen, 'height', {
+    get: function () {
+        var activity = Android.getActivity();
+        var metrics = activity.getResources().getDisplayMetrics();
+        return UnitConverter.pixelToDp(activity, metrics.heightPixels);
+    },
+    configurable: false
+});
+
+Object.defineProperty(Screen, 'width', {
+    get: function () {
+        var activity = Android.getActivity();
+        var metrics = activity.getResources().getDisplayMetrics();
+        return UnitConverter.pixelToDp(activity, metrics.widthPixels);
+    },
+    configurable: false
+});
+
+Object.defineProperty(Screen, 'touchSupported', {
+    get: function () {
+        var activity = Android.getActivity();
+        var packageManager = activity.getPackageManager();
+        return packageManager.hasSystemFeature("android.hardware.touchscreen");
+    },
+    configurable: false
+});
+
+Object.defineProperty(Screen, 'orientation', {
+    get: function () {
+        var activity = Android.getActivity();
+        var windowManager = activity.getSystemService(NativeContext.WINDOW_SERVICE);
+        var display = windowManager.getDefaultDisplay();
+        
+        return display.getRotation();
+    },
+    configurable: false
+});
+
+Screen.capture = function() {
+    var activity = Android.getActivity();
+    var resources = activity.getResources();
+    
+    var content = NativeR.id.content;
+    var rootView = activity.findViewById(content).getRootView();
+    rootView.setDrawingCacheEnabled(true);
+    var cachedBitmap = rootView.getDrawingCache();
+    var bitmap = NativeBitmap.createBitmap(cachedBitmap);
+    rootView.setDrawingCacheEnabled(false);
+
+    var image = new Image();
+    image.nativeObject = new NativeBitmapDrawable(resources, bitmap);
+    return image;
+};
+
+module.exports = Screen;
