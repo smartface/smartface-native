d7927d8057a8e5b6178c3e66e9a9d538a1ae8f6c
Metehan Toksoy <metehan.toksoy@smartface.io>
2017-02-03 08:30:54 +0000

    [COR-1193] Implement File for Android. Make changes in Path.

--- util/Android/androidconfig.js
+++ util/Android/androidconfig.js
@@ -3,5 +3,6 @@ const NativeBuildConfig = requireClass(Android.getActivity().getPackageName()+".
 function AndroidConfig() {}
 
 AndroidConfig.isEmulator = (NativeBuildConfig.FLAVOR.toLowerCase().indexOf("emulator") !== -1);
+AndroidConfig.packageName = Android.getActivity().getPackageName();
 
 module.exports = AndroidConfig;
