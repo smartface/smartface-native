c20d1a777d09bfe7796a49130137b378733303c9
Dogan Ekici <smartface556@users.smartfacecloud.io>
2017-02-12 14:29:54 +0000

    [COR-1214] added scroll ios

--- ui/scrollview/analysis.js
+++ ui/scrollview/analysis.js
@@ -5,7 +5,20 @@ const ScrollView = extend(ViewGroup)(
     function (_super, params) {
         _super(this);
 
+        this.contentHeight = 0;
+        this.contentWidth = 0;
         
+        this.scrollX = 0;
+        this.scrollY = 0;
+        
+        this.showHorizontalScrollBar = false;
+        this.showVerticalScrollBar = false;
+        
+        // this.enableZoom = false;
+        this.declerationRate = 0.5;
+        
+        this.onScrollBegin = function(){};
+        this.onScrollEnd = function(){};
     }
 );
 
--- ui/scrollview/scrollview-iOS.js
+++ ui/scrollview/scrollview-iOS.js
@@ -6,12 +6,90 @@ const ScrollView = extend(ViewGroup)(
         var self = this;
         
         if(!self.nativeObject){
-            //self.nativeObject = new SMFUITableView();
+            self.nativeObject = new SMFUIScrollView();
         }
     
         _super(this);
         
-         if (params) {
+        Object.defineProperty(self, 'contentWidth', {
+            get: function() {
+                return self.nativeObject.contentSize.width;
+            },
+            set: function(value) {
+                self.nativeObject.contentSize = { width : value, height : self.contentHeight};
+            },
+            enumerable: true
+         });
+         
+        Object.defineProperty(self, 'contentHeight', {
+            get: function() {
+                return self.nativeObject.contentSize.height;
+            },
+            set: function(value) {
+                self.nativeObject.contentSize = { width : self.contentWidth, height : value};
+            },
+            enumerable: true
+         });
+         
+        Object.defineProperty(self, 'showHorizontalScrollBar', {
+            get: function() {
+                return self.nativeObject.showsHorizontalScrollIndicator;
+            },
+            set: function(value) {
+                self.nativeObject.showsHorizontalScrollIndicator = value;
+            },
+            enumerable: true
+         });
+         
+        Object.defineProperty(self, 'showVerticalScrollBar', {
+            get: function() {
+                return self.nativeObject.showsVerticalScrollIndicator;
+            },
+            set: function(value) {
+                self.nativeObject.showsVerticalScrollIndicator = value;
+            },
+            enumerable: true
+         });
+         
+         Object.defineProperty(self, 'declerationRate', {
+            get: function() {
+                return self.nativeObject.decelerationRate;
+            },
+            set: function(value) {
+                self.nativeObject.decelerationRate = value;
+            },
+            enumerable: true
+         });
+         
+         Object.defineProperty(self, 'scrollX', {
+            get: function() {
+                return self.nativeObject.contentOffset.x;
+            },
+            set: function(value) {
+                self.nativeObject.setContentOffsetAnimated({x : value,y : self.nativeObject.contentOffset.y},true);
+            },
+            enumerable: true
+         });
+         
+         Object.defineProperty(self, 'scrollY', {
+            get: function() {
+                return self.nativeObject.contentOffset.y;
+            },
+            set: function(value) {
+                self.nativeObject.setContentOffsetAnimated({x : self.nativeObject.contentOffset.x,y : value},true);
+            },
+            enumerable: true
+         });
+          
+        self.nativeObject.onScrollBegin = function(){
+              self.onScrollBegin();
+         }
+          
+        self.nativeObject.onScrollEnd = function(){
+              self.onScrollEnd();
+         }
+         
+        if (params) {
             for (var param in params) {
                 this[param] = params[param];
             }
