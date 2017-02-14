e7818f546fb81ab0a0a7f6fe3f59387b04c17f56
Eray Tuncer <eray.tuncer@smartface.io>
2017-02-14 10:11:02 +0300

    [COR-1214] refactor jsduck comments.

--- ui/scrollview/scrollview-position.js
+++ ui/scrollview/scrollview-position.js
@@ -1,21 +1,54 @@
+/**
+* @enum UI.ScrollView.Position
+* @static
+* @readonly
+* @since 0.1
+*
+* Indicates where to scroll.
+*
+*/
 const ScrollViewPosition = {};
 
 Object.defineProperties(ScrollViewPosition, {
+   /**
+    * @property {String} [LEFT = 'left'] 
+    * @static
+    * @readonly
+    * @since 0.1
+    */
     'LEFT': {
         value: 'left',
         configurable: false,
         enumerable: true
-    }, 
+    },
+   /**
+    * @property {String} [TOP = 'top'] 
+    * @static
+    * @readonly
+    * @since 0.1
+    */
     'TOP': {
         value: 'top',
         configurable:false,
         enumerable: true
-    }, 
+    },
+   /**
+    * @property {String} [RIGHT = 'right'] 
+    * @static
+    * @readonly
+    * @since 0.1
+    */ 
     'RIGHT': {
         value: 'right',
         configurable:false,
         enumerable: true
-    }, 
+    },
+   /**
+    * @property {String} [BOTTOM = 'bottom'] 
+    * @static
+    * @readonly
+    * @since 0.1
+    */
     'BOTTOM': {
         value: 'bottom',
         configurable:false,
