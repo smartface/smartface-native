d10d94336f86615c90273268c554392091fe8106
Dogan Ekici <smartface556@users.smartfacecloud.io>
2017-02-15 14:31:17 +0000

    chane property

--- ui/datepicker/datepicker-iOS.js
+++ ui/datepicker/datepicker-iOS.js
@@ -6,8 +6,8 @@ function DatePicker (params) {
         self.calendar = NSCalendar.currentCalendar();
     }
     
-    self.onDateSelectedListener = function(date){
-        self.onDateSelected(date);
+    self.onDateSelectedListener = function(e){
+        self.onDateSelected(e.date);
     }
     
     self.nativeObject.onSelected = self.onDateSelectedListener;
