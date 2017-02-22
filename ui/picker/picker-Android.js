f6c7234071db4be181ae2e924ec11c9bb3ee5f0f
smartface10661 <smartface10661@users.smartfacecloud.io>
2017-02-22 12:32:16 +0000

    [AND-2627] Implement Picker class for android

---
+++ ui/picker/picker-Android.js
@@ -0,0 +1,105 @@
+const extend = require('js-base/core/extend');
+const View = require('nf-core/ui/view');
+const Pages = require('nf-core/ui/pages');
+
+const NativeNumberPicker = requireClass("android.widget.NumberPicker");
+
+const Picker = extend(View)(
+    function (_super, params) {
+        var self = this;
+        if(!self.nativeObject) {
+            var page = Pages.currentPage;
+            var pageNativeObject = page.nativeObject;
+            var fragmentActivity = pageNativeObject.getActivity();
+            self.nativeObject = new NativeNumberPicker(fragmentActivity);
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+        _super(this);
+        
+        var _items = [];
+        Object.defineProperty(this, 'items', {
+            get: function() {
+                return _items;  // todo: Returns self.nativeObject.getDisplayValues()
+            },                  // after string problem is solved.
+            set: function(items) {
+                _items = items;
+                setNumberPicker();
+            },
+            enumerable: true
+        });
+        
+        Object.defineProperty(this, 'valueIndex', {
+            get: function() {
+                return self.nativeObject.getValue();
+            },
+            set: function(valueIndex) {
+                self.nativeObject.setValue(valueIndex);
+            },
+            enumerable: true
+        });
+        
+        Object.defineProperty(this, 'enabled', {
+            get: function() {
+                return self.nativeObject.isEnabled();
+            },
+            set: function(enabled) {
+                self.nativeObject.setEnabled(enabled);
+            },
+            enumerable: true
+        });
+        
+        var _onSelectedCallback;
+        Object.defineProperty(this, 'onSelected', {
+            get: function() {
+                return _onSelectedCallback;
+            },
+            set: function(onSelected) {
+                _onSelectedCallback = onSelected;
+            },
+            enumerable: true
+        });
+        
+        var _scrollState = -1;
+        
+        function setNumberPicker() {
+            if(_items.length > 0) {
+                self.nativeObject.setDisplayedValues(null);
+                self.nativeObject.setMaxValue(_items.length -1);
+                self.nativeObject.setMinValue(0);
+                self.nativeObject.setDescendantFocusability(NativeNumberPicker.FOCUS_BLOCK_DESCENDANTS);
+                self.nativeObject.setDisplayedValues(_items);
+                self.nativeObject.setWrapSelectorWheel(true);
+            }
+        }
+                
+        self.nativeObject.setOnScrollListener(NativeNumberPicker.OnScrollListener.implement({
+            onScrollStateChange: function(picker, scrollState) {
+                _scrollState = scrollState;
+                if(scrollState == NativeNumberPicker.OnScrollListener.SCROLL_STATE_IDLE) {
+                    fireOnSelectedCallback();
+                }
+            }
+        }));
+                
+        self.nativeObject.setOnValueChangedListener(NativeNumberPicker.OnValueChangeListener.implement({
+            onValueChange: function(picker, oldVal, newVal) {
+                if(_scrollState == NativeNumberPicker.OnScrollListener.SCROLL_STATE_IDLE) {
+                    fireOnSelectedCallback();
+                }
+            }
+        }));
+        
+        function fireOnSelectedCallback() {
+            if(_onSelectedCallback) 
+                _onSelectedCallback(self.valueIndex);
+        }
+        
+        // Assign parameters given in constructor
+        if (params) {
+            for (var param in params) {
+                this[param] = params[param];
+            }
+        }
+    }
+);
+
+module.exports = Picker;
