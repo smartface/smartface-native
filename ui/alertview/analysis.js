22a00206c8dcec87636430560f32c6a5c58ec750
Ercan Baran <ercan.baran@smartface.io>
2017-02-23 23:21:09 +0300

    Android & ios tags added

--- ui/alertview/analysis.js
+++ ui/alertview/analysis.js
@@ -1,37 +1,39 @@
-/** 
- * @enum {Number} UI.AlertView.ButtonType 
+/**
+ * @enum {Number} UI.AlertView.ButtonType
  * @since 0.1
- * 
+ *
  * ButtonType is used to indicate the behaviors of buttons in UI.AlertView.
  * You can specify a button have negative, positive or neutral user experience.
  * According to operating system button appearance or positions may change.
- * 
+ *
  *     @example
  *     const AlertView = require('nf-core/ui/alertview');
- *     
+ *
  *     var myAlertView = new AlertView({
  *         title: "Alert Title",
  *         message: "Alert Message"
  *     });
  *     myAlertView.addButton({
- *         index: AlertView.ButtonType.NEUTRAL, 
- *         text: "Ignore", 
+ *         index: AlertView.ButtonType.NEUTRAL,
+ *         text: "Ignore",
  *     });
  *     myAlertView.addButton({
- *         index: AlertView.ButtonType.NEGATIVE, 
+ *         index: AlertView.ButtonType.NEGATIVE,
  *         text: "Cancel"
  *     });
  *     myAlertView.addButton({
- *         index: AlertView.ButtonType.POSITIVE, 
+ *         index: AlertView.ButtonType.POSITIVE,
  *         text: "Okay"
  *     });
- *     
+ *
  *     myAlertView.show();
  */
 var ButtonType = { };
 
 /**
  * @property {Number} POSITIVE
+ * @android
+ * @ios
  * @static
  * @readonly
  * @since 0.1
@@ -40,6 +42,8 @@ ButtonType.POSITIVE = 0;
 
 /**
  * @property {Number} NEUTRAL
+ * @android
+ * @ios
  * @static
  * @readonly
  * @since 0.1
@@ -48,6 +52,8 @@ ButtonType.NEUTRAL = 1;
 
 /**
  * @property {Number} NEGATIVE
+ * @android
+ * @ios
  * @static
  * @readonly
  * @since 0.1
@@ -58,110 +64,124 @@ ButtonType.NEGATIVE = 2;
  * @class UI.AlertView
  * @since 0.1
  *
- * AlertView is an alert box with buttons having custom behaviors. You can 
+ * AlertView is an alert box with buttons having custom behaviors. You can
  * use AlertView for informing user or asking for confirmations. AlertView
  * has buttons with callbacks that you can take action for each of them separately.
- * 
+ *
  *     @example
  *     const AlertView = require('nf-core/ui/alertview');
- *    
+ *
  *     var myAlertView = new AlertView({
  *         title: "Alert Title",
  *         message: "Alert Message"
  *     });
  *     myAlertView.addButton({
- *         index: AlertView.ButtonType.NEGATIVE, 
+ *         index: AlertView.ButtonType.NEGATIVE,
  *         text: "Cancel"
  *     });
  *     myAlertView.addButton({
- *         index: AlertView.ButtonType.POSITIVE, 
- *         text: "Okay", 
+ *         index: AlertView.ButtonType.POSITIVE,
+ *         text: "Okay",
  *         onClick: function() {
  *             console.log("Okay clicked.");
  *         }
  *     });
- *     
+ *
  *     myAlertView.show();
  */
 function AlertView () {
 
     /**
      * Gets/sets title of AlertView.
-     * 
-     * @property {String} title 
+     *
+     * @property {String} title
+     * @android
+     * @ios
      * @since 0.1
      */
     this.title = "";
-    
+
     /**
      * Gets/sets message of AlertView.
-     * 
-     * @property {String} message 
+     *
+     * @property {String} message
+     * @android
+     * @ios
      * @since 0.1
      */
     this.message = "";
-     
+
     /**
      * Gets showing status of AlertView. It is set to true if AlertView is
-     * currently displayed in screen, false otherwise.
-     * 
-     * @property {boolean} isShowing 
+     * currently displayed on screen, false otherwise.
+     *
+     * @property {boolean} isShowing
+     * @android
+     * @ios
      * @since 0.1
      * @readonly
      */
     this.isShowing = false; // read only
 
     /**
-     * Shows AlertView on screen with specified properties, isShowing property
+     * Shows AlertView on the screen with specified properties, isShowing property
      * set to true after this operation.
-     * 
+     *
      * @method show
+     * @android
+     * @ios
      * @since 0.1
      */
-    this.show = function() {};    
-    
+    this.show = function() {};
+
     /**
      * Dismisses the AlertView, isShowing property set to false after this
      * operation.
-     * 
+     *
      * @method dismiss
+     * @android
+     * @ios
      * @since 0.1
      */
     this.dismiss = function() {};
-    
+
     /**
-     * Allows you to add button to AlertView. You can add maximum 3 buttons 
+     * Allows you to add button to AlertView. You can add maximum 3 buttons
      * on Android platform, on iOS there is no limitation.
-     * 
+     *
      *     @example
      *     myAlertView.addButton({
-     *         index: AlertView.ButtonType.POSITIVE, 
-     *         text: "Okay", 
+     *         index: AlertView.ButtonType.POSITIVE,
+     *         text: "Okay",
      *         onClick: function() {
      *             console.log("Okay clicked.");
      *         }
      *     });
-     * 
+     *
      * @param {Object} params Object describing button properties
      * @param {UI.AlertView.ButtonType} params.index Button type, it is set to
      *                                      UI.AlertView.ButtonType.NEUTRAL as default
      * @param {String} params.text Button text
      * @param {Function} [params.onClick] Callback for button click action
      * @method addButton
+     * @android
+     * @ios
      * @since 0.1
      */
     this.addButton = function(params) {};
 
-    // events  
+    // events
     /**
      * Gets/sets dismiss callback function.
-     * 
+     *
      *     @example
      *     myAlertView.onDismiss = function(alertView) {
      *         console.log("Dismissed alert view with title: " + alertView.title);
      *     };
-     * 
+     *
      * @event onDismiss
+     * @android
+     * @ios
      * @param {UI.AlertView} alertView Dismissed AlertView object
      * @since 0.1
      */
