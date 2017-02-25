15cc69680b823302d2224b2806c3fef5043025d3
Eray Tuncer <eray.tuncer@smartface.io>
2017-02-25 10:26:43 +0000

    [COR-1231] write initial analysis of UI.Animator.

---
+++ ui/animator/analysis.js
@@ -0,0 +1,44 @@
+/**
+ * @class UI.Animator
+ * @since 0.1
+ *
+ *     @example
+ *     const Animator = require('nf-core/ui/animator');
+ *     const View = require('nf-core/ui/view');
+ *     const Flex = require('nf-core/ui/flexlayout');
+ * 
+ *     var myView = new View({
+ *         left:10, top:10, width:150, height:65,
+ *         positionType: Flex.PositionType.ABSOLUTE
+ *     });
+ *     myPage.layout.addChild(myView);
+ * 
+ *     Animator.animate(myPage.layout, 2, function() {
+ *         myView.right  = 10;
+ *         myView.bottom = 10;
+ *        
+ *     }).then(2, function() {
+ *         myView.width  = 150;
+ *         myView.height = 65;
+ *         myView.right  = 10;
+ *         myView.bottom = 10;
+ *     });
+ *
+ */
+function Animator() {
+    Object.defineProperty(this, 'then', {
+        value: function(duration, animFunction) {
+            doStuff();
+            return new Animator();
+        }
+    });
+};
+
+Object.defineProperty(Animator, 'animate', {
+   value: function(rootLayout, duration, animFunction) {
+       doStuff();
+       return new Animator();
+   }
+});
+
+module.exports = Animator;
