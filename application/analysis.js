d5f101c29e598ed8cba778042481148ac0ccc72e
metehan.toksoy <metehan.toksoy@smartface.io>
2017-02-27 14:18:33 +0000

    [COR-1236] Write analysis of Application #2

--- application/analysis.js
+++ application/analysis.js
@@ -44,7 +44,7 @@ Application.byteSent;
 /**
  * The specified release channel within project.json.
  * 
- * @property {Number} byteSent
+ * @property {String} currentReleaseChannel
  * @readonly
  * @android
  * @ios
@@ -56,7 +56,7 @@ Application.currentReleaseChannel;
 /**
  * The application name within project.json
  * 
- * @property {Number} byteSent
+ * @property {String} smartfaceAppName
  * @readonly
  * @android
  * @ios
@@ -68,7 +68,7 @@ Application.smartfaceAppName;
 /**
  * The application version within project.json
  * 
- * @property {Number} byteSent
+ * @property {Number} version
  * @readonly
  * @android
  * @ios
@@ -76,5 +76,107 @@ Application.smartfaceAppName;
  * @since 0.1
  */
 Application.version;
+
+/**
+ * Launches another application and passes data.
+ *
+ * @method call
+ * @param {String} uriScheme
+ * @param {JSON} data
+ * @readonly
+ * @android
+ * @ios
+ * @static
+ * @since 0.1
+ */
+Application.call = function(uriScheme, data) {};
+
+/**
+ * Exists the application.
+ *
+ * @method exit
+ * @readonly
+ * @android
+ * @ios
+ * @static
+ * @since 0.1
+ */
+Application.exit = function(){};
+
+/**
+ * Restarts the application.
+ *
+ * @method restart
+ * @readonly
+ * @android
+ * @ios
+ * @static
+ * @since 0.1
+ */
+Application.restart = function(){};
+
+/**
+ * Triggered when application is called by another application.
+ * 
+ * @since 0.1
+ * @event onApplicationCallReceived
+ * @param {Object} [e] 
+ * @param {JSON} [e.data] Data sent by application.
+ * @param {String} [e.eventType] This parameter is available only for Android. For iOS this always returns "call". 
+ * @param {e.result} [e.result] This parameter is available only for Android and when eventType is "callback". Returns Android Activity result code.
+ * @android
+ * @ios
+ * @static
+ * @since 0.1
+ */
+Application.onApplicationCallReceived = function(e){};
+
+/**
+ * Triggered before exiting application.
+ * 
+ * @since 0.1
+ * @event onExit
+ * @android
+ * @ios
+ * @static
+ * @since 0.1
+ */
+Application.onExit = function(){};
+
+/**
+ * Triggered when application is started.
+ * 
+ * @since 0.1
+ * @event onStart
+ * @android
+ * @ios
+ * @static
+ * @since 0.1
+ */
+Application.onStart = function(){};
+
+/**
+ * Triggered after application bring to front.
+ * 
+ * @since 0.1
+ * @event onMaximize
+ * @android
+ * @ios
+ * @static
+ * @since 0.1
+ */
+Application.onMaximize = function(){};
+
+/**
+ * Triggered after application bring to back.
+ * 
+ * @since 0.1
+ * @event onMinimize
+ * @android
+ * @ios
+ * @static
+ * @since 0.1
+ */
+Application.onMinimize = function(){};
 
 module.exports = Application;
