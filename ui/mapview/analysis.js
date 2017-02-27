dc01802d19cfb8d59b9403ba3d5923078abb444e
Ercan Baran <ercan.baran@smartface.io>
2017-02-27 20:18:44 +0300

    Analysis update

--- ui/mapview/analysis.js
+++ ui/mapview/analysis.js
@@ -10,7 +10,8 @@ const extend = require('js-base/core/extend');
  *     @example
  *     const MapView = require('nf-core/ui/mapview');
  *     var myMapView = new MapView({
- *         top:10, left:10, right:10, bottom:150,
+ *         flexGrow :1,
+           alignSelf : FlexLayout.AlignSelf.STRETCH,
  *         onCreate: function() {
  *             myMapView.centerLocation = {
  *                 latitude: 41.0209078,
