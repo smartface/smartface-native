dcf30d74cae60df9e68cb1eafe090012450de3fb
Eray Tuncer <eray.tuncer@smartface.io>
2017-02-28 14:49:05 +0300

    [cor-1239] update analysis.

--- global/share/analysis.js
+++ global/share/analysis.js
@@ -4,18 +4,25 @@
  * @since 0.1
  * 
  * Share allows sending a text, an image or a file over 
- * other apps on the device. 
+ * other apps on the device. Blacklist works for iOS only. 
  * 
  */
 const Share = {};
 
 Object.defineProperties(Share, {
    /**
+    * Shares a text.
+    * 
+    *     @example
+    *     const Share = require('nf-core/share');
+    *     Share.shareText("Hello from nf-core", myPage, [Share.ios.Twitter, Share.ios.Facebook]);
     *
     * @method shareText
     * @param {String} text
-    * @param {UI.Page} page    
+    * @param {UI.Page} page
     * @param {Array} [blacklist]
+    * @ios
+    * @android
     * @since 0.1
     */
     'shareText': {
@@ -24,11 +31,21 @@ Object.defineProperties(Share, {
         }
     },
    /**
+    * Shares an image.
+    *
+    *     @example
+    *     const Share = require('nf-core/share');
+    *     const Image = require('nf-core/ui/image');
+    *    
+    *     var image = Image.createFromFile('path to the image');
+    *     Share.shareImage(image, myPage);
     *
     * @method shareImage
     * @param {UI.Image} image
     * @param {UI.Page} page
     * @param {Array} [blacklist]
+    * @android
+    * @ios
     * @since 0.1
     */
     'shareImage': {
@@ -37,11 +54,21 @@ Object.defineProperties(Share, {
         }
     },
    /**
+    * Shares a file.
+    * 
+    *     @example
+    *     const Share = require('nf-core/share');
+    *     const File = require('nf-core/io/file');
+    *    
+    *     var file = new File({path: 'path to the file'});
+    *     Share.shareFile(file, myPage);
     *
     * @method shareFile
     * @param {IO.File} file
     * @param {UI.Page} page
     * @param {Array} [blacklist]
+    * @ios
+    * @android
     * @since 0.1
     */
     'shareFile': {
@@ -52,51 +79,66 @@ Object.defineProperties(Share, {
 });
 
 /**
+ * @property {String} AirDrop
+ * @static
+ * @readonly
+ * @ios
+ * @since 0.1
+ */
+Share.ios.AirDrop = "...";
+
+/**
  * @property {String} Facebook
  * @static
  * @readonly
+ * @ios
  * @since 0.1
  */
-Share.Facebook = "...";
+Share.ios.Facebook = "...";
 
 /**
  * @property {String} Twitter
  * @static
  * @readonly
+ * @ios
  * @since 0.1
  */
-Share.Twitter = "...";
+Share.ios.Twitter = "...";
 
 /**
  * @property {String} Flickr
  * @static
  * @readonly
+ * @ios
  * @since 0.1
  */
-Share.Flickr = "...";
+Share.ios.Flickr = "...";
 
 /**
  * @property {String} Message
  * @static
  * @readonly
+ * @ios
  * @since 0.1
  */
-Share.Message = "...";
+Share.ios.Message = "...";
 
 /**
  * @property {String} Mail
  * @static
  * @readonly
+ * @ios
  * @since 0.1
  */
-Share.Mail = "...";
+Share.ios.Mail = "...";
 
 /**
  * @property {String} Vimeo
  * @static
  * @readonly
+ * @ios
  * @since 0.1
  */
-Share.Vimeo = "...";
+Share.ios.Vimeo = "...";
 
 module.exports = Share;
