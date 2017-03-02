f8f3b9569b23edc162ff8d1135ceabf94d040381
smartface10661 <smartface10661@users.smartfacecloud.io>
2017-03-02 10:54:31 +0000

    [AND-2650] Implement Timer class. Edit the analysis

--- global/timer/timer-Android.js
+++ global/timer/timer-Android.js
@@ -2,56 +2,42 @@ const NativeHandler = requireClass("android.os.Handler");
 const NativeRunnable = requireClass("java.lang.Runnable");
 
 var handler = new NativeHandler();
-var timeOutTask = [];
-var intervalTask = [];
 
 const NUMBER = "number";
     
-function Timer() {}
+function Timer(params) {
+    if(params) {
+        var delay = 0;
+        if(typeof(params.delay) == NUMBER)
+            delay = params.delay;
+        var self = this;
+        self.nativeObject = NativeRunnable.implement({
+            run: function() {
+                params.task();
+                if(params.repeat) {
+                    handler.postDelayed(self.nativeObject, delay);
+                }
+            }
+        }); 
+        handler.postDelayed(self.nativeObject, delay);
+    }
+}
 
 Timer.setTimeout = function(params){
-    var delay = 0;
-    if(typeof(params.delay) == NUMBER)
-        delay = params.delay;
-    var task = NativeRunnable.implement({
-        run: function() {
-            params.task();
-        }
-    });
-    timeOutTask.push(task);
-    handler.postDelayed(task, delay);
-    return (timeOutTask.length - 1);
+    params.repeat = false;
+    return new Timer(params);
 };
-
 
 Timer.setInterval = function(params){
-    var delay = 0;
-    if(typeof(params.delay) == NUMBER)
-        delay = params.delay;
-        
-    var task = NativeRunnable.implement({
-        run: function() {
-            params.task();
-            handler.postDelayed(task, delay);
-        }
-    });
-    intervalTask.push(task);
-    handler.postDelayed(task, delay);
-    return (intervalTask.length - 1);
+    params.repeat = true;
+    return new Timer(params);
 };
 
-Timer.clearTimeout = function(id) {
-    if(id >= timeOutTask.length)
-        throw new Error("Not found task, that created Timer.setTimeout, given id.");
-    else
-        handler.removeCallbacks(timeOutTask[id], null);
-};
-
-Timer.clearInterval = function(id) {
-    if(id >= timeOutTask.length)
-        throw new Error("Not found task given id.");
-    else
-        handler.removeCallbacks(intervalTask[id], null);
+Timer.clearTimer = function(timer) {
+    if(timer && timer.nativeObject)
+        handler.removeCallbacks(timer.nativeObject, null);
+    else 
+        throw new Error("Not found given timer.");
 };
 
 module.exports = Timer;
