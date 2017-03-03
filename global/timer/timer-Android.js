45c82a05c40e789f669d6b4f96409eecb3cc95bf
M.Halit Karakis <halit.karakis@smartface.io>
2017-03-03 07:33:21 +0000

    Fix cancelling setInterval inside interval problem

--- global/timer/timer-Android.js
+++ global/timer/timer-Android.js
@@ -4,20 +4,26 @@ const NativeRunnable = requireClass("java.lang.Runnable");
 const NUMBER = "number";
     
 function Timer(params) {
-    if(params) {
-        var delay = 0;
-        if(typeof(params.delay) == NUMBER)
-            delay = params.delay;
-        var self = this;
-        self.nativeObject = NativeRunnable.implement({
-            run: function() {
-                params.task();
-                if(params.repeat) {
-                    Timer.handler.postDelayed(self.nativeObject, delay);
-                }
-            }
+    this.repeat = params.repeat;
+
+    if (params) {
+        this.task = params.task;
+        this.delay = 0;
+        if(typeof(params.delay) === NUMBER) {
+            this.delay = params.delay;
+        }
+
+        this.nativeObject = NativeRunnable.implement({
+            run: runnableTask.bind(this)
         }); 
-        Timer.handler.postDelayed(self.nativeObject, delay);
+        Timer.handler.postDelayed(this.nativeObject, this.delay);
+    }
+}
+
+function runnableTask() {
+    this.task();
+    if (this.repeat) {
+        Timer.handler.postDelayed(this.nativeObject, this.delay);
     }
 }
 
@@ -34,10 +40,12 @@ Timer.setInterval = function(params){
 };
 
 Timer.clearTimer = function(timer) {
-    if(timer && timer.nativeObject)
+    if(timer && timer.nativeObject) {
+        timer.repeat = false;
         Timer.handler.removeCallbacks(timer.nativeObject, null);
-    else 
+    } else {
         throw new Error("Not found given timer.");
+    }
 };
 
 Timer.clearAllTimer = function() {
