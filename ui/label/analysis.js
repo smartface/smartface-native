1cc738cb334b0977c8d93252b8f5d382ff43fd97
Metehan Toksoy <metehan.toksoy@smartface.io>
2017-05-04 14:43:43 +0000

    [COR-1343] Write analysis of selectable property of Label

--- ui/label/analysis.js
+++ ui/label/analysis.js
@@ -70,6 +70,17 @@ const Label = extend(View)(
          * @since 0.1
          */
         this.multiline = false;
+        
+        /**
+         * Enables/disables selectable status of a Label. If set to true
+         * the text inside the Label will be selectable.
+         *
+         * @property {Boolean} [selectable = false]
+         * @android
+         * @ios
+         * @since 0.1
+         */
+        this.selectable = false;
 
         /**
          * Gets/sets text on Label.
