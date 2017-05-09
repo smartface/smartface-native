69b7941541d987a994e3e953f8970d7226f6f931
Metehan Toksoy <metehan.toksoy@smartface.io>
2017-05-09 06:06:39 +0000

    [COR-1334] Write analysis of onViewAdded and onViewRemoved

--- ui/viewgroup/analysis.js
+++ ui/viewgroup/analysis.js
@@ -14,84 +14,101 @@
  *     });
  *     myFlexLayout.addChild(myLabel);
  */
-const ViewGroup = extend(View)(
-    function (_super, params) {
-        _super(this);
+function ViewGroup(params) {
 
-        /**
-         * This function adds a child view to a viewgroup.
-         *
-         * @param {UI.View} view The child view to add.
-         * @android
-         * @ios
-         * @method addChild
-         * @since 0.1
-         */
-        this.addChild = function(view){};
+    /**
+     * This function adds a child view to a viewgroup.
+     *
+     * @param {UI.View} view The child view to add.
+     * @android
+     * @ios
+     * @method addChild
+     * @since 0.1
+     */
+    this.addChild = function(view){};
 
-        /**
-         * Remove a child view from viewgroup.
-         *
-         * @param {UI.View} view The child view to remove.
-         * @android
-         * @ios
-         * @method removeChild
-         * @since 0.1
-         */
-        this.removeChild = function(view){};
+    /**
+     * Remove a child view from viewgroup.
+     *
+     * @param {UI.View} view The child view to remove.
+     * @android
+     * @ios
+     * @method removeChild
+     * @since 0.1
+     */
+    this.removeChild = function(view){};
 
-        /**
-         * Removes all child views from viewgroup.
-         *
-         * @method removeAll
-         * @android
-         * @ios
-         * @since 0.1
-         */
-        this.removeAll = function(){};
+    /**
+     * Removes all child views from viewgroup.
+     *
+     * @method removeAll
+     * @android
+     * @ios
+     * @since 0.1
+     */
+    this.removeAll = function(){};
 
-        /**
-         * Gets the count of children in a viewgroup.
-         *
-         * @returns {Number} The number of children in the layout, or 0 if there is no child exists within the layout.
-         * @method getChildCount
-         * @android
-         * @ios
-         * @since 0.1
-         */
-        this.getChildCount = function(){};
+    /**
+     * Gets the count of children in a viewgroup.
+     *
+     * @returns {Number} The number of children in the layout, or 0 if there is no child exists within the layout.
+     * @method getChildCount
+     * @android
+     * @ios
+     * @since 0.1
+     */
+    this.getChildCount = function(){};
 
-        /**
-         * Finds a child view with specified id within the layout.
-         *
-         *     @example
-         *     const FlexLayout = require('sf-core/ui/flexlayout');
-         *     const Label = require('sf-core/ui/label');
-         *     var myFlexLayout = new FlexLayout();
-         *     var myLabel = new Label({
-         *          text: "Smartface Label",
-         *          id: 11235
-         *     });
-         *     myFlexLayout.addChild(myLabel);
-         *     var childView = myFlexLayout.findChildById(11235);
-         *
-         * @param {Number} id The specified id of the view.
-         * @returns {UI.View} Founded view within the layout, or null if view does not exists within the layout.
-         * @method findChildById
-         * @android
-         * @ios
-         * @since 0.1
-         */
-        this.findChildById = function(id){};
+    /**
+     * Finds a child view with specified id within the layout.
+     *
+     *     @example
+     *     const FlexLayout = require('sf-core/ui/flexlayout');
+     *     const Label = require('sf-core/ui/label');
+     *     var myFlexLayout = new FlexLayout();
+     *     var myLabel = new Label({
+     *          text: "Smartface Label",
+     *          id: 11235
+     *     });
+     *     myFlexLayout.addChild(myLabel);
+     *     var childView = myFlexLayout.findChildById(11235);
+     *
+     * @param {Number} id The specified id of the view.
+     * @returns {UI.View} Founded view within the layout, or null if view does not exists within the layout.
+     * @method findChildById
+     * @android
+     * @ios
+     * @since 0.1
+     */
+    this.findChildById = function(id){};
+    
+    /**
+     * This event is called when a view added to this view's hierarchy.
+     *
+     * @event onViewAdded
+     * @android
+     * @ios
+     * @since 1.1.8
+     */
+    this.onViewAdded = function() {};
+    
+    /**
+     * This event is called when a view removed to this view's hierarchy.
+     *
+     * @event onViewRemoved
+     * @android
+     * @ios
+     * @since 1.1.8
+     */
+    this.onViewRemoved = function() {};
 
 
-        // Assign parameters given in constructor
-        if (params) {
-            for (var param in params) {
-                this[param] = params[param];
-            }
+    // Assign parameters given in constructor
+    if (params) {
+        for (var param in params) {
+            this[param] = params[param];
         }
     }
-);
+};
 
 module.exports = ViewGroup;
