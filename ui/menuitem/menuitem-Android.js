491d219c3f7fe04bef04a5aff4edd172801f463f
smartface495 <smartface495@users.smartfacecloud.io>
2017-05-11 10:30:44 +0000

    [AND-2819] add titleColor to menuItem.

--- ui/menuitem/menuitem-Android.js
+++ ui/menuitem/menuitem-Android.js
@@ -1,5 +1,13 @@
+const NativeSpannable = requireClass("android.text.Spanned");
+const NativeColorSpan = requireClass("android.text.style.ForegroundColorSpan");
+const NativeSpannableStringBuilder = requireClass("android.text.SpannableStringBuilder");
+
 function MenuItem(params) {
+    this.android = {};
+    this.android.titleSpanned;
+
     var _title;
+    var _titleColor;
     
     Object.defineProperties(this, {
         'title': {
@@ -8,6 +16,7 @@ function MenuItem(params) {
             },
             set: function(title) {
                _title = title;
+               this.android.titleSpanned = spanTitle(_title, _titleColor);
             },
             enumerable: true
         },
@@ -26,6 +35,21 @@ function MenuItem(params) {
         }
     });
     
+    Object.defineProperties(this.android, {
+        'titleColor': {
+            get: function() {
+                return _titleColor;
+            },
+            set: function(color) {
+                if (color) {
+                    _titleColor = color;
+                    this.titleSpanned = spanTitle(_title, _titleColor);
+                }
+            },
+            enumerable: true
+        }
+    });
+    
     // Assign parameters given in constructor
     if (params) {
         for (var param in params) {
@@ -33,6 +57,17 @@ function MenuItem(params) {
         }
     }
 }
+
+function spanTitle(_title, _titleColor) {
+    if (_title && _titleColor) {
+        var colorSpan = new NativeColorSpan(_titleColor); // TODO: refactor according to AND-2685
+        
+        var spannableStringBuilder = new NativeSpannableStringBuilder(_title);
+        spannableStringBuilder.setSpan(colorSpan, 0, _title.length, NativeSpannable.SPAN_INCLUSIVE_INCLUSIVE);
+        return spannableStringBuilder;
+    }
+    return null;
+};
 
 MenuItem.ios = {
     Style: {
