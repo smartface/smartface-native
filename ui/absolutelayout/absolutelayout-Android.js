574ca63851d2b32d85cd0be7970cb242d13b0d57
Hasret Sariyer <hasret.sariyer@smartface.io>
2017-05-15 11:31:56 +0000

    [AND-2835] Fixed addChild exception of AbsoluteLayout

--- ui/absolutelayout/absolutelayout-Android.js
+++ ui/absolutelayout/absolutelayout-Android.js
@@ -1,18 +1,19 @@
 const extend     = require('js-base/core/extend');
 const FlexLayout = require('../flexlayout');
+var superAddChild;
 
 const AbsoluteLayout = extend(FlexLayout)(
     function (_super, params) {
         _super(this);
-
-        var superAddChild = this.addChild;
+        superAddChild = this.addChild; 
         
         Object.defineProperties(this, {
             'addChild': {
                 value: function(view){
                     superAddChild(view);
                     view.positionType = FlexLayout.PositionType.ABSOLUTE;
-                }
+                }, 
+                configurable: true
             },
             'toString': {
                 value: function(){
