9fb4525047098eede7fd97c2bd25ef1372b334af
Metehan Toksoy <metehan.toksoy@smartface.io>
2017-05-26 11:05:17 +0000

    Fixed http example

--- net/http/analysis.js
+++ net/http/analysis.js
@@ -4,17 +4,29 @@
  * http module allows sending http requests.
  * 
  *     @example
- *     const http = require("sf-core/net/http");
- *     var myImageUrl = your-image-url;
- *     var request = http.requestImage(myImageUrl, onLoad, onError);
- * 
- *     function onLoad(response) {
- *         var image = response;
- *     }
- * 
- *     function onError(error) {
- *         alert(error);
- *     }
+ *     Http.request(
+ *         {
+ *             'url':'YOUR_URL_HERE',
+ *             'headers': {
+ *                 // YOUR_HEADER_HERE',
+ *             },
+ *             'method':'HTTP_METHOD_HERE',
+ *             'body': 'YOUR_BODY_HERE',
+ *         },
+ *         function(data){
+ *             // Handle response
+ *         
+ *         },
+ *         function(e){
+ *             // Handle error like:
+ *             if(e.statusCode === 500){
+ *                 console.log("Internal Server Error Occurred.");
+ *             }
+ *             else{
+ *                 console.log("Server responsed with: " + e.statusCode + ". Message is: " + e.message);
+ *             }
+ *         }
+ *     );
  */
 var http = {};
 
@@ -96,29 +108,6 @@ http.requestJSON = function(url, onLoad, onError) {}
  * @method request
  * 
  * Sends an http request defined with parameters.
- * 
- *     @example
- *     const http = require("sf-core/net/http");
- *     var myHeaders = {
- *         "Content-Type": "text/plain;charset=UTF-8"
- *     }
- * 
- *     var params = {
- *         url: your-url,
- *         body: your-body,
- *         method: "POST",
- *         headers: myHeaders
- *     }
- *     
- *     http.request(params, onLoad, onError);
- *     
- *     function onLoad(response) {
- *         var body = response.body;
- *         var headers = params.headers;
- *     }
- *     function onError(error) {
- *         alert(error);
- *     }
  * 
  * @param {Object} params Parameters
  * @param {String} params.url URL
