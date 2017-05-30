92022e42483a2b1ed0233ed7ec524c8ed9258683
Eray Tuncer <eray.tuncer@smartface.io>
2017-05-30 10:46:48 +0300

    make analysis auto-complete compatible.

--- ui/floatingmenu/analysis.js
+++ ui/floatingmenu/analysis.js
@@ -43,164 +43,162 @@
  *     page.layout.addChild(floatingMenu);
  * 
  */
-function FloatingMenu(params) {
-    /**
-     * Gets/sets array of UI.FloatingMenu.Item of the menu.
-     *
-     * @property {Array<UI.FloatingMenu.Item>} items
-     * @android
-     * @ios
-     * @since 1.1.10
-     */
-    this.items = [];
-    
-     /**
-     * Gets/sets icon of the button
-     *
-     * @property {UI.Image} icon
-     * @android
-     * @ios
-     * @since 1.1.10
-     */
-    this.icon;
+function FloatingMenu(params) {};
 
-    /**
-     * Gets/sets the menu button's rotate behaviour on click.
-     *
-     * @property {Boolean} [rotateEnabled=true]
-     * @android
-     * @ios
-     * @since 1.1.10
-     */
-    this.rotateEnabled = true;
+/**
+ * Gets/sets array of UI.FloatingMenu.Item of the menu.
+ *
+ * @property {Array<UI.FloatingMenu.Item>} items
+ * @android
+ * @ios
+ * @since 1.1.10
+ */
+FloatingMenu.prototype.items = [];
     
-    /**
-     * Gets/sets color of the button
-     *
-     * @property {UI.Color} color
-     * @android
-     * @ios
-     * @since 1.1.10
-     */
-    this.color;
+/**
+ * Gets/sets icon of the button
+ *
+ * @property {UI.Image} icon
+ * @android
+ * @ios
+ * @since 1.1.10
+ */
+FloatingMenu.prototype.icon;
+
+/**
+ * Gets/sets the menu button's rotate behaviour on click.
+ *
+ * @property {Boolean} [rotateEnabled=true]
+ * @android
+ * @ios
+ * @since 1.1.10
+ */
+FloatingMenu.prototype.rotateEnabled = true;
     
-    /**
-     * Gets/sets visible of the FloatingMenu
-     *
-     * @property {Boolean} visible
-     * @android
-     * @ios
-     * @since 1.1.10
-     */
-     this.visible;
+/**
+ * Gets/sets color of the button
+ *
+ * @property {UI.Color} color
+ * @android
+ * @ios
+ * @since 1.1.10
+ */
+FloatingMenu.prototype.color;
     
-    /**
-     * Gets/sets click event callback function. This function is fired when items array is empty.
-     *
-     * @since 1.1.10
-     * @event onClick
-     * @android
-     * @ios
-     */
-     this.onClick = function(e) {};
+/**
+ * Gets/sets visible of the FloatingMenu
+ *
+ * @property {Boolean} visible
+ * @android
+ * @ios
+ * @since 1.1.10
+ */
+FloatingMenu.prototype.visible;
+    
+/**
+ * Gets/sets click event callback function. This function is fired when items array is empty.
+ *
+ * @since 1.1.10
+ * @event onClick
+ * @android
+ * @ios
+ */
+FloatingMenu.prototype.onClick = function() {};
      
-     /**
-     * Gets/sets open event callback.
-     *
-     * @since 1.1.10
-     * @event onOpen
-     * @android
-     * @ios
-     */
-     this.onOpen = function(e) {};
+/**
+ * Gets/sets open event callback.
+ *
+ * @since 1.1.10
+ * @event onOpen
+ * @android
+ * @ios
+ */
+FloatingMenu.prototype.onOpen = function() {};
      
-     /**
-     * Gets/sets onClose event callback.
-     *
-     * @since 1.1.10
-     * @event onClose
-     * @android
-     * @ios
-     */
-     this.onClose = function(e) {};
+/**
+ * Gets/sets onClose event callback.
+ *
+ * @since 1.1.10
+ * @event onClose
+ * @android
+ * @ios
+ */
+FloatingMenu.prototype.onClose = function() {};
      
-     /**
-     * Opens the menu.
-     *
-     * @since 1.1.10
-     * @method open
-     * @android
-     * @ios
-     */
-     this.open = function() {};
+/**
+ * Opens the menu.
+ *
+ * @since 1.1.10
+ * @method open
+ * @android
+ * @ios
+ */
+FloatingMenu.prototype.open = function() {};
      
-     /**
-     * Closes the menu.
-     *
-     * @since 1.1.10
-     * @method close
-     * @android
-     * @ios
-     */
-     this.close = function() {};
-}
+/**
+ * Closes the menu.
+ *
+ * @since 1.1.10
+ * @method close
+ * @android
+ * @ios
+ */
+FloatingMenu.prototype.close = function() {};
 
 /**
  * @class UI.FloatingMenu.Item
  * @since 1.1.10
+ */ 
+FloatingMenu.Item = function() {};
+
+/**
+ * Gets/sets title.
+ *
+ * @property {String} title
+ * @android
+ * @ios
+ * @since 1.1.10
  */
- 
-FloatingMenu.Item = function() {
+FloatingMenu.Item.prototype.title;
 
-        /**
-         * Gets/sets title.
-         *
-         * @property {String} title
-         * @android
-         * @ios
-         * @since 1.1.10
-         */
-        this.title;
-
-        /**
-         * Gets/sets icon.
-         *
-         * @property {UI.Image} icon
-         * @android
-         * @ios
-         * @since 1.1.10
-         */
-        this.icon;
+/**
+ * Gets/sets icon.
+ *
+ * @property {UI.Image} icon
+ * @android
+ * @ios
+ * @since 1.1.10
+ */
+FloatingMenu.Item.prototype.icon;
         
-         /**
-         * Gets/sets color of the item button.
-         *
-         * @property {UI.Color} color
-         * @android
-         * @ios
-         * @since 1.1.10
-         */
-        this.color;
+/**
+ * Gets/sets color of the item button.
+ *
+ * @property {UI.Color} color
+ * @android
+ * @ios
+ * @since 1.1.10
+ */
+FloatingMenu.Item.prototype.color;
 
-        /**
-         * Gets/sets the click callback function. This event will be fired when the item is touched.
-         *
-         * @event onClick
-         * @android
-         * @ios
-         * @since 1.1.10
-         */
-        this.onClick;
+/**
+ * Gets/sets the click callback function. This event will be fired when the item is touched.
+ *
+ * @event onClick
+ * @android
+ * @ios
+ * @since 1.1.10
+ */
+FloatingMenu.Item.prototype.onClick = function() {};
         
-        /**
-         * Gets/sets color of the title.
-         *
-         * @property {UI.Color} titleColor
-         * @android
-         * @ios
-         * @since 1.1.10
-         */
-        this.titleColor;
-};
+/**
+ * Gets/sets color of the title.
+ *
+ * @property {UI.Color} titleColor
+ * @android
+ * @ios
+ * @since 1.1.10
+ */
+FloatingMenu.Item.prototype.titleColor;
 
 module.exports = FloatingMenu;
