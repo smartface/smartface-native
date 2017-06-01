391f6fc9b19afe7ae121e40ac315b0d0672f87b4
Eray Tuncer <eray.tuncer@smartface.io>
2017-06-01 13:08:20 +0000

    [AND-2860] implement android textarea.

---
+++ ui/textarea/textarea-Android.js
@@ -0,0 +1,57 @@
+const extend = require('js-base/core/extend');
+
+const TextBox = require("../textbox");
+const TextAlignment = require('sf-core/ui/textalignment');
+
+const TextArea = extend(TextBox)(
+    function (_super, params) {
+        var self = this;
+        _super(this);
+
+        self.nativeObject.setSingleLine(false);
+        self.textAlignment = TextAlignment.TOPLEFT;
+
+        Object.defineProperties(self, {
+            'isPassword': {
+                get: function(){},
+                set: function(){}
+            },
+            'onActionButtonPress': {
+                get: function(){},
+                set: function(){}
+            },
+            'actionKeyType': {
+                get: function(){},
+                set: function(){},
+            },
+            'keyboardType': {
+                get: function(){},
+                set: function(){},
+            },
+            'hint': {
+                get: function(){},
+                set: function(){}
+            }
+        });
+
+        Object.defineProperties(self.android, {
+            'hint': {
+                get: function() {
+                    return self.nativeObject.getHint();
+                },
+                set: function(hint) {
+                    self.nativeObject.setHint(hint);
+                },
+            },
+        });
+
+        // Assign parameters given in constructor
+        if (params) {
+            for (var param in params) {
+                this[param] = params[param];
+            }
+        }
+    }
+);
+
+module.exports = TextArea;
