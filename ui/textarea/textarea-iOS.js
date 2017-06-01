7bd4cb32f5229c015e6888e0e90b1b4b176ae1d6
Dogan Ekici <doganekici26@gmail.com>
2017-06-01 12:42:41 +0000

    [IOS-2227] scrollbar default

--- ui/textarea/textarea-iOS.js
+++ ui/textarea/textarea-iOS.js
@@ -37,7 +37,7 @@ const TextArea = extend(TextBox)(
             },
             enumerable: true
         });
-        self.showScrollBar = false;
+        self.ios.showScrollBar = false;
         
         Object.defineProperty(self, 'hint', {
             get: function() {},
