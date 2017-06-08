05ca04ff1ba9d41ca14ee4d91e1ae19a3bed8756
Eray Tuncer <eray.tuncer@smartface.io>
2017-06-08 06:43:48 +0000

    [AND-2776] remove unnecessary if checks.

--- util/Android/unitconverter.js
+++ util/Android/unitconverter.js
@@ -5,20 +5,12 @@ AndroidUnitConverter.density = AndroidUnitConverter.displayMetrics.density;
 
 AndroidUnitConverter.pixelToDp = function(pixel) {
     if(isNaN(pixel) || typeof pixel !== "number") return Number.NaN;
-    if (pixel === 0) {
-        return 0;
-    } else {
-        return Math.round(pixel / AndroidUnitConverter.density);
-    }
+    return Math.round(pixel / AndroidUnitConverter.density);
 };
 
 AndroidUnitConverter.dpToPixel = function(dp) {
     if(isNaN(dp) || typeof dp !== "number") return Number.NaN;
-    if (dp === 0) {
-        return 0;
-    } else {
-        return Math.round(dp * AndroidUnitConverter.density);
-    }
+    return Math.round(dp * AndroidUnitConverter.density);
 };
 
 module.exports = AndroidUnitConverter;
