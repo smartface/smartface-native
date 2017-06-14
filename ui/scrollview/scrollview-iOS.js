4a18bd81c4e6f4b85969ec258a9f73596bc6792d
Dogan Ekici <doganekici26@gmail.com>
2017-06-14 07:54:31 +0000

    [IOS-2235] Scrollview layout property gradient color

--- ui/scrollview/scrollview-iOS.js
+++ ui/scrollview/scrollview-iOS.js
@@ -3,6 +3,7 @@ const extend = require('js-base/core/extend');
 const ScrollViewAlign = require("sf-core/ui/scrollview/scrollview-align");
 const ScrollViewEdge = require("sf-core/ui/scrollview/scrollview-edge");
 const FlexLayout = require('sf-core/ui/flexlayout');
+const Color = require('sf-core/ui/color');
 
 const ScrollType = {
     vertical : 0,
@@ -19,17 +20,53 @@ const ScrollView = extend(ViewGroup)(
             self.contentLayout.nativeObject.addFrameObserver();
             self.contentLayout.nativeObject.frameObserveHandler = function(e){
                 self.changeContentSize(e.frame);
+                if (typeof self.gradientColorFrameObserver === 'function') {
+                    self.gradientColorFrameObserver(e);
+                }
             }; 
             self.nativeObject.addSubview(self.contentLayout.nativeObject);
         }
         
         _super(this);
-
+    
         Object.defineProperty(self, 'layout', {
             get: function() {
                 return self.contentLayout;
             },
             enumerable: true
+        });
+        
+        Object.defineProperty(self.layout, 'backgroundColor', {
+            get: function() {
+                return new Color({color : self.layout.nativeObject.backgroundColor});
+            },
+            set: function(value) {
+                if (value.nativeObject.constructor.name === "CAGradientLayer"){
+                    if (!self.gradientColor){
+                        self.gradientColorFrameObserver = function(e){
+                            if (self.layout.nativeObject.frame.width === 0 || self.layout.nativeObject.frame.height === 0){
+                                return;
+                            }
+                            self.gradientColor.frame = e.frame;
+                            self.layout.nativeObject.backgroundColor = self.gradientColor.layerToColor();
+                        }
+                    }
+                    self.gradientColor = value.nativeObject;
+                    if (self.layout.nativeObject.frame.width === 0 || self.layout.nativeObject.frame.height === 0){
+                        return;
+                    }
+                    self.gradientColor.frame = self.layout.nativeObject.frame;
+                    self.layout.nativeObject.backgroundColor = self.gradientColor.layerToColor();
+                }else{
+                    if(self.gradientColor){
+                        self.gradientColorFrameObserver = undefined;
+                        self.gradientColor = undefined;
+                    }
+                    self.layout.nativeObject.backgroundColor = value.nativeObject;
+                }
+            },
+            enumerable: true,
+            configurable: true
         });
         
         Object.defineProperty(self, 'scrollBarEnabled', {
