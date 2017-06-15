194b238f5f4ee6005b0c11d16935894cc071d5c4
Hasret Sariyer <hasret.sariyer@smartface.io>
2017-06-15 12:56:13 +0000

    [AND-2903] Fixed twice creating page on bottomtabbar

--- ui/bottomtabbar/bottomtabbar-Android.js
+++ ui/bottomtabbar/bottomtabbar-Android.js
@@ -5,7 +5,7 @@ const MAXITEMCOUNT = 5;
 
 function BottomTabBar(params) {
         var _items = {};
-        var _itemInstances = null;
+        var _itemInstances = [];
         var _itemColors;
         var _index;
         var _itemCount = 0;
@@ -26,6 +26,13 @@ function BottomTabBar(params) {
                         _itemCount++;
                         if(!_index)
                             _index = path;
+                        var page;
+                        if(typeof(item.route) !== 'function') 
+                            page = item.route.getRoute(null, true);
+                        else 
+                            page = new _items[path].page();
+                            
+                        _itemInstances.push(page);
                     }
                     else {
                         throw new Error('Parameters of add method must be a string and a TabBarItem.');
@@ -108,9 +115,6 @@ function BottomTabBar(params) {
                 value: function(to){
                     if(!to) {
                         if(typeof(_items[_index].page) === 'function') {
-                            if(!_itemInstances) {  
-                               this.createItemInstances();
-                            }
                             var keys = Object.keys(_items);
                             var page = _itemInstances[keys.indexOf(_index)];
                             this.setPageProperties(page, _index);
@@ -134,9 +138,6 @@ function BottomTabBar(params) {
                         }
                         
                         if(_items[to] instanceof TabBarItem){
-                            if(!_itemInstances) {   
-                               this.createItemInstances();
-                            }
                             var keys = Object.keys(_items);
                             this.selectedIndex = keys.indexOf(to);
                             var page;
@@ -162,20 +163,6 @@ function BottomTabBar(params) {
                 get: function() {
                     return _tag;
                 }
-            },
-            'createItemInstances': {
-                value: function(){
-                    var keys = Object.keys(_items);
-                    _itemInstances = [];
-                    for(var i = 0; i < keys.length; i++) {
-                        var page = new _items[keys[i]].page();
-                        if(typeof(_items[keys[i]].route) === 'function') {
-                            page.isBottomTabBarPage = true;
-                        }
-                        _itemInstances.push(page);
-                    } 
-                },
-                enumerable: true
             },
             'toString': {
                 value: function(){
