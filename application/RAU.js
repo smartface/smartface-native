03219a1dc4097ca01f2999c00c1442136b5c6f6e
Eray Tuncer <eray.tuncer@smartface.io>
2017-07-05 15:23:58 +0000

    [COR-1432] implement sf-core RAU.

---
+++ application/RAU.js
@@ -0,0 +1,119 @@
+const HTTP = require("sf-core/net/http");
+const Path = require('sf-core/io/path');
+const File = require('sf-core/io/file');
+
+const RemoteUpdateService = {};
+RemoteUpdateService.firstUrl  = "";
+RemoteUpdateService.secondUrl = "";
+
+var zipPath = "";
+if (Device.deviceOS === "iOS") {
+    zipPath = Path.DataDirectory + "/stage/iOS.zip";
+} else if (Device.deviceOS === "Android") {
+    zipPath = Path.android.storages.internal + "/Android/data/AndroidRAU.zip";
+}
+
+RemoteUpdateService.checkUpdate = function(callback) {
+    HTTP.request(
+        {
+            'url': "https://portalapi.smartface.io/api/v1/rau/check",
+            'method':'POST',
+            'body': RAU.getRequestBody(),
+        },
+        function(response) {
+            if (response.statusCode === 200) { // Has update
+                var responseString = response.body.toString();
+                var responseJSON = JSON.parse(responseString);
+                RemoteUpdateService.firstUrl  = responseJSON["url"][0];
+                RemoteUpdateService.secondUrl = responseJSON["url"][1];
+                RAU.setUpdateResponse(responseString);
+                
+                callback(null, {
+                    meta: responseJSON.meta,
+                    newVersion: responseJSON.version,
+                    revision: responseJSON.revision,
+                    download: download
+                });
+            } else if (response.statusCode === 304) { // No update for Android
+                callback("No update", null);
+            } else {
+                callback("Unknown Response", null); // Unknown Response
+            }
+        },
+        function(error) {
+            if (error.statusCode === 304) { // No update for iOS
+                callback("No update", null);
+            } else if (error.statusCode === 404 || error.statusCode == 406 || error.statusCode == 400) {
+                var responseJSON = JSON.parse(error.body.toString());
+                callback(responseJSON.message, null);
+            } else {
+                callback("Unknown Error", null);
+            }
+        }
+    );
+};
+
+function download(callback) {
+    if (Device.deviceOS === "iOS") {
+        downloadZIP(callback);
+    } else if (Device.deviceOS === "Android") {
+        const Application = require("sf-core/application");
+        Application.android.onRequestPermissionsResult = function(e){
+            if(e.requestCode === 7174) {
+                if(e.result) {
+                    downloadZIP(callback);
+                } else {
+                    callback("Permission denied while RAU download", null);
+                }
+            }
+        };
+        Application.android.requestPermissions(7174, Application.android.Permissions.WRITE_EXTERNAL_STORAGE);
+    }
+};
+
+function downloadZIP(callback) {
+    HTTP.requestFile(
+        RemoteUpdateService.firstUrl,
+        zipPath, 
+        function(file) {
+            callback(null, {
+                updateAll: updateAll,
+                updateCancel: updateCancel
+            });
+        },
+        function(error) {
+            HTTP.requestFile(
+                RemoteUpdateService.secondUrl, 
+                zipPath, 
+                function(file) {
+                    callback(null, {
+                        updateAll: updateAll,
+                        updateCancel: updateCancel
+                    });
+                },
+                function(error) {
+                    callback("An error occured while downloding", null);
+                }
+            );
+        }
+    );
+};
+
+function updateAll(callback) {
+    RAU.updateAll();
+    callback(null, "success");
+}
+
+function updateCancel(callback) {
+    var zipFile = new File({
+        path: zipPath
+    });
+    if(zipFile.exists){
+        zipFile.remove();
+    }    
+
+    RemoteUpdateService.firstUrl  = "";
+    RemoteUpdateService.secondUrl = "";
+}
+
+module && (module.exports = RemoteUpdateService);
