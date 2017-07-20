e075fd0efb7a6a539ffadb877a034abda5db120e
Dogan Ekici <doganekici26@gmail.com>
2017-07-20 08:01:18 +0000

    Discard webview

--- ui/webview/webview-iOS.js
+++ ui/webview/webview-iOS.js
@@ -105,7 +105,6 @@ const WebView = extend(View)(
             enumerable: true
          });
          
-         var ScrollView = SF.requireClass("UIScrollView");
          /////////////////////
          // Temporary disabled
          Object.defineProperty(self, 'bounceEnabled', {
