19b2fdfdafffd3f09a2b544be5f409d4851c0374
Hasret Sariyer <hasret.sariyer@smartface.io>
2017-08-26 14:34:41 +0000

    Write unit test for status bar. Color assertion is failed

---
+++ tests/ui/statusbar.js
@@ -0,0 +1,24 @@
+var assert = require("chai").assert;
+
+const Page = require("../../ui/page");
+const Color = require("../../ui/color");
+
+const Creator = {page: new Page()};
+
+salep.test("sf-core/ui/page.statusBar Unit Test", function () {
+    this.case("StatusBar visible property", function () {
+        Creator.page.statusBar.visible = true;
+        assert.isTrue(Creator.page.statusBar.visible, "visible must be equal to true.");
+    });
+    
+    this.case("StatusBar height property.", function () { // read-only
+        Creator.page.statusBar.android.height = -10;
+        assert.notEqual(Creator.page.statusBar.height, -10, "height cannot be equal to -10.");
+    });
+    
+    this.case("StatusBar color property", function () {
+        Creator.page.statusBar.android.color = Color.BLUE;
+        console.log(Creator.page.statusBar.color);
+        assert.equal(int(Creator.page.statusBar.color.nativeObject), int(Color.BLUE.nativeObject), "color must be equal to Color.BLUE.");
+    });
+});
