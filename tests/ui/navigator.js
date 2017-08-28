cc3fc7ac1d7000455ce65149f297a2c366075136
Hasret Sariyer <hasret.sariyer@smartface.io>
2017-08-28 13:36:43 +0000

    Refactor

--- tests/ui/navigator.js
+++ tests/ui/navigator.js
@@ -5,16 +5,21 @@ const Page = require("../../ui/page");
 const extend = require('js-base/core/extend');
 
 salep.test("sf-core/ui/navigator Unit Test", function() {
+    var navigator;
+    this.case("Navigator constructor function", function() {
+        assert.doesNotThrow(function(){navigator = new Navigator();}, Error);
+    });
+    
     this.case("Navigator add function", function() {
         const page1 = extend(Page)(
             function (_super, params) {
                 _super(this);
             }
         );
-        assert.doesNotThrow(function(){Navigator.add("page1", page1)}, Error);
+        assert.doesNotThrow(function(){navigator.add("page1", page1)}, Error);
     });
     
     this.case("Navigator go function", function() {
-        assert.doesNotThrow(function(){Navigator.go('page1'); }, Error);
+        assert.doesNotThrow(function(){navigator.go('page1'); }, Error);
     });
 });
