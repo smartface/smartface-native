bd777020770e3a26b6adace563a43b629154f6fd
Dogan Ekici <doganekici26@gmail.com>
2018-04-13 12:20:28 +0000

    [COR-1611] Added ios

---
+++ ui/webbrowser/webbrowser-iOS.js
@@ -0,0 +1,40 @@
+const Invocation = require('sf-core/util').Invocation;
+const Options = require('sf-core/ui/webbrowser/webbrowseroptions');
+
+function WebBrowser () {}
+
+WebBrowser.open = function(page,options){
+    var nsURL = __SF_NSURL.URLWithString(options.url);
+    var argURL = new Invocation.Argument({
+        type:"NSObject",
+        value: nsURL
+    });
+
+    var safariViewController = __SF_SMFSFSafariViewController.create(nsURL,false);
+    
+    safariViewController.statusBarHidden = !options.ios.statusBarVisible;
+
+    var argBarColor = new Invocation.Argument({
+        type:"NSObject",
+        value: options.barColor.nativeObject
+    });
+    Invocation.invokeInstanceMethod(safariViewController,"setPreferredBarTintColor:",[argBarColor]);
+    
+    var argItemColor = new Invocation.Argument({
+        type:"NSObject",
+        value: options.ios.itemColor.nativeObject
+    });
+    Invocation.invokeInstanceMethod(safariViewController,"setPreferredControlTintColor:",[argItemColor]);
+    
+   var argPresentationStyle = new Invocation.Argument({
+        type:"NSInteger",
+        value: 7
+    });
+    Invocation.invokeInstanceMethod(safariViewController,"setModalPresentationStyle:",[argPresentationStyle]);
+    
+    page.nativeObject.presentViewController(safariViewController);
+};
+
+WebBrowser.Options = Options;
+
+module.exports = WebBrowser;
