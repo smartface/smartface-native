087d8eab33b020f3894ad3361d9d227eef4c0d93
muhammedyalcin <yalcincuru@gmail.com>
2018-04-24 07:47:58 +0000

    [COR-1627] Removed unnecessary statements

--- ui/page/page-Android.js
+++ ui/page/page-Android.js
@@ -791,7 +791,7 @@ function Page(params) {
                     NativeViewCompat.setZ(item.badge.nativeObject, 10);
                     NativeViewCompat.setZ(item.badge.nativeObject, 20);
 
-                    layoutParams.setMargins(AndroidUnitConverter.dpToPixel(-100), AndroidUnitConverter.dpToPixel(2), AndroidUnitConverter.dpToPixel(1), AndroidUnitConverter.dpToPixel(-50));
+                    layoutParams.setMargins(0, AndroidUnitConverter.dpToPixel(2), AndroidUnitConverter.dpToPixel(1), 0);
                     item.badge.layoutParams = layoutParams;
                     item.badge.nativeObject.setLayoutParams(item.badge.layoutParams);
 
