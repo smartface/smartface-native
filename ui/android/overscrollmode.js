e63de341a3b901d3cc36374fee9c25a34359f450
Hasret Sariyer <hasret.sariyer@smartface.io>
2018-06-27 08:36:10 +0000

    [AND-3221] Edit analysis

---
+++ ui/android/overscrollmode.js
@@ -0,0 +1,45 @@
+/**
+* @enum {Number} UI.Android.OverScrollMode
+* @static
+* @readonly
+ * @since 3.0.2
+*
+* Indicates the over-scroll mode for this search view. 
+*
+*/
+const OverScrollMode = {};
+
+/**
+ * Always allow a user to over-scroll this scroll view.
+ * 
+ * @property {Number} ALWAYS
+ * @android
+ * @static
+ * @readonly
+ * @since 3.0.2
+ */
+OverScrollMode.ALWAYS = 0;
+
+/**
+ * Allow a user to over-scroll this scroll view only if the content is large enough to meaningfully scroll.
+ * 
+ * @property {Number} AUTO
+ * @android
+ * @static
+ * @readonly
+ * @since 3.0.2
+ */
+OverScrollMode.AUTO = 1;
+
+/**
+ * Never allow a user to over-scroll this scroll view.
+ * 
+ * @property {Number} NEWER
+ * @android
+ * @static
+ * @readonly
+ * @since 3.0.2
+ */
+OverScrollMode.NEWER = 2;
+
+module.exports = OverScrollMode;
