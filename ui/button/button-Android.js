b1ca9a127d2010011bef201da24106b51aca715d
myalcinkuru <yalcin.kuru@smartface.io>
2018-07-11 09:06:04 +0000

    [AND-3245] textAlignment of button acts as label which we not expect

--- ui/button/button-Android.js
+++ ui/button/button-Android.js
@@ -1,28 +1,41 @@
 /*globals requireClass*/
-const AndroidConfig             = require("../../util/Android/androidconfig");
-const Label                     = require("../label");
-const Color                     = require("../color");
-const extend                    = require('js-base/core/extend');
-const TypeUtil                  = require("../../util/type");
+const TextAlignment = require("../textalignment");
 
-const NativeButton              = requireClass("android.widget.Button");
-const NativeView                = requireClass("android.view.View");
+const AndroidConfig = require("../../util/Android/androidconfig");
+const Label = require("../label");
+const Color = require("../color");
+const extend = require('js-base/core/extend');
+const TypeUtil = require("../../util/type");
+
+const NativeButton = requireClass("android.widget.Button");
+const NativeView = requireClass("android.view.View");
+
+const TextAlignmentDic = {};
+TextAlignmentDic[TextAlignment.TOPLEFT] = 48 | 3; // Gravity.TOP | Gravity.LEFT
+TextAlignmentDic[TextAlignment.TOPCENTER] = 48 | 1; //Gravity.TOP | Gravity.CENTER_HORIZONTAL
+TextAlignmentDic[TextAlignment.TOPRIGHT] = 48 | 5; //Gravity.TOP | Gravity.RIGHT
+TextAlignmentDic[TextAlignment.MIDLEFT] = 16 | 3; // Gravity.CENTER_VERTICAL | Gravity.LEFT
+TextAlignmentDic[TextAlignment.MIDCENTER] = 17; // Gravity.CENTER
+TextAlignmentDic[TextAlignment.MIDRIGHT] = 16 | 5; // Gravity.CENTER_VERTICAL | Gravity.RIGHT
+TextAlignmentDic[TextAlignment.BOTTOMLEFT] = 80 | 3; // Gravity.BOTTOM | Gravity.LEFT
+TextAlignmentDic[TextAlignment.BOTTOMCENTER] = 80 | 1; // Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL
+TextAlignmentDic[TextAlignment.BOTTOMRIGHT] = 80 | 5; // Gravity.BOTTOM | Gravity.RIGHT
 
 const Button = extend(Label)(
-    function (_super, params) {
-        if(!this.nativeObject){
+    function(_super, params) {
+        if (!this.nativeObject) {
             this.nativeObject = new NativeButton(AndroidConfig.activity);
         }
         _super(this);
-        
+
         // Default settings
-        if(!this.isNotSetDefaults){
+        if (!this.isNotSetDefaults) {
             this.nativeObject.setAllCaps(false); // enable lowercase texts
             this.backgroundColor = Color.create("#00A1F1"); // Smartface blue
             this.textColor = Color.WHITE;
             this.padding = 0;
         }
-        
+
         // Assign parameters given in constructor
         if (params) {
             for (var param in params) {
@@ -32,6 +45,24 @@ const Button = extend(Label)(
     },
     function(prop) {
         Object.defineProperties(prop, {
+            'textAlignment': {
+                get: function() {
+                    return this._textAlignment;
+                },
+                set: function(textAlignment) {
+                    console.log("textAlignment " + textAlignment);
+                    if (textAlignment in TextAlignmentDic) {
+                        console.log("textAlignment  if condition" + textAlignment);
+                        this._textAlignment = textAlignment;
+                    }
+                    else {
+                        console.log("textAlignment else condition" + textAlignment);
+                        this._textAlignment = this.viewNativeDefaultTextAlignment;
+                    }
+                    this.nativeObject.setGravity(TextAlignmentDic[this._textAlignment]);
+                },
+                enumerable: true
+            },
             'onPress': {
                 get: function() {
                     return this.__onPress;
@@ -53,10 +84,10 @@ const Button = extend(Label)(
                 enumerable: true
             },
             'toString': {
-                value: function(){
+                value: function() {
                     return 'Button';
                 },
-                enumerable: true, 
+                enumerable: true,
                 configurable: true
             }
         });
@@ -66,7 +97,7 @@ const Button = extend(Label)(
 function setOnClickListener(object) {
     object.nativeObject.setOnClickListener(NativeView.OnClickListener.implement({
         onClick: function(view) {
-                this.__onPress && this.__onPress();
+            this.__onPress && this.__onPress();
         }.bind(object)
     }));
     object.__didSetOnClickListener = true;
@@ -74,8 +105,8 @@ function setOnClickListener(object) {
 
 function setOnLongClickListener(object) {
     object.nativeObject.setOnLongClickListener(NativeView.OnLongClickListener.implement({
-        onLongClick : function(view){
-            if(this.__onLongPress) {
+        onLongClick: function(view) {
+            if (this.__onLongPress) {
                 this.__onLongPress();
             }
             return true; // Returns always true to solve AND-2713 bug.
