8113d3c672a6e05a7aa9c974a50f09761599ead6
myalcinkuru <yalcin.kuru@smartface.io>
2018-07-20 13:02:52 +0000

    [COR-1539] properties are added

--- ui/emailcomposer/emailcomposer-Android.js
+++ ui/emailcomposer/emailcomposer-Android.js
@@ -1,66 +1,90 @@
+const TypeUtil = require("../../util/type");
+
 const NativeIntent = requireClass("android.content.Intent");
 const NativeUri = requireClass("android.net.Uri");
 
+var _closeCallback;
+var self;
 function EmailComposer(params) {
 
-    const self = this;
-
-    const nativeIntent = new NativeIntent(NativeIntent.ACTION_VIEW);
-    nativeIntent.setData(NativeUri.parse("mailto:"));
-
+    self = this;
+    self.nativeObject = new NativeIntent(NativeIntent.ACTION_VIEW);
+    self.nativeObject.setData(NativeUri.parse("mailto:"));
 
 
-    var _CC, _BCC, _TO, _htmlText, _subject, _text, _title, _attachment;
     Object.defineProperties(self, {
         'setCC': {
             value: function(cc) {
-                if (typeof cc === "array")
-                    nativeIntent.putExtra(NativeIntent.EXTRA_CC, array(cc, "java.lang.String"));
+                if (typeof cc === "object")
+                    self.nativeObject.putExtra(NativeIntent.EXTRA_CC, array(cc, "java.lang.String"));
             }
         },
         'setBCC': {
             value: function(bcc) {
-                if (typeof bcc === "array")
-                    nativeIntent.putExtra(NativeIntent.EXTRA_BCC, array(bcc, "java.lang.String"));
+                if (typeof bcc === "object")
+                    self.nativeObject.putExtra(NativeIntent.EXTRA_BCC, array(bcc, "java.lang.String"));
             }
         },
         'setTO': {
             value: function(to) {
-                if (typeof to === "array")
-                    nativeIntent.putExtra(NativeIntent.EXTRA_EMAIL, array(to, "java.lang.String"));
+                if (typeof to === "object")
+                    self.nativeObject.putExtra(NativeIntent.EXTRA_EMAIL, array(to, "java.lang.String"));
             }
         },
         'setMessage': {
             value: function(text, isHtmlText) {
-                if (!isHtmlText && text.isString()) {
-                    nativeIntent.putExtra(NativeIntent.EXTRA_TEXT, text);
+                if (!isHtmlText && typeof text === "string") {
+                    self.nativeObject.putExtra(NativeIntent.EXTRA_TEXT, text);
                 }
                 else {
-
+                    self.nativeObject.putExtra(NativeIntent.EXTRA_HTML_TEXT, text);
                 }
             }
         },
         'setSubject': {
             value: function(subject) {
-                if (title.isString()) {
-                    nativeIntent.putExtra(NativeIntent.EXTRA_TITLE, title);
+                if (typeof subject === "string") {
+                    self.nativeObject.putExtra(NativeIntent.EXTRA_SUBJECT, subject);
                 }
             }
         },
-        'addAttachment': {
-            value: function(attachment) {
-                const File = require('sf-core/io/file');
-
-                if (attachment instanceof File) {
-                    let absulotePath = attachment.nativeObject.getAbsolutePath();
-                    nativeIntent.putExtra(NativeIntent.EXTRA_STREAM, NativeUri.parse("file://" + absulotePath));
+        'show': {
+            value: function(page) {
+                if (self.nativeObject && page) {
+                    page.nativeObject.startActivityForResult(self.nativeObject, EmailComposer.EMAIL_REQUESTCODE);
                 }
             }
         }
     });
 
+    Object.defineProperty(self, 'onClose', {
+        get: function() {
+            return _closeCallback;
+        },
+        set: function(callback) {
+            if (!TypeUtil.isFunction(callback))
+                return;
+            _closeCallback = callback;
+        },
+        enumerable: true
+    });
 
-    Object.keys(params || {}).forEach(function(key) {
-        this[key]
+
+    Object.defineProperty(self, 'addAttachmentForAndroid', {
+        value: function(attachment) {
+            const File = require('sf-core/io/file');
+            if (attachment instanceof File) {
+                let absulotePath = attachment.nativeObject.getAbsolutePath();
+                self.nativeObject.putExtra(NativeIntent.EXTRA_STREAM, NativeUri.parse("file://" + absulotePath));
+            }
+        }
     });
 }
+
+EmailComposer.EMAIL_REQUESTCODE = 57;
+EmailComposer.onActivityResult = function(requestCode, resultCode, data) {
+    _closeCallback && self.onClose();
+}
+
+
+module.exports = EmailComposer;
