c293ae92737c58643a63c9ec56f1fe3bb134a6a2
Hasret Sariyer <hasret.sariyer@smartface.io>
2018-07-25 09:01:07 +0000

    Implement thread

--- util/Android/asynctask.js
+++ util/Android/asynctask.js
@@ -1,6 +1,6 @@
 /*globals array,requireClass,release */
-const TypeUtil          = require("sf-core/util/type");
-const NativeAsynTask    = requireClass('android.os.AsyncTask');
+const TypeUtil = require("sf-core/util/type");
+const SFAsyncTask = requireClass('io.smartface.android.asynctask.SFAsyncTask');
 
 /**
  * This is JS implementtation of Android AsyncTask. 
@@ -11,83 +11,82 @@ const NativeAsynTask    = requireClass('android.os.AsyncTask');
  * params.onPostExecute: result
  * For more information: https://developer.android.com/reference/android/os/AsyncTask.html
  * 
- * NOTE: Not working due to COR-1281. 
  */
-function AsyncTask(params){
-    var asyncTask = NativeAsynTask.extend("SFAsyncTask", {
-        onPreExecute: function(){
+function AsyncTask(params) {
+    var callbacks = {
+        onPreExecute: function() {
             _onPreExecute && _onPreExecute();
         },
-        doInBackground: function(objects){
+        doInBackground: function(objects) {
             _doInBackground && _doInBackground(objects);
         },
-        onProgressUpdate: function(){
+        onProgressUpdate: function() {
             _onProgressUpdate && _onProgressUpdate();
         },
-        onPostExecute: function(){
+        onPostExecute: function() {
             _onPostExecute && _onPostExecute();
         }
-    }, null);
-    
+    };
+    var asyncTask = new SFAsyncTask();
+    asyncTask.setJsCallback(callbacks);
+
+
     var _onPreExecute;
     var _doInBackground;
     var _onProgressUpdate;
     var _onPostExecute;
-    Object.defineProperties(this,{
+    Object.defineProperties(this, {
         'onPreExecute': {
-            get: function(){
+            get: function() {
                 return _onPreExecute;
             },
-            set: function(value){
-                if(TypeUtil.isFunction(value)){
+            set: function(value) {
+                if (TypeUtil.isFunction(value)) {
                     _onPreExecute = value.bind(this);
                 }
             }
         },
         'doInBackground': {
-            get: function(){
+            get: function() {
                 return _doInBackground;
             },
-            set: function(value){
-                if(TypeUtil.isFunction(value)){
+            set: function(value) {
+                if (TypeUtil.isFunction(value)) {
                     _doInBackground = value.bind(this);
                 }
             }
         },
         'onProgressUpdate': {
-            get: function(){
+            get: function() {
                 return _onProgressUpdate;
             },
-            set: function(value){
-                if(TypeUtil.isFunction(value)){
+            set: function(value) {
+                if (TypeUtil.isFunction(value)) {
                     _onProgressUpdate = value.bind(this);
                 }
             }
         },
         'onPostExecute': {
-            get: function(){
+            get: function() {
                 return _onPostExecute;
             },
-            set: function(value){
-                if(TypeUtil.isFunction(value)){
+            set: function(value) {
+                if (TypeUtil.isFunction(value)) {
                     _onPostExecute = value.bind(this);
                 }
             }
         },
         'execute': {
-            value: function(params){
-                if(TypeUtil.isArray(params)){
-                    try{
-                        asyncTask.execute(params)
-                    }
-                    catch(e){
-                        Application.onUnhandledError(e);
-                    }
+            value: function(params) {
+                if (TypeUtil.isArray(params)) {
+                    asyncTask.executeTask(array(params, "java.lang.Object"));
+                } else {
+                    asyncTask.executeTask(null);
                 }
             }
         }
     });
-    
+
     // Assign parameters given in constructor
     if (params) {
         for (var param in params) {
