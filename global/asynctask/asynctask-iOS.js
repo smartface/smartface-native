c533b86d20d6386eefb2cb3e5e54ca79109d0e53
Hasret Sariyer <hasret.sariyer@smartface.io>
2018-08-05 18:15:47 +0000

    Fixed asynctask for both platform

--- global/asynctask/asynctask-iOS.js
+++ global/asynctask/asynctask-iOS.js
@@ -3,21 +3,10 @@ const TypeUtil          = require("sf-core/util/type");
 function AsyncTask(params){
     var self = this;
     
-    var _onPreExecute;
     var _task;
     var _onComplete;
     
     Object.defineProperties(self,{
-        'onPreExecute': {
-            get: function(){
-                return _onPreExecute;
-            },
-            set: function(value){
-                if(TypeUtil.isFunction(value)){
-                    _onPreExecute = value.bind(this);
-                }
-            }
-        },
         'task': {
             get: function(){
                 return _task;
@@ -41,8 +30,6 @@ function AsyncTask(params){
         'run': {
             value: function(){
                 try{
-                    self.onPreExecute();
-                    
                     // Background
                     SF.dispatch_async(SF.dispatch_get_global_queue(0,0), function() {
                         self.doInBackground();
