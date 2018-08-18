4a8eafcfb8c9d6b8e1bfa24b647a753223e8ddd8
myalcinkuru <yalcin.kuru@smartface.io>
2018-08-18 23:02:30 +0000

    [COR-1745] picker title is added

--- ui/picker/picker-Android.js
+++ ui/picker/picker-Android.js
@@ -1,32 +1,32 @@
 /*globals requireClass*/
-const extend        = require('js-base/core/extend');
-const View          = require('../view');
-const TypeUtil      = require('../../util/type');
+const extend = require('js-base/core/extend');
+const View = require('../view');
+const TypeUtil = require('../../util/type');
 const AndroidConfig = require("../../util/Android/androidconfig");
 
-const NativeNumberPicker    = requireClass("android.widget.NumberPicker");
-const NativeFrameLayout     = requireClass("android.widget.FrameLayout");
-const NativeAlertDialog     = requireClass("android.app.AlertDialog");
+const NativeNumberPicker = requireClass("android.widget.NumberPicker");
+const NativeFrameLayout = requireClass("android.widget.FrameLayout");
+const NativeAlertDialog = requireClass("android.app.AlertDialog");
 const NativeDialogInterface = requireClass("android.content.DialogInterface");
 
 const Picker = extend(View)(
-    function (_super, params) {
+    function(_super, params) {
         var self = this;
         const activity = AndroidConfig.activity;
-        if(!self.nativeObject) {
+        if (!self.nativeObject) {
             self.nativeObject = new NativeNumberPicker(activity);
         }
         _super(this);
-        
+
         this.ios = {};
-        
+
         var _items = [];
         var _onSelected;
-        Object.defineProperties(this,{
+        Object.defineProperties(this, {
             'items': {
                 get: function() {
-                    return _items;  // todo: Returns self.nativeObject.getDisplayValues()
-                },                  // after string problem is solved.
+                    return _items; // todo: Returns self.nativeObject.getDisplayValues()
+                }, // after string problem is solved.
                 set: function(items) {
                     _items = items;
                     setNumberPicker(this.nativeObject, _items);
@@ -52,26 +52,27 @@ const Picker = extend(View)(
                 enumerable: true
             },
             'show': {
-                value: function(done, cancel) {
+                value: function(done, cancel, titleText) {
                     var layout = addViewToLayout(this.nativeObject);
-                    
+
                     var cancelListener = NativeDialogInterface.OnClickListener.implement({
                         onClick: function(dialogInterface, i) {
-                            if(cancel)
+                            if (cancel)
                                 cancel();
                         }
                     });
-                    
+
                     var doneListener = NativeDialogInterface.OnClickListener.implement({
                         onClick: function(dialogInterface, i) {
-                            if(done)
-                                done({index: self.currentIndex});
+                            if (done)
+                                done({ index: self.currentIndex });
                         }
                     });
                     
                     const NativeRString = requireClass("android.R").string;
                     var builder = new NativeAlertDialog.Builder(AndroidConfig.activity);
                     builder = builder.setView(layout);
+                    builder.setTitle(titleText || "");
                     builder = builder.setNegativeButton(NativeRString.cancel, cancelListener);
                     builder = builder.setPositiveButton(NativeRString.ok, doneListener);
                     builder.show();
@@ -83,19 +84,19 @@ const Picker = extend(View)(
                 enumerable: true
             },
             'toString': {
-                value: function(){
+                value: function() {
                     return 'Picker';
                 },
-                enumerable: true, 
+                enumerable: true,
                 configurable: true
             }
         });
         Object.defineProperty(this.android, 'enabled', {
             get: function() {
-                return self.nativeObject.isEnabled();  
-            }, 
+                return self.nativeObject.isEnabled();
+            },
             set: function(value) {
-                if(!TypeUtil.isBoolean(value)){
+                if (!TypeUtil.isBoolean(value)) {
                     throw new TypeError("Value should be boolean for enabled.");
                 }
                 self.nativeObject.setEnabled(value);
@@ -103,17 +104,17 @@ const Picker = extend(View)(
             enumerable: true
         });
 
-        if(!this.skipDefaults){
+        if (!this.skipDefaults) {
             self.nativeObject.setOnScrollListener(NativeNumberPicker.OnScrollListener.implement({
                 onScrollStateChange: function(picker, scrollState) {
-                    if(scrollState === NativeNumberPicker.OnScrollListener.SCROLL_STATE_IDLE) {
-                        if(_onSelected)
+                    if (scrollState === NativeNumberPicker.OnScrollListener.SCROLL_STATE_IDLE) {
+                        if (_onSelected)
                             _onSelected(self.currentIndex);
                     }
                 }
             }));
         }
-        
+
         // Assign parameters given in constructor
         if (params) {
             for (var param in params) {
@@ -124,16 +125,16 @@ const Picker = extend(View)(
 );
 
 function setNumberPicker(nativeObject, _items) {
-    if(_items.length > 0) {
+    if (_items.length > 0) {
         nativeObject.setDisplayedValues(null);
-        nativeObject.setMaxValue(_items.length-1);
+        nativeObject.setMaxValue(_items.length - 1);
         nativeObject.setMinValue(0);
         nativeObject.setDescendantFocusability(NativeNumberPicker.FOCUS_BLOCK_DESCENDANTS);
         var items = [];
-        for(var item in _items) {
+        for (var item in _items) {
             items.push(_items[item]);
         }
-        
+
         nativeObject.setDisplayedValues(array(items, "java.lang.String"));
         nativeObject.setWrapSelectorWheel(false);
     }
@@ -142,12 +143,11 @@ function setNumberPicker(nativeObject, _items) {
 function addViewToLayout(nativeObject) {
     var layout = new NativeFrameLayout(AndroidConfig.activity);
     var parent = nativeObject.getParent();
-    if(parent) {
+    if (parent) {
         parent.removeView(nativeObject);
     }
-    layout.addView(nativeObject, new NativeFrameLayout.LayoutParams(
-        -1 , // FrameLayout.LayoutParams.MATCH_PARENT
-        -2 , // FrameLayout.LayoutParams.WRAP_CONTENT
+    layout.addView(nativeObject, new NativeFrameLayout.LayoutParams(-1, // FrameLayout.LayoutParams.MATCH_PARENT
+        -2, // FrameLayout.LayoutParams.WRAP_CONTENT
         17)); // Gravity.CENTER
     return layout;
 }
