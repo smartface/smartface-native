7d4957d1ff1bd55fc73ce1cbd6ac7740eca29cca
Dogan Ekici <doganekici26@gmail.com>
2018-08-31 07:03:21 +0000

    Picker improvements

--- ui/picker/picker-iOS.js
+++ ui/picker/picker-iOS.js
@@ -85,7 +85,106 @@ const Picker = extend(View)(
             enumerable: true
         });
         
-        self.show = function(ok,cancel,title){
+        var _title;
+        Object.defineProperty(this, 'title', {
+            get: function() {
+                return _title;
+            },
+            set: function(value) {
+                _title = value;
+            },
+            enumerable: true
+        });
+        
+        var _titleColor;
+        Object.defineProperty(this, 'titleColor', {
+            get: function() {
+                return _titleColor;
+            },
+            set: function(value) {
+                _titleColor = value;
+            },
+            enumerable: true
+        });
+        
+        var _titleFont;
+        Object.defineProperty(this, 'titleFont', {
+            get: function() {
+                return _titleFont;
+            },
+            set: function(value) {
+                _titleFont = value;
+            },
+            enumerable: true
+        });
+        
+        var _cancelColor;
+        Object.defineProperty(this, 'cancelColor', {
+            get: function() {
+                return _cancelColor;
+            },
+            set: function(value) {
+                _cancelColor = value;
+            },
+            enumerable: true
+        });
+        
+        var _cancelHighlightedColor;
+        Object.defineProperty(this, 'cancelHighlightedColor', {
+            get: function() {
+                return _cancelHighlightedColor;
+            },
+            set: function(value) {
+                _cancelHighlightedColor = value;
+            },
+            enumerable: true
+        });
+        
+        var _cancelFont;
+        Object.defineProperty(this, 'cancelFont', {
+            get: function() {
+                return _cancelFont;
+            },
+            set: function(value) {
+                _cancelFont = value;
+            },
+            enumerable: true
+        });
+        
+        var _okColor;
+        Object.defineProperty(this, 'okColor', {
+            get: function() {
+                return _okColor;
+            },
+            set: function(value) {
+                _okColor = value;
+            },
+            enumerable: true
+        });
+        
+        var _okHighlightedColor;
+        Object.defineProperty(this, 'okHighlightedColor', {
+            get: function() {
+                return _okHighlightedColor;
+            },
+            set: function(value) {
+                _okHighlightedColor = value;
+            },
+            enumerable: true
+        });
+        
+        var _okFont;
+        Object.defineProperty(this, 'okFont', {
+            get: function() {
+                return _okFont;
+            },
+            set: function(value) {
+                _okFont = value;
+            },
+            enumerable: true
+        });
+        
+        self.show = function(ok,cancel){
               var okFunc = function(e){
                 if (typeof ok === "function"){
                     ok({index : e.index});
@@ -97,11 +196,19 @@ const Picker = extend(View)(
                 }
               };
               
-              if (typeof title === "undefined") {
-                self.nativeObject.show(self.nativeObject,"",cancelFunc,okFunc);
-              } else {
-                self.nativeObject.show(self.nativeObject,title,cancelFunc,okFunc);
-              }
+              self.nativeObject.show(self.nativeObject,
+                (self.title === undefined) ? "" : self.title,
+                cancelFunc,
+                okFunc,
+                self.titleColor ? self.titleColor.nativeObject : undefined,
+                self.titleFont ? self.titleFont : undefined,
+                self.cancelColor ? self.cancelColor.nativeObject : undefined,
+                self.cancelHighlightedColor ? self.cancelHighlightedColor.nativeObject : undefined,
+                self.cancelFont ? self.cancelFont : undefined,
+                self.okColor ? self.okColor.nativeObject : undefined,
+                self.okHighlightedColor ? self.okHighlightedColor.nativeObject : undefined,
+                self.okFont ? self.okFont : undefined
+                );
         }
         // Assign parameters given in constructor
         if (params) {
