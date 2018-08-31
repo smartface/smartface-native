fde13543b3cefe4a305d3282916b4d884ff72097
Yalcin Kuru <yalcin.kuru@smartface.io>
2018-08-31 07:37:02 +0000

    [COR-1745] titleFont,color & positive and ok button font, color added

--- ui/picker/picker-Android.js
+++ ui/picker/picker-Android.js
@@ -4,6 +4,8 @@ const View = require('../view');
 const TypeUtil = require('../../util/type');
 const AndroidConfig = require("../../util/Android/androidconfig");
 
+const Color = require('sf-core/ui/color');
+
 const NativeNumberPicker = requireClass("android.widget.NumberPicker");
 const NativeFrameLayout = requireClass("android.widget.FrameLayout");
 const NativeAlertDialog = requireClass("android.app.AlertDialog");
@@ -22,6 +24,11 @@ const Picker = extend(View)(
 
         var _items = [];
         var _onSelected;
+        var _title = "";
+        var _titleColor = Color.BLACK;
+        var _titleFont;
+        var _okColor, _cancleColor, _okFont, _cancleFont;
+        var listenerEnable;
         Object.defineProperties(this, {
             'items': {
                 get: function() {
@@ -51,6 +58,84 @@ const Picker = extend(View)(
                 },
                 enumerable: true
             },
+            'title': {
+                get: function() {
+                    return _title;
+                },
+                set: function(text) {
+                    if (typeof text === "string")
+                        _title = text;
+                },
+                enumerable: true
+            },
+            'titleColor': {
+                get: function() {
+                    return _titleColor;
+
+                },
+                set: function(color) {
+                    if (color instanceof Color)
+                        _titleColor = color;
+                },
+                enumerable: true
+            },
+            'titleFont': {
+                get: function() {
+                    return _titleFont;
+                },
+                set: function(font) {
+                    const Font = require('sf-core/ui/font');
+                    if (font instanceof Font)
+                        _titleFont = font;
+                },
+                enumerable: true
+            },
+            'okColor': {
+                get: function() {
+                    return _okColor;
+                },
+                set: function(color) {
+                    listenerEnable = true;
+                    if (color instanceof Color)
+                        _okColor = color;
+                },
+                enumerable: true
+            },
+            'cancleColor': {
+                get: function() {
+                    return _cancleColor;
+                },
+                set: function(color) {
+                    listenerEnable = true;
+                    if (color instanceof Color)
+                        _cancleColor = color;
+                },
+                enumerable: true
+            },
+            'okFont': {
+                get: function() {
+                    return _okFont;
+                },
+                set: function(font) {
+                    listenerEnable = true;
+                    const Font = require('sf-core/ui/font');
+                    if (font instanceof Font)
+                        _okFont = font;
+                },
+                enumerable: true
+            },
+            'cancleFont': {
+                get: function() {
+                    return _cancleFont;
+                },
+                set: function(font) {
+                    listenerEnable = true;
+                    const Font = require('sf-core/ui/font');
+                    if (font instanceof Font)
+                        _cancleFont = font;
+                },
+                enumerable: true
+            },
             'show': {
                 value: function(done, cancel, titleText) {
                     var layout = addViewToLayout(this.nativeObject);
@@ -69,15 +154,28 @@ const Picker = extend(View)(
                         }
                     });
 
-
                     const NativeRString = requireClass("android.R").string;
                     var builder = new NativeAlertDialog.Builder(AndroidConfig.activity);
                     builder = builder.setView(layout);
                     builder = builder.setNegativeButton(NativeRString.cancel, cancelListener);
                     builder = builder.setPositiveButton(NativeRString.ok, doneListener);
                     if (typeof titleText === 'string')
-                        builder = builder.setCustomTitle(creatTitleView(titleText));
-                        
+                        builder = builder.setCustomTitle(creatTitleView(titleText).bind(self));
+
+                    if (listenerEnable === true) {
+                        builder.setOnShowListener(NativeDialogInterface.OnShowListener.implement({
+                            onShow: function(dialogInterface) {
+                                var negativeButton = builder.getButton(NativeAlertDialog.BUTTON_NEGATIVE);
+                                var positiveButton = builder.getButton(NativeAlertDialog.BUTTON_POSITIVE);
+
+                                self.cancleColor && negativeButton.setTextColor(self.cancleColor.nativeObject);
+                                self.okColor && positiveButton.setTextColor(self.okColor.nativeObject);
+                                self.okFont && negativeButton.setTypeface(self.okFont.nativeObject);
+                                self.cancleFont && positiveButton.setTypeface(self.cancleFont.nativeObject);
+                            }
+                        }));
+                    }
+
                     builder.show();
                 },
                 enumerable: true
@@ -124,8 +222,7 @@ const Picker = extend(View)(
                 this[param] = params[param];
             }
         }
-    }
-);
+    });
 
 function setNumberPicker(nativeObject, _items) {
     if (_items.length > 0) {
@@ -156,18 +253,21 @@ function addViewToLayout(nativeObject) {
 }
 
 function creatTitleView(title) {
+    const picker = this;
+
     const NativeTextView = requireClass("android.widget.TextView");
     const Color = require('sf-core/ui/color');
 
     const CENTER = 17;
 
     var titleTextView = new NativeTextView(AndroidConfig.activity);
-    titleTextView.setText(title);
-    titleTextView.setBackgroundColor(Color.WHITE.nativeObject);
-    titleTextView.setPadding(10, 15, 10, 10);
+    titleTextView.setText(picker.titleText);
+    titleTextView.setBackgroundColor(Color.TRANSPARENT.nativeObject);
+    titleTextView.setPadding(10, 20, 10, 10);
     titleTextView.setGravity(CENTER);
-    titleTextView.setTextColor(Color.BLACK.nativeObject);
-    titleTextView.setTextSize(20);
+    picker.titleTextColor && titleTextView.setTextColor(picker.titleTextColor.nativeObject);
+    // titleTextView.setTextSize(picker.titleFont);
+    picker.titleFont && titleTextView.setTypeface(picker.titleFont.nativeObject);
 
     return titleTextView;
 }
