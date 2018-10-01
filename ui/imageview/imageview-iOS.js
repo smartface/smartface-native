9c1a4b296e0045d975627b0f976028eb8feeb40d
Dogan Ekici <doganekici26@gmail.com>
2018-10-01 08:15:45 +0000

    [IOS-2641] Fixed ios

--- ui/imageview/imageview-iOS.js
+++ ui/imageview/imageview-iOS.js
@@ -80,13 +80,10 @@ const ImageView = extend(View)(
         }
         
         self.loadFromUrl = function(url,placeholder,fade){
-        	if (fade === false) {
-        		self.nativeObject.loadFromURL(__SF_NSURL.URLWithString(url),placeholder ? placeholder.nativeObject : undefined,undefined);
-        	}else{
-				self.nativeObject.loadFromURL(__SF_NSURL.URLWithString(url),placeholder ? placeholder.nativeObject : undefined,function(image,error,cache,url){
+            if (typeof url === "string") { //Depraceted: Use loadFromUrl(object);
+				self.nativeObject.loadFromURL(__SF_NSURL.URLWithString(url),placeholder ? placeholder.nativeObject : undefined,undefined,function(innerFade,image,error,cache,url){
 					if (!error) {
-						this.nativeObject.loadImage(image);
-						if (cache == ImageCacheType.NONE) {
+						if (cache == ImageCacheType.NONE && innerFade !== false) {
 							var alpha = this.nativeObject.alpha;
 							this.nativeObject.alpha = 0;
 			                __SF_UIView.animation(0.3,0,function(){
@@ -94,12 +91,35 @@ const ImageView = extend(View)(
 			                }.bind(this),function(){});
 						}
 					}
-				}.bind(self));
-        	}
+				}.bind(self,fade));
+            }else if (typeof url === "object"){
+		        self.nativeObject.loadFromURL(__SF_NSURL.URLWithString(url.url),url.placeholder ? url.placeholder.nativeObject : undefined,undefined,function(onSuccess,onError,innerFade,image,error,cache,url){
+    				if (!error) {
+    				    if (cache == ImageCacheType.NONE && innerFade !== false) {
+							var alpha = this.nativeObject.alpha;
+							this.nativeObject.alpha = 0;
+			                __SF_UIView.animation(0.3,0,function(){
+			                   this.nativeObject.alpha = alpha; 
+			                }.bind(this),function(){});
+						}
+    					if (typeof onSuccess === "function") {
+    					    __SF_Dispatch.mainAsync(function(innerIndex){
+    						    onSuccess();
+    					    });
+    					}
+    				}else{
+    					if (typeof onError === "function") {
+    					    __SF_Dispatch.mainAsync(function(innerIndex){
+    						    onError();
+    					    });
+    					}
+    				}
+    			}.bind(self,url.onSuccess,url.onError,url.fade));
+            }
         }
         
         self.fetchFromUrl = function(object){
-			self.nativeObject.loadFromURL(__SF_NSURL.URLWithString(object.url),object.placeholder ? object.placeholder.nativeObject : undefined,function(onSuccess,onError,image,error,cache,url){
+			self.nativeObject.loadFromURL(__SF_NSURL.URLWithString(object.url),object.placeholder ? object.placeholder.nativeObject : undefined,[SDWebImageOptions.SDWebImageAvoidAutoSetImage],function(onSuccess,onError,image,error,cache,url){
 				if (!error) {
 					if (typeof onSuccess === "function") {
 						onSuccess(Image.createFromImage(image),cache);
@@ -200,5 +220,112 @@ Object.defineProperties(ImageView.FillType.ios,{
         enumerable: true
     }
 });
+
+const SDWebImageOptions = {
+    /**
+     * By default, when a URL fail to be downloaded, the URL is blacklisted so the library won't keep trying.
+     * This flag disable this blacklisting.
+     */
+    SDWebImageRetryFailed : 1 << 0,
+
+    /**
+     * By default, image downloads are started during UI interactions, this flags disable this feature,
+     * leading to delayed download on UIScrollView deceleration for instance.
+     */
+    SDWebImageLowPriority : 1 << 1,
+
+    /**
+     * This flag disables on-disk caching after the download finished, only cache in memory
+     */
+    SDWebImageCacheMemoryOnly : 1 << 2,
+
+    /**
+     * This flag enables progressive download, the image is displayed progressively during download as a browser would do.
+     * By default, the image is only displayed once completely downloaded.
+     */
+    SDWebImageProgressiveDownload : 1 << 3,
+
+    /**
+     * Even if the image is cached, respect the HTTP response cache control, and refresh the image from remote location if needed.
+     * The disk caching will be handled by NSURLCache instead of SDWebImage leading to slight performance degradation.
+     * This option helps deal with images changing behind the same request URL, e.g. Facebook graph api profile pics.
+     * If a cached image is refreshed, the completion block is called once with the cached image and again with the final image.
+     *
+     * Use this flag only if you can't make your URLs static with embedded cache busting parameter.
+     */
+    SDWebImageRefreshCached : 1 << 4,
+
+    /**
+     * In iOS 4+, continue the download of the image if the app goes to background. This is achieved by asking the system for
+     * extra time in background to let the request finish. If the background task expires the operation will be cancelled.
+     */
+    SDWebImageContinueInBackground : 1 << 5,
+
+    /**
+     * Handles cookies stored in NSHTTPCookieStore by setting
+     * NSMutableURLRequest.HTTPShouldHandleCookies = YES;
+     */
+    SDWebImageHandleCookies : 1 << 6,
+
+    /**
+     * Enable to allow untrusted SSL certificates.
+     * Useful for testing purposes. Use with caution in production.
+     */
+    SDWebImageAllowInvalidSSLCertificates : 1 << 7,
+
+    /**
+     * By default, images are loaded in the order in which they were queued. This flag moves them to
+     * the front of the queue.
+     */
+    SDWebImageHighPriority : 1 << 8,
+    
+    /**
+     * By default, placeholder images are loaded while the image is loading. This flag will delay the loading
+     * of the placeholder image until after the image has finished loading.
+     */
+    SDWebImageDelayPlaceholder : 1 << 9,
+
+    /**
+     * We usually don't call transformDownloadedImage delegate method on animated images,
+     * as most transformation code would mangle it.
+     * Use this flag to transform them anyway.
+     */
+    SDWebImageTransformAnimatedImage : 1 << 10,
+    
+    /**
+     * By default, image is added to the imageView after download. But in some cases, we want to
+     * have the hand before setting the image (apply a filter or add it with cross-fade animation for instance)
+     * Use this flag if you want to manually set the image in the completion when success
+     */
+    SDWebImageAvoidAutoSetImage : 1 << 11,
+    
+    /**
+     * By default, images are decoded respecting their original size. On iOS, this flag will scale down the
+     * images to a size compatible with the constrained memory of devices.
+     * If `SDWebImageProgressiveDownload` flag is set the scale down is deactivated.
+     */
+    SDWebImageScaleDownLargeImages : 1 << 12,
+    
+    /**
+     * By default, we do not query disk data when the image is cached in memory. This mask can force to query disk data at the same time.
+     * This flag is recommend to be used with `SDWebImageQueryDiskSync` to ensure the image is loaded in the same runloop.
+     */
+    SDWebImageQueryDataWhenInMemory : 1 << 13,
+    
+    /**
+     * By default, we query the memory cache synchronously, disk cache asynchronously. This mask can force to query disk cache synchronously to ensure that image is loaded in the same runloop.
+     * This flag can avoid flashing during cell reuse if you disable memory cache or in some other cases.
+     */
+    SDWebImageQueryDiskSync : 1 << 14,
+    
+    /**
+     * By default, when the cache missed, the image is download from the network. This flag can prevent network to load from cache only.
+     */
+    SDWebImageFromCacheOnly : 1 << 15,
+    /**
+     * By default, when you use `SDWebImageTransition` to do some view transition after the image load finished, this transition is only applied for image download from the network. This mask can force to apply view transition for memory and disk cache as well.
+     */
+    SDWebImageForceTransition : 1 << 16
+};
 
 module.exports = ImageView;
