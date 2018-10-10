3bb16c5652f6bb967eb6f767f371c4a11de546d3
Hasret Sariyer <hasret.sariyer@smartface.io>
2018-10-10 13:36:01 +0000

    [AND-3334] Edit TopTabbar analysis

--- ui/toptabbar/analysis.js
+++ ui/toptabbar/analysis.js
@@ -8,6 +8,16 @@
 function TopTabBar(params){}
 
 /**
+ * Gets/sets the tab bar item array of the TopTabBar.
+ * @property {UI.TabBarItem[]} items
+ * @android
+ * @ios
+ * @since 3.2.0
+ */
+TopTabBar.prototype.items = [];
+
+/**
+ * TODO: Add methods of swipeview
  * Gets the swipe view of the TopTabBar.
  * @property {UI.SwipeView} swipeView
  * @android
@@ -17,28 +27,25 @@ function TopTabBar(params){}
 TopTabBar.prototype.swipeView = null;
 
 /**
- * Gets the divider color of the TopTabBar.
+ * Gets/sets the divider color of the TopTabBar.
  * @property {UI.Color} [dividerColor = UI.Color.BLACK]
  * @android
- * @ios
  * @since 3.2.0
  */
 TopTabBar.prototype.dividerColor = Color.BLACK;
 
 /**
- * Gets the divider padding of the TopTabBar.
+ * Gets/sets the divider padding of the TopTabBar.
  * @property {Number} [dividerPadding = 0]
  * @android
- * @ios
  * @since 3.2.0
  */
 TopTabBar.prototype.dividerPadding = 0;
 
 /**
- * Gets the divider width of the TopTabBar.
+ * Gets/sets the divider width of the TopTabBar.
  * @property {Number} [dividerWidth = 0]
  * @android
- * @ios
  * @since 3.2.0
  */
 TopTabBar.prototype.dividerWidth = 0;
@@ -80,13 +87,33 @@ TopTabBar.prototype.backgroundColor = Color.WHITE;
 TopTabBar.prototype.scrollEnabled = false;
 
 /**
- * Gets/sets the selected index of TopTabBar.
+ * Gets the selected index of TopTabBar.
  * @property {Number} selectedIndex
  * @android
  * @ios
  * @since 3.2.0
  */
 TopTabBar.prototype.selectedIndex;
+
+/**
+ * Sets the selected index of TopTabBar.
+ * @method setSelectedIndex
+ * @param {Number} index
+ * @param {Boolean} [animated=true]
+ * @android
+ * @ios
+ * @since 3.2.0
+ */
+TopTabBar.prototype.setSelectedIndex;
+
+/**
+ * Gets/sets the icon color of the tabs. You can specify text colors for the different states (normal, selected) used for the tabs.
+ * @property {UI.Color|Object} textColor
+ * @android
+ * @ios
+ * @since 3.2.0
+ */
+TopTabBar.prototype.iconColor;
 
 /**
  * Gets/sets the text color of the tabs. You can specify text colors for the different states (normal, selected) used for the tabs.
@@ -101,29 +128,9 @@ TopTabBar.prototype.textColor;
  * This event called when a tab is chosen by the user.
  *
  * @event onSelected
+ * @param index
  * @android
  * @ios
  * @since 3.2.0
  */
 TopTabBar.prototype.onSelected;
-
-
-/**
- * This event called when a tab exits the selected state.
- *
- * @event onUnselected
- * @android
- * @ios
- * @since 3.2.0
- */
-TopTabBar.prototype.onUnselected;
-
-/**
- * This event called when a tab that is already selected is chosen again by the user.
- *
- * @event onReselected
- * @android
- * @ios
- * @since 3.2.0
- */
-TopTabBar.prototype.onReselected;
