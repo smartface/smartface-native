bb517e901cce1eb7ab1ca4ff7327d8129c72e88b
Yalcin Kuru <yalcin.kuru@smartface.io>
2018-10-11 11:59:07 +0000

    [COR-1781]

--- ui/gifimage/gifimage-Android.js
+++ ui/gifimage/gifimage-Android.js
@@ -1,5 +1,141 @@
-function Gifimage(params) {
-    
+const NativeGifDrawable = requireClass("pl.droidsonroids.gif.GifDrawable");
+const NativeBitmap = requireClass("android.graphics.Bitmap");
+const NativeByteArrayOutputStream = requireClass("java.io.ByteArrayOutputStream");
+
+const File = require('../../io/file');
+const Blob = require('../../blob');
+const Image = require('sf-core/ui/image');
+
+const CompressFormat = [
+    NativeBitmap.CompressFormat.JPEG,
+    NativeBitmap.CompressFormat.PNG
+];
+const GifImage = function(params) {
+    const self = this;
+
+    if (params && params.drawable)
+        self.nativeObject = params.drawable;
+
+    // Assign parameters given in constructor
+    if (params) {
+        for (var param in params) {
+            this[param] = params[param];
+        }
+    }
+
+    Object.defineProperties(self, {
+        'loopCount': {
+            get: function() {
+                return self.nativeObject.getLoopCount();
+            },
+            set: function(count) {
+                if (typeof count !== "number")
+                    return;
+                self.nativeObject.setLoopCount(count)
+            },
+            enumerable: true
+        },
+        'frameCount': {
+            get: function() {
+                return self.nativeObject.getNumberOfFrames();
+            },
+            enumerable: true
+        },
+        'posterImage': {
+            get: function() {
+                var newBitmap = self.nativeObject.seekToFrameAndGet(0);
+                return (new Image({ bitmap: newBitmap }));
+            },
+            enumerable: true
+        },
+        'instrinsicSize': {
+            get: function() {
+                var instrinsicWidth = self.nativeObject.getIntrinsicWidth();
+                var instrinsicHeight = self.nativeObject.getIntrinsicHeight();
+                return { width: instrinsicWidth, height: instrinsicHeight };
+            },
+            enumerable: true
+        },
+        'toBlob': {
+            value: function() {
+                var bitmap = self.nativeObject.getBitmap();
+                var stream = new NativeByteArrayOutputStream();
+                bitmap.compress(CompressFormat[1], 100, stream);
+                return new Blob(stream.toByteArray(), { type: "image" });
+            },
+            enumerable: true
+        }
+    });
+
+    self.android = {};
+    var _seekPosition, _speed, _seekFrame;
+    Object.defineProperties(self.android, {
+        'reset': {
+            value: function() {
+                self.nativeObject.reset();
+            },
+            enumerable: true
+        },
+        'seekTo': {
+            get: function() {
+                return _seekPosition;
+            },
+            set: function(seekPosition) {
+                if (typeof seekPosition !== "number")
+                    return;
+                _seekPosition = seekPosition
+                self.nativeObject.seekTo(seekPosition);
+            },
+            enumerable: true
+        },
+        'seekToFrame': {
+            get: function() {
+                return _seekFrame;
+            },
+            set: function(seekFrame) {
+                if (typeof _seekFrame !== "number")
+                    return;
+                _seekFrame = seekFrame
+                self.nativeObject.seekToFrame(_seekFrame);
+            },
+            enumerable: true
+        },
+        'speed': {
+            get: function() {
+                return _speed;
+            },
+            set: function(speed) {
+                if (typeof speed !== "number")
+                    return;
+                _speed = speed;
+                self.nativeObject.setSpeed(_speed);
+            },
+            enumerable: true
+        }
+    });
+
+    self.ios = {};
+    self.ios.getDelayTimesForIndexes = function() {}
+};
+
+
+GifImage.createFromFile = function(pathOrFile) {
+    if (pathOrFile instanceof File) {
+        return new GifImage({ drawable: GifImage(new NativeGifDrawable(pathOrFile)) });
+    }
+    else {
+        var imageFile = new File({ path: pathOrFile });
+        if (imageFile && imageFile.nativeObject) {
+            return new GifImage({ drawable: GifImage(new NativeGifDrawable(imageFile)) });
+        }
+    }
+    return null;
+}
+GifImage.createFromBlob = function(blob) {
+    var byteArray = blob.nativeObject.toByteArray();
+    if (byteArray)
+        return new GifImage({ drawable: GifImage(new NativeGifDrawable(byteArray)) });
+    return null;
 }
 
-module.exports = Gifimage;
+module.exports = GifImage;
---
+++ ui/gifimageview/gifimageview-Android.js
@@ -0,0 +1,76 @@
+const extend = require('js-base/core/extend');
+const ImageView = require("sf-core/ui/imageview");
+const GifImage = require("sf-core/ui/gifimage");
+
+const GifImageView = extend(ImageView)(
+    function(_super, params) {
+        _super(this);
+
+        const self = this;
+
+        // Assign parameters given in constructor
+        if (params) {
+            for (var param in params) {
+                this[param] = params[param];
+            }
+        }
+
+        var _image;
+        Object.defineProperties(self, {
+            'gifImage': {
+                get: function() {
+                    return _image;
+                },
+                set: function(value) {
+                    // We don't use backgroundImage of view. Because, it breaks image fill type.
+                    if (!value instanceof GifImage)
+                        return;
+                    _image = value;
+                    this.nativeObject.setImageDrawable(_image.nativeObject);
+                },
+                enumerable: true
+            },
+            'startAnimating': {
+                value: function() {
+                    self.gifImage.nativeObject.start();
+                },
+                enumerable: true
+            },
+            'stopAnimation': {
+                get: function() {
+                    return _image;
+                },
+                set: function(value) {
+                    // We don't use backgroundImage of view. Because, it breaks image fill type.
+                    if (!value instanceof GifImage)
+                        return;
+                    _image = value;
+                    this.nativeObject.setImageDrawable(_image.nativeObject);
+                },
+                enumerable: true
+            },
+            'currentFrameIndex': {
+                get: function() {
+                    return self.gifImage.nativeObject.getCurrentFrameIndex();
+                },
+                enumerable: true
+            },
+            'currentFrame': {
+                get: function() {
+                    return self.gifImage.nativeObject.getCurrentFrame();
+                },
+                enumerable: true
+            },
+            'isAnimating': {
+                get: function() {
+                    return self.gifImage.nativeObject.isPlaying();
+                },
+                enumerable: true
+            }
+        });
+
+        self.ios.setLoopCompletionCallback = function() {}
+    }
+);
+
+module.exports = GifImageView;
