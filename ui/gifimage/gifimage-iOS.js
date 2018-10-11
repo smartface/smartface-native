03f1e31d7bf9fc0184354608fc89d52b8838ce72
Dogan Ekici <doganekici26@gmail.com>
2018-10-11 11:49:32 +0000

    file parameter support added to createfromfile function

--- ui/gifimage/gifimage-iOS.js
+++ ui/gifimage/gifimage-iOS.js
@@ -12,68 +12,68 @@ function GifImage(params) {
     self.nativeObject = params.nativeObject;
 
     Object.defineProperty(self, 'loopCount', {
-        set: function(){
-            
+        set: function() {
+
         },
         get: function() {
             return self.nativeObject.loopCount;
         },
         enumerable: true
     });
-    
+
     Object.defineProperty(self, 'frameCount', {
-        set: function(){
-            
+        set: function() {
+
         },
         get: function() {
             return self.nativeObject.frameCount;
         },
         enumerable: true
     });
-    
+
     Object.defineProperty(self.ios, 'frameCacheSizeCurrent', {
-        set: function(){
-            
+        set: function() {
+
         },
         get: function() {
             return self.nativeObject.frameCacheSizeCurrent;
         },
         enumerable: true
     });
-    
+
     Object.defineProperty(self, 'posterImage', {
-        set: function(){
-            
+        set: function() {
+
         },
         get: function() {
             return Image.createFromImage(self.nativeObject.posterImage);
         },
         enumerable: true
     });
-    
+
     Object.defineProperty(self, 'instrinsicSize', {
-        set: function(){
-            
+        set: function() {
+
         },
         get: function() {
             return self.nativeObject.size;
         },
         enumerable: true
     });
-    
+
     Object.defineProperty(self.ios, 'getDelayTimesForIndexes', {
-        value: function(){
+        value: function() {
             return self.nativeObject.getDelayTimesForIndexes();
         },
         enumerable: true
     });
-    
+
     Object.defineProperty(self, 'toBlob', {
-        value: function(){
+        value: function() {
             var retval = null;
             var data = self.nativeObject.data;
             if (data) {
-              retval = new Blob(data);
+                retval = new Blob(data);
             }
             return retval;
         },
@@ -81,10 +81,17 @@ function GifImage(params) {
     });
 }
 
-GifImage.createFromFile = function(path) {
-    var file = new File({
-        path: path
-    });
+GifImage.createFromFile = function(pathOrFile) {
+    var file;
+    if (typeof pathOrFile === 'string') {
+        file = new File({
+            path: pathOrFile
+        });
+    }
+    else {
+        file = pathOrFile
+    }
+
     var fileStream = file.openStream(FileStream.StreamType.READ, FileStream.ContentMode.BINARY);
     var blob = fileStream.readToEnd();
     var nativeObject = __SF_FLAnimatedImage.animatedImageWithGIFData(blob.nativeObject);
