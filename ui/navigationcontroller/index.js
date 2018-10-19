3497af2997e818eb6f1982298f3c3457f8612a66
Tolga Haliloğlu <tolga.haliloglu@smartface.io>
2018-10-20 01:57:02 +0300

    Console logs are removed for iOS.

--- ui/navigationcontroller/index.js
+++ ui/navigationcontroller/index.js
@@ -1,6 +1,5 @@
 if (Device.deviceOS === "iOS") {
   module.exports = require('./navigationcontroller-iOS');
 } else if (Device.deviceOS === "Android") {
-  console.log("NavigationController index");
   module.exports = require('./navigationcontroller-Android');
 }
