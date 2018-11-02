292126b196b075550f2173414da698e9b2046aa9
myalcinkuru <yalcin.kuru@smartface.io>
2018-11-02 07:05:02 +0000

    cleared analysis is changed with removed tag

--- ui/textview/analysis.js
+++ ui/textview/analysis.js
@@ -198,6 +198,24 @@ TextView.prototype.ios.scrollEnabled = true;
 TextView.prototype.attributedText = "";
 
 /**
+ * This event is called when user click link string. onClick just work with attributedText.
+ * onClick deprecated on 3.0.1 version.Use onLinkClick instead.
+ * 
+ *     @example
+ *     myTextView.onClick = function(string) {
+ *         console.log(string);
+ *     };
+ *
+ * @param {String} string
+ * @event onClick
+ * @android
+ * @ios
+ * @since 3.0.0
+ * @removed since 3.2.1, you should use UI.TextView#onLinkClick instead
+ */
+TextView.prototype.onClick = function(string) {};
+
+/**
  * This event is called when user click link string. onLinkClick just work with attributedText.
  *
  *     @example
