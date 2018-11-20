9521bfb8760c8517adef7922f5186fe8bf4bef60
myalcinkuru <yalcin.kuru@smartface.io>
2018-11-20 19:49:49 +0000

    more defination is added

--- ui/webview/analysis.js
+++ ui/webview/analysis.js
@@ -225,7 +225,8 @@ WebView.prototype.evaluateJS = function(javascript,onReceive) {};
 WebView.prototype.onChangedURL = function(event) {};
 
 /**
- * This event will be triggered when user clicks back button on the Device. The purpose of using this event might be
+ * This event will be triggered when user clicks back button on the Device. WebView is focusable view. When it gains focus, this
+ * event begin to trigger. The purpose of using this event might be
  * navigating back to pervious web pages.
  *
  * @event onBackButtonPressed
