e86c921c83eba75f674fad5d1a46d302ccd5c8da
Dogan Ekici <doganekici26@gmail.com>
2018-11-20 09:43:06 +0000

    [IOS-2698] ActivityIndicator enum changes

---
+++ ui/activityindicator/ios/activityindicatorviewstyle/analysis.js
@@ -0,0 +1,26 @@
+/**
+ * iOS Specific Properties.
+ * @class UI.ActivityIndicator.iOS.ActivityIndicatorViewStyle
+ * @since 3.2.1
+ */
+const ActivityIndicatorViewStyle = {};
+
+/**
+ * @property {Number} LARGE
+ * @ios
+ * @static
+ * @readonly
+ * @since 3.2.1
+ */
+ActivityIndicatorViewStyle.LARGE = 0;
+
+/**
+ * @property {Number} NORMAL
+ * @ios
+ * @static
+ * @readonly
+ * @since 3.2.1
+ */
+ActivityIndicatorViewStyle.NORMAL = 1;
+
+module.exports = ActivityIndicatorViewStyle;
