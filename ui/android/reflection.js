2c95becfd64dc00c312ef81a7a839b975ea40ba6
myalcinkuru <yalcin.kuru@smartface.io>
2018-11-29 06:42:56 +0000

    [AND-3445] reflection is under control

--- ui/android/reflection.js
+++ ui/android/reflection.js
@@ -9,29 +9,34 @@ Object.defineProperties(Reflection, {
     'setCursorColor': {
         value: function(textbox, color) {
             /*
-            ToDo: Keep until pragmatic solution available. Does not work for Android P and higher.
+            ToDo: Keep until pragmatic solution available. Does not work for Android P and might a few devices.
             */
-            if (AndroidConfig.sdkVersion >= 28)
-                return;
-            const NativeTextView = requireClass("android.widget.TextView");
-            const NativePorterDuff = requireClass("android.graphics.PorterDuff");
+            try {
+                const NativeTextView = requireClass("android.widget.TextView");
+                const NativePorterDuff = requireClass("android.graphics.PorterDuff");
 
-            var fCursorDrawableRes = NativeTextView.getDeclaredField("mCursorDrawableRes");
-            fCursorDrawableRes.setAccessible(true);
-            var mCursorDrawableRes = fCursorDrawableRes.getInt(textbox);
-            var fEditor = NativeTextView.getDeclaredField("mEditor");
-            fEditor.setAccessible(true);
-            var editor = fEditor.get(textbox);
-            var clazz = editor.getClass();
-            var fCursorDrawable = clazz.getDeclaredField("mCursorDrawable");
-            fCursorDrawable.setAccessible(true);
-            var drawables = [];
-            drawables[0] = textbox.getContext().getResources().getDrawable(mCursorDrawableRes);
-            drawables[1] = textbox.getContext().getResources().getDrawable(mCursorDrawableRes);
-            drawables[0].setColorFilter(color, NativePorterDuff.Mode.SRC_IN);
-            drawables[1].setColorFilter(color, NativePorterDuff.Mode.SRC_IN);
+                var fCursorDrawableRes = NativeTextView.getDeclaredField("mCursorDrawableRes");
+                fCursorDrawableRes.setAccessible(true);
+                var mCursorDrawableRes = fCursorDrawableRes.getInt(textbox);
+                var fEditor = NativeTextView.getDeclaredField("mEditor");
+                fEditor.setAccessible(true);
+                var editor = fEditor.get(textbox);
+                var clazz = editor.getClass();
+                var fCursorDrawable = clazz.getDeclaredField("mCursorDrawable");
+                fCursorDrawable.setAccessible(true);
+                var drawables = [];
+                drawables[0] = textbox.getContext().getResources().getDrawable(mCursorDrawableRes);
+                drawables[1] = textbox.getContext().getResources().getDrawable(mCursorDrawableRes);
+                drawables[0].setColorFilter(color, NativePorterDuff.Mode.SRC_IN);
+                drawables[1].setColorFilter(color, NativePorterDuff.Mode.SRC_IN);
 
-            fCursorDrawable.set(editor, array(drawables, 'android.graphics.drawable.Drawable'));
+                fCursorDrawable.set(editor, array(drawables, 'android.graphics.drawable.Drawable'));
+            }
+            catch (e) {
+                const NativeLog = requireClass("android.util.Log");
+                NativeLog.e("SMF", " This exception is comes from sf-core " + e.stack);
+                //Ignore the exception for now.
+            }
         },
         enumerable: true
     }
