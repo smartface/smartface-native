ad9e82ac1336cfea48f2b6c6eedf5e4a604d686d
myalcinkuru <yalcin.kuru@smartface.io>
2018-12-14 08:00:32 +0000

    [AND-3477] elevation property is added

--- ui/page/page-Android.js
+++ ui/page/page-Android.js
@@ -521,7 +521,6 @@ function Page(params) {
         configurable: true
     });
 
-
     var _leftItemEnabled;
     Object.defineProperty(self.headerBar, 'leftItemEnabled', {
         get: function() {
@@ -633,6 +632,20 @@ function Page(params) {
         enumerable: true,
         configurable: true
     });
+
+    let _headerBarElevation = null;
+    Object.defineProperty(self.headerBar.android, 'elevation', {
+        get: function() {
+            return (_headerBarElevation === null ? AndroidUnitConverter.pixelToDp(actionBar.getElevation()) : _headerBarElevation);
+        },
+        set: function(value) {
+            _headerBarElevation = value;
+            actionBar.setElevation(AndroidUnitConverter.dpToPixel(value));
+        },
+        enumerable: true,
+        configurable: true
+    });
+
     Object.defineProperty(self.headerBar.android, 'subtitle', {
         get: function() {
             return toolbar.getSubtitle();
