dd5f8bcc00054a047503660ee7365f97e4bc631e
myalcinkuru <yalcin.kuru@smartface.io>
2018-12-20 13:52:18 +0000

    [AND-3457] async task necessaries are added

--- global/asynctask/asynctask-Android.js
+++ global/asynctask/asynctask-Android.js
@@ -3,6 +3,9 @@ const TypeUtil = require("sf-core/util/type");
 const SFAsyncTask = requireClass('io.smartface.android.SFAsyncTask');
 
 function AsyncTask(params) {
+    
+    const self = this;
+    
     var callbacks = {
         onPreExecute: function() {
             _onPreExecute && _onPreExecute();
@@ -25,34 +28,14 @@ function AsyncTask(params) {
     var _task;
     var _onProgressUpdate;
     var _onComplete;
-    Object.defineProperties(this, {
-        'onPreExecute': {
-            get: function() {
-                return _onPreExecute;
-            },
-            set: function(value) {
-                if (TypeUtil.isFunction(value)) {
-                    _onPreExecute = value.bind(this);
-                }
-            }
-        },
+    Object.defineProperties(self, {
         'task': {
             get: function() {
                 return _task;
             },
             set: function(value) {
                 if (TypeUtil.isFunction(value)) {
-                    _task = value.bind(this);
-                }
-            }
-        },
-        'onProgressUpdate': {
-            get: function() {
-                return _onProgressUpdate;
-            },
-            set: function(value) {
-                if (TypeUtil.isFunction(value)) {
-                    _onProgressUpdate = value.bind(this);
+                    _task = value.bind(self);
                 }
             }
         },
@@ -62,32 +45,57 @@ function AsyncTask(params) {
             },
             set: function(value) {
                 if (TypeUtil.isFunction(value)) {
-                    _onComplete = value.bind(this);
+                    _onComplete = value.bind(self);
                 }
             }
         },
         'run': {
             value: function(params) {
                 if (TypeUtil.isArray(params)) {
-                    this.nativeObject.executeTask(array(params, "java.lang.Object"));
-                } else {
-                    this.nativeObject.executeTask(null);
+                    self.nativeObject.executeTask(array(params, "java.lang.Object"));
                 }
-            }
-        },
-        'cancel': {
-            value: function() {
-                this.nativeObject.cancel();
-            }
-        },
-        'status': {
-            value: function() {
-                return this.nativeObject.getStatus();
+                else {
+                    self.nativeObject.executeTask(null);
+                }
             }
         },
         'toString': {
             value: function() {
                 return "AsyncTask";
+            }
+        }
+    });
+
+    self.android = {};
+    Object.defineProperties(self.android, {
+        'setCancel': {
+            value: function(mayInterruptIfRunning = false) {
+                self.nativeObject.cancel(mayInterruptIfRunning);
+            }
+        },
+        'getStatus': {
+            value: function() {
+                return self.nativeObject.getStatus();
+            }
+        },
+        'onPreExecute': {
+            get: function() {
+                return _onPreExecute;
+            },
+            set: function(value) {
+                if (TypeUtil.isFunction(value)) {
+                    _onPreExecute = value.bind(self);
+                }
+            }
+        },
+        'onProgressUpdate': {
+            get: function() {
+                return _onProgressUpdate;
+            },
+            set: function(value) {
+                if (TypeUtil.isFunction(value)) {
+                    _onProgressUpdate = value.bind(self);
+                }
             }
         }
     });
@@ -102,8 +110,10 @@ function AsyncTask(params) {
 
 
 AsyncTask.Status = {};
-AsyncTask.Status.FINISHED = SFAsyncTask.Status.FINISHED;
-AsyncTask.Status.PENDING = SFAsyncTask.Status.PENDING;
-AsyncTask.Status.RUNNING = SFAsyncTask.Status.RUNNING;
+AsyncTask.Status.Android = {};
+AsyncTask.Status.Android.FINISHED = SFAsyncTask.Status.FINISHED;
+AsyncTask.Status.Android.PENDING = SFAsyncTask.Status.PENDING;
+AsyncTask.Status.Android.RUNNING = SFAsyncTask.Status.RUNNING;
+Object.freeze(AsyncTask.Status.Android);
 
 module.exports = AsyncTask;
--- global/asynctask/asynctask-iOS.js
+++ global/asynctask/asynctask-iOS.js
@@ -1,58 +1,60 @@
-const TypeUtil          = require("sf-core/util/type");
+const TypeUtil = require("sf-core/util/type");
 
-function AsyncTask(params){
+function AsyncTask(params) {
     var self = this;
-    
+
     var _task;
     var _onComplete;
-    
-    Object.defineProperties(self,{
+
+    Object.defineProperties(self, {
         'task': {
-            get: function(){
+            get: function() {
                 return _task;
             },
-            set: function(value){
-                if(TypeUtil.isFunction(value)){
+            set: function(value) {
+                if (TypeUtil.isFunction(value)) {
                     _task = value.bind(this);
                 }
             }
         },
         'onComplete': {
-            get: function(){
+            get: function() {
                 return _onComplete;
             },
-            set: function(value){
-                if(TypeUtil.isFunction(value)){
+            set: function(value) {
+                if (TypeUtil.isFunction(value)) {
                     _onComplete = value.bind(this);
                 }
             }
         },
         'run': {
-            value: function(){
-                try{
+            value: function() {
+                try {
                     // Background
-                    SF.dispatch_async(SF.dispatch_get_global_queue(0,0), function() {
+                    SF.dispatch_async(SF.dispatch_get_global_queue(0, 0), function() {
                         self.task();
-                        
+
                         // Main
                         SF.dispatch_async(SF.dispatch_get_main_queue(), function() {
                             self.onComplete();
                         });
                     });
                 }
-                catch(e){
+                catch (e) {
                     Application.onUnhandledError(e);
                 }
             }
         }
     });
-    
+
     // Assign parameters given in constructor
     if (params) {
         for (var param in params) {
             this[param] = params[param];
         }
     }
+    
+    self.android = {};
 }
 
 module.exports = AsyncTask;
