bc38d408d3cbedc64bfdf0a594c90ba02aca1e1d
Hasret Sariyer <hasret.sariyer@smartface.io>
2018-12-27 05:35:38 +0000

    [DOC-528] Edit analysis for new router

--- ui/bottomtabbar/analysis.js
+++ ui/bottomtabbar/analysis.js
@@ -2,36 +2,10 @@
  * @class UI.BottomTabBar
  * @since 1.1.10
  *
- * BottomTabBar is used for navigating between tab bar items with given tags.
+ * BottomTabBar represents a bottom navigation bar. You can specify bar color and item color.
+ * 
+ * @see https://smartface.github.io/router/class/src/native/BottomTabBarRouter.js~BottomTabBarRouter.html
  *
- *     @example
- *     const TabBarItem = require('sf-core/ui/tabbaritem');
- *     const BottomTabBar = require('sf-core/ui/bottomtabbar');
- *     const Router = require('sf-core/router');
- *     const Image = require('sf-core/ui/image');
- *     
- *     var myProfileImage = Image.createFromFile("images://profile.png");
- *     var myMessageImage = Image.createFromFile("images://messages.png");
- *     var myTab = new BottomTabBar();
- * 
- *     var myProfileItem = new TabBarItem({
- *         title: "Profile",
- *         icon: myProfileImage,
- *         route: 'pages/pgProfile'
- *     });
- * 
- *     var myMessageItem = new TabBarItem({
- *         title: "Messages",
- *         icon: myMessageImage,
- *         route: 'pages/pgMessages'
- *     });
- * 
- *     myTab.add('profile', myProfileItem);
- *     myTab.add('messages', myMessageItem);
- *     myTab.setIndex('messages');
- * 
- *     Router.add('dashboard', myTab);
- *     Router.go('dashboard');
  */
 function BottomTabBar() {}
 
