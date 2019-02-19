d3cad30421a089d5be56789d84d919cce769b349
myalcinkuru <yalcin.kuru@smartface.io>
2019-02-19 19:54:20 +0000

    [COR-1831] Secure Data implementation is done

--- global/securedata/securedata-Android.js
+++ global/securedata/securedata-Android.js
@@ -1,3 +1,64 @@
-var SecureData = {};
+const Application = require("sf-core/application");
+const NativeSFKeyStore = requireClass('io.smartface.android.sfcore.global.SFKeyStore');
 
+function SecureData(params) {
+    const self = this;
+    
+    let key = params && params.key;
+    if (!key) {
+        throw new Error("Constructor must have key parameter.");
+    }
+
+    if (!self.nativeObject) {
+        self.nativeObject = new NativeSFKeyStore(key);
+    }
+
+    Object.defineProperty(self, 'key', {
+        get: function() {
+            return key;
+        },
+        enumerable: true
+    });
+
+    self.read = () => {
+        return new Promise((resolve, reject) => {
+            try {
+                let decryptData = self.nativeObject.decryptData();
+                resolve(decryptData);
+            }
+            catch (e) {
+                reject();
+                Application.onUnhandledError && Application.onUnhandledError(e);
+            }
+        });
+    };
+
+    self.save = (params) => {
+        return new Promise((resolve, reject) => {
+            try {
+                self.nativeObject.encryptData(params.value);
+                resolve();
+            }
+            catch (e) {
+                reject();
+                Application.onUnhandledError && Application.onUnhandledError(e);
+            }
+        });
+    };
+
+    self.delete = () => {
+        return new Promise((resolve, reject) => {
+            try {
+                self.nativeObject.deleteEntry();
+                resolve();
+            }
+            catch (e) {
+                reject();
+                Application.onUnhandledError && Application.onUnhandledError(e);
+            }
+        });
+    };
+
+    self.ios = {};
+}
 module.exports = SecureData;
