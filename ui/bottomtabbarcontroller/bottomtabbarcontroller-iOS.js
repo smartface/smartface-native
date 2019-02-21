70cd9a3a6fdf5adc979fcb34bd357b1828bd754a
dogan ekici <doganekici26@gmail.com>
2019-02-21 09:19:55 +0000

    [bugfix/IOS-2756] changed object assign parameter set

--- ui/bottomtabbarcontroller/bottomtabbarcontroller-iOS.js
+++ ui/bottomtabbarcontroller/bottomtabbarcontroller-iOS.js
@@ -147,23 +147,7 @@ function BottomTabBarController(params) {
     };
     //////////////////////////////////////////////////////////////////////////
     
-    // Assign parameters given in constructor
-    (function(params) {
-        for (var param in params) {
-            if (param === "ios" || param === "android") {
-                setOSSpecificParams.call(this, params[param], param);
-            }
-            else {
-                this[param] = params[param];
-            }
-        }
-
-        function setOSSpecificParams(params, key) {
-            for (var param in params) {
-                this[key][param] = params[param];
-            }
-        }
-    }.bind(this)(params));
+    params && (Object.assign(this, params));
 };
 
 function BottomTabBarView(params) {    
