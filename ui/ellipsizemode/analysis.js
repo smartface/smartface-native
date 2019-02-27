d1622a01fc44e79ff8e6e996efbbf32f50dfa469
myalcinkuru <yalcin.kuru@smartface.io>
2019-02-27 08:31:46 +0000

    [COR-1925] ellipsizemode analysis and enums are added

---
+++ ui/ellipsizemode/analysis.js
@@ -0,0 +1,63 @@
+/**
+ * @enum {Number} UI.EllipsizeMode
+ * @static
+ * @readonly
+ * @since 0.1
+ *
+ * EllipsizeMode is an enum. It defines how to text will be truncated.
+ *
+ *     @example
+ *     const Color = require('sf-core/ui/color');
+ *     const TextView = require('sf-core/ui/textview');
+ *     const EllipsizeMode = require('sf-core/ui/ellipsizemode');
+ *
+ *     var myTextView = new TextView({
+ *         hint: "hint",
+ *         width: 100,
+ *         backgroundColor: Color.create("#7fd0ff"),
+ *         ellipsizeMode: EllipsizeMode.START
+ *     });
+ *
+ *     myPage.layout.addChild(myTextView);
+ *
+ */
+const EllipsizeMode = {};
+
+/**
+ * End of text fits in the container and the missing text at the beginning of the line is indicated by 
+ * an ellipsis glyph. Such as "...smrtfc"
+ * 
+ * @property {Number} START
+ * @android
+ * @ios
+ * @static
+ * @readonly
+ * @since 4.0.2
+ */
+EllipsizeMode.START = 0;
+
+/**
+ * End & start of text fits in the container and the missing middle of the text  is indicated by 
+ * an ellipsis glyph. Such as "smrt...fc"
+ * 
+ * @property {Number} MIDDLE
+ * @android
+ * @ios
+ * @static
+ * @readonly
+ * @since 4.0.2s
+ */
+EllipsizeMode.MIDDLE = 1;
+
+/**
+ * Start of text fits in the container and the missing end of the text  is indicated by 
+ * an ellipsis glyph. Such as "smrtfc..."
+ * 
+ * @property {Number} END
+ * @android
+ * @ios
+ * @static
+ * @since 4.0.2
+ * @readonly
+ */
+EllipsizeMode.END = 2;
---
+++ ui/ellipsizemode/ellipsizemode.js
@@ -0,0 +1,8 @@
+const EllipsizeMode = { };
+EllipsizeMode.START = 0;
+EllipsizeMode.MIDDLE = 1;
+EllipsizeMode.END = 2;
+
+Object.freeze(EllipsizeMode);
+
+module.exports = EllipsizeMode;
