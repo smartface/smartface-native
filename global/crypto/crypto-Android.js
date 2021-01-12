335f361f331536ea1ebefdadeab880912762b992
Hasret Sariyer <hasret.sariyer@smartface.io>
2021-01-12 05:28:31 +0000

    Merged in story/COR-2070 (pull request #1689)
    [COR-2070] Implement crypto for ios
    
    * [COR-2070] Implement crypto for ios

--- global/crypto/crypto-Android.js
+++ global/crypto/crypto-Android.js
@@ -1,32 +1,49 @@
 const SFCrypto = requireClass("io.smartface.android.sfcore.global.Crypto");
 
 function Crypto() {}
+Crypto.singletonObject = {};
+Crypto.ios = {};
+Crypto.ios.getExportedPublicKey = function() {};
 
 Crypto.generateKeyPair = function (params = {}) {
-    let {keySize, algorithm} = params;
+    let {keySize, algorithm="RSA"} = params;
     var keyPair = SFCrypto.generateKeyPair(keySize, algorithm);
-    return {
-        public: keyPair.getPublic(),
-        private: keyPair.getPrivate()
-    }
+    Crypto.singletonObject.publicKey = keyPair.getPublic();
+    Crypto.singletonObject.privateKey = keyPair.getPrivate();
 };
 
-Crypto.toBase64String = function(key) {
-    return SFCrypto.toBase64String(key);
+Crypto.getBase64PublicString = function() {
+    return SFCrypto.toBase64String(Crypto.singletonObject.publicKey);
 };
 
-Crypto.getKeyFromBase64String = function(params) {
-    let {base64String, isPrivate, algorithm} = params;
-    return SFCrypto.getKeyFromBase64String(base64String, isPrivate, algorithm);
+Crypto.getBase64PrivateString = function() {
+    return SFCrypto.toBase64String(Crypto.singletonObject.privateKey);
+};
+
+Crypto.setPrivateKey = function(base64String) {
+    let privateKey = SFCrypto.getKeyFromBase64String(base64String, true, "RSA");
+    Crypto.singletonObject.privateKey = privateKey;
+};
+
+Crypto.setPublicKey = function(base64String) {
+    let publicKey = SFCrypto.getKeyFromBase64String(base64String, false, "RSA");
+    Crypto.singletonObject.publicKey = publicKey;
+};
+
+Crypto.setServerPublicKey = function(base64String) {
+    let serverPublicKey = SFCrypto.getKeyFromBase64String(base64String, false, "RSA");
+    Crypto.singletonObject.serverPublicKey = serverPublicKey;
 };
 
 Crypto.encrypt = function(params = {}) {
-    let {plainText, key, cipherType} = params;
+    let {plainText, useServerKey, cipherType} = params;
+    let key = useServerKey ? Crypto.singletonObject.serverPublicKey : Crypto.singletonObject.publicKey;
     return SFCrypto.encrypt(plainText, key, cipherType);
 };
 
 Crypto.decrypt = function(params = {}) {
-    let {encryptedText, key, cipherType} = params;
+    let {encryptedText, cipherType} = params;
+    let key = Crypto.singletonObject.privateKey;
     return SFCrypto.decrypt(encryptedText, key, cipherType);
 };
 
