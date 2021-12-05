6a2d0e7141420cd948e8854bf0c5abb8e03e6874
Berk Baski <baskiberk41@gmail.com>
2021-12-05 14:49:17 +0300

    Add timers

---
+++ core/base/timers/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Smartface global timer replacer module
+ * @module timer
+ * @type {object}
+ * @author Alper Ozisik <alper.ozisik@smartface.io>
+ * @copyright Smartface 2018
+ */
+export { };
