209d2badaf52c63712dd93d0be4b83c914ea9090
Berk Baski <baskiberk41@gmail.com>
2021-12-16 14:37:24 +0300

    Add chooserTitle to MapOptions & NavigationOptions

--- application/maps/maps-Android.js
+++ application/maps/maps-Android.js
@@ -4,9 +4,10 @@ const openMaps = (options) => {
   return new Promise((resolve, reject) => {
     const { latitude, longitude } = options.location;
     const locationName = options.name || '';
+    const chooserTitle = options.chooserTitle || '';
     Linking.openURL({
       uriScheme: `geo:${latitude},${longitude}?q=${encodeURIComponent(locationName)}`,
-      chooserTitle: global.lang.chooseMapsApp || "Choose Maps App",
+      chooserTitle,
       onSuccess: (e) => resolve(e),
       onFailure: (e) => reject(e),
       isShowChooser: true
@@ -18,9 +19,10 @@ const openNavigation = (options) => {
   return new Promise((resolve, reject) => {
     const { latitude, longitude } = options.location;
     const transportType = options.transportType;
+    const chooserTitle = options.chooserTitle || '';
     Linking.openURL({
       uriScheme: `geo:${latitude},${longitude}?q=${latitude},${longitude}&mode=${transportType}`,
-      chooserTitle: global.lang.chooseMapsApp || "Choose Maps App",
+      chooserTitle,
       onSuccess: (e) => resolve(e),
       onFailure: (e) => reject(e),
       isShowChooser: true,
