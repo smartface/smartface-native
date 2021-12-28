b91d8b5b75a01584db9bdc175c7b0242de9eb826
Furkan Arabacı <furkan.arabaci@smartface.io>
2021-12-28 11:30:44 +0300

    Update maps-Android.js

--- application/maps/maps-Android.js
+++ application/maps/maps-Android.js
@@ -1,10 +1,12 @@
 const Linking = require('../linking');
 
+const DEFAULT_CHOOSER_TITLE = "Choose Maps App";
+
 const openMaps = (options) => {
   return new Promise((resolve, reject) => {
     const { latitude, longitude } = options.location;
     const locationName = options.name || '';
-    const chooserTitle = options.chooserTitle || 'Choose Map';
+    const chooserTitle = options.chooserTitle || DEFAULT_CHOOSER_TITLE;
     Linking.openURL({
       uriScheme: `geo:${latitude},${longitude}?q=${encodeURIComponent(locationName)}`,
       chooserTitle,
@@ -19,7 +21,7 @@ const openNavigation = (options) => {
   return new Promise((resolve, reject) => {
     const { latitude, longitude } = options.location;
     const transportType = options.transportType;
-    const chooserTitle = options.chooserTitle || '';
+    const chooserTitle = options.chooserTitle || DEFAULT_CHOOSER_TITLE;
     Linking.openURL({
       uriScheme: `geo:${latitude},${longitude}?q=${latitude},${longitude}&mode=${transportType}`,
       chooserTitle,
