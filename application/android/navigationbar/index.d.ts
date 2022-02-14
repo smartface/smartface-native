c34f12c500f7e6a3b144f1317d40e8495157643d
alimoncul <alimoncul@gmail.com>
2022-02-14 14:31:47 +0300

    feature: navigationbar

--- application/android/navigationbar/index.d.ts
+++ application/android/navigationbar/index.d.ts
@@ -1,24 +1,25 @@
-import NavigationbarStyle from "./style";
-import Color from "../../../ui/color";
+import Color from '../../../ui/color';
+import { NavigationBarStyle } from './navigationbar-Android';
 /**
  * @class Application.Android.NavigationBar
  *
  * This class represents Android navigation bar (includes soft keys) object. Creating instance of
  * NavigationBar is not valid since you can't use in anywhere.
- * 
+ *
  * @since 4.0.0
  */
 declare class Navigationbar {
-  color: Color;
-/**
- * Gets/sets transparency of status bar.This property works only for Android version 
- * OREO 8.1.0 (API 27) or above.
- *
- * @property {Application.Android.NavigationBar.Style} [style = Application.Android.NavigationBar.Style.DARKCONTENT]
- * @android
- * @since 4.0.0
- */
-  style: NavigationbarStyle
+	color: Color;
+	/**
+	 * Style is an enum. It defines navigation bar appearance style.
+	 * Gets/sets transparency of status bar.This property works only for Android version
+	 * OREO 8.1.0 (API 27) or above.
+	 *
+	 * @property {Application.Android.NavigationBar.Style} [style = Application.Android.NavigationBar.Styles.DARKCONTENT]
+	 * @android
+	 * @since 4.0.0
+	 */
+	style: NavigationBarStyle;
 }
 
 export = Navigationbar;
