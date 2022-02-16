f833371b98d3a1ee639a4072fbae70a4657d4367
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-02-16 22:50:32 +0300

    util common ones

---
+++ util/exception.ts
@@ -0,0 +1,14 @@
+namespace Exception {
+  export const TypeError = Object.freeze({
+    DEFAULT: "Parameter type must be ",
+    NUMBER: "Parameter type must be number",
+    STRING: "Parameter type must be string",
+    BOOLEAN: "Parameter type must be boolean",
+    OBJECT: "Parameter type must be object",
+    ARRAY: "Parameter type must be array",
+    FUNCTION: "Parameter type must be function",
+    URL: "URL is not valid",
+  })
+};
+
+export default Exception;
