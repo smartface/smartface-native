45cba2b407ed076fbefab965fb429eefe1f5af87
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2022-02-18 14:43:22 +0300

    refactor: Add instance type of Image

--- ui/image/index.ts
+++ ui/image/index.ts
@@ -1,6 +1,6 @@
 import {ImageBase} from "./image";
 
-const Image: typeof ImageBase = require(`./image.${Device.deviceOS.toLowerCase()}`)
-.default;
+const Image: typeof ImageBase = require(`./image.${Device.deviceOS.toLowerCase()}`).default;
+type Image = ImageBase;
 
 export default Image;
