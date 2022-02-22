67bfc404560c15874b7abb4dcb43080374ed861c
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-02-22 11:50:17 +0300

    add type to font

--- ui/font/index.ts
+++ ui/font/index.ts
@@ -1,4 +1,5 @@
 import { AbstractFont } from './font';
 
 const Font: typeof AbstractFont = require(`./font.${Device.deviceOS.toLowerCase()}`).default;
+type Font = AbstractFont;
 export default Font;
