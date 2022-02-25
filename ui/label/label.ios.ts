1c093b529dfe6adb0e598fa7c7692adba7e4db19
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-02-25 13:04:18 +0300

    remove abstractlabel

--- ui/label/label.ios.ts
+++ ui/label/label.ios.ts
@@ -3,7 +3,7 @@ import Color from '../color';
 import TextAlignment from '../textalignment';
 import { ViewEvents } from '../view/view-event';
 import ViewIOS from '../view/view.ios';
-import { AbstractLabel, ILabel } from './label';
+import { ILabel } from '.';
 
 const DEFAULT_MINIMUM_FONT_SIZE = 1;
 
@@ -11,7 +11,7 @@ export default class LabelIOS<TEvent extends string = ViewEvents> extends ViewIO
   private _minimumFontSize = DEFAULT_MINIMUM_FONT_SIZE;
   private _textAlignment = TextAlignment.MIDLEFT;
   protected _textColor: ILabel['textColor'] = Color.BLACK;
-  constructor(params: Partial<AbstractLabel> = {}) {
+  constructor(params: Partial<ILabel> = {}) {
     super();
     if (!this.nativeObject) {
       this._nativeObject = new __SF_SMFUILabel();
@@ -26,60 +26,60 @@ export default class LabelIOS<TEvent extends string = ViewEvents> extends ViewIO
   get font() {
     return this.nativeObject.font;
   }
-  set font(value: AbstractLabel['font']) {
+  set font(value: ILabel['font']) {
     this.nativeObject.font = value;
     this.minimumFontSize = this.minimumFontSize;
   }
   get adjustFontSizeToFit() {
     return this.nativeObject.adjustsFontSizeToFitWidth;
   }
-  set adjustFontSizeToFit(value: AbstractLabel['adjustFontSizeToFit']) {
+  set adjustFontSizeToFit(value: ILabel['adjustFontSizeToFit']) {
     this.nativeObject.baselineAdjustment = DEFAULT_MINIMUM_FONT_SIZE;
     this.nativeObject.adjustsFontSizeToFitWidth = value;
   }
   get minimumFontSize() {
     return this._minimumFontSize;
   }
-  set minimumFontSize(value: AbstractLabel['minimumFontSize']) {
+  set minimumFontSize(value: ILabel['minimumFontSize']) {
     this._minimumFontSize = value;
     this.nativeObject.minimumScaleFactor = this._minimumFontSize / this.font.size;
   }
   get ellipsizeMode() {
     return NSLineBreakMode.nsLineBreakModeToEllipsizeMode(this.nativeObject.lineBreakMode);
   }
-  set ellipsizeMode(value: AbstractLabel['ellipsizeMode']) {
+  set ellipsizeMode(value: ILabel['ellipsizeMode']) {
     this.nativeObject.lineBreakMode = NSLineBreakMode.ellipsizeModeToNSLineBreakMode(value);
   }
   get maxLines() {
     return this.nativeObject.numberOfLines;
   }
-  set maxLines(value: AbstractLabel['maxLines']) {
+  set maxLines(value: ILabel['maxLines']) {
     this.nativeObject.numberOfLines = value;
   }
   get multiline() {
     return this.nativeObject.numberOfLines === 0 && this.nativeObject.numberOfLines === 0;
   }
-  set multiline(value: AbstractLabel['multiline']) {
+  set multiline(value: ILabel['multiline']) {
     this.nativeObject.numberOfLines = value ? 0 : 1;
     this.nativeObject.lineBreakMode = value ? 0 : 4;
   }
   get text() {
     return this.nativeObject.text;
   }
-  set text(value: AbstractLabel['text']) {
+  set text(value: ILabel['text']) {
     this.nativeObject.text = value;
   }
   get textAlignment() {
     return this._textAlignment;
   }
-  set textAlignment(value: AbstractLabel['textAlignment']) {
+  set textAlignment(value: ILabel['textAlignment']) {
     this._textAlignment = value;
     this.nativeObject.textAlignment = value % 3;
   }
   get textColor() {
     return this._textColor;
   }
-  set textColor(value: AbstractLabel['textColor']) {
+  set textColor(value: ILabel['textColor']) {
     if (value instanceof Color) {
       this._textColor = value;
       this.nativeObject.textColor = value.nativeObject;
