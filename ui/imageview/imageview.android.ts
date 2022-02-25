1dee8855064b21789577a2d0353a225de1a6147b
Berk Baski <baskiberk41@gmail.com>
2022-02-25 13:24:37 +0300

    Update imageview to TypeScript

---
+++ ui/imageview/imageview.android.ts
@@ -0,0 +1,268 @@
+/*globals requireClass, array*/
+import { IImageView, ImageViewFillType, ImageViewFillTypeIOS } from '.';
+import File from '../../io/file';
+import Path from '../../io/path';
+import AndroidConfig from '../../util/Android/androidconfig';
+import Color from '../color';
+import Image from '../image';
+import { ImageCacheType } from '../imagecachetype';
+import { ViewAndroid } from '../view/view.android';
+import { ImageViewEvents } from './imageview-events';
+
+const NativeImageView = requireClass('android.widget.ImageView');
+const SFGlide = requireClass('io.smartface.android.sfcore.ui.imageview.SFGlide');
+const LoadFromUrlParameters = requireClass('io.smartface.android.sfcore.ui.imageview.models.LoadFromUrlParameters');
+const FetchFromUrlParameters = requireClass('io.smartface.android.sfcore.ui.imageview.models.FetchFromUrlParameters');
+const LoadFromFileParameters = requireClass('io.smartface.android.sfcore.ui.imageview.models.LoadFromFileParameters');
+const NativeImageCompat = requireClass('androidx.core.widget.ImageViewCompat');
+const NativeColorStateListUtil = requireClass('io.smartface.android.utils.ColorStateListUtil');
+const GlideRequestListener = requireClass('io.smartface.android.sfcore.ui.imageview.listeners.GlideRequestListener');
+
+const ImageFillTypeDic = {
+  [ImageViewFillType.NORMAL]: NativeImageView.ScaleType.CENTER,
+  [ImageViewFillType.STRETCH]: NativeImageView.ScaleType.FIT_XY,
+  [ImageViewFillType.ASPECTFIT]: NativeImageView.ScaleType.FIT_CENTER, // should be fit().centerInside()
+  [ImageViewFillType.ASPECTFILL]: NativeImageView.ScaleType.CENTER_CROP //should be centerCrop
+};
+
+export default class ImageViewAndroid<TEvent extends string = ImageViewEvents> extends ViewAndroid<TEvent | ImageViewEvents> implements IImageView {
+  private _fillType: ImageViewFillType | ImageViewFillTypeIOS;
+  private _image: Image;
+  private _adjustViewBounds: boolean = false;
+  private _tintColor: Color;
+  private _newImageLoaded: boolean = false;
+  constructor(params: Partial<IImageView> = {}) {
+    super();
+
+    if (!this.nativeObject) {
+      this._nativeObject = new NativeImageView(AndroidConfig.activity);
+    }
+
+    // Assign parameters given in constructor
+    for (const param in params) {
+      this[param] = params[param];
+    }
+  }
+
+  get image(): string | Image {
+    if (!this._image || this._newImageLoaded) {
+      this._newImageLoaded = false;
+      const drawable = this.nativeObject.getDrawable();
+      return (this._image = drawable
+        ? new Image({
+            // TODO Recheck after build
+            drawable: drawable
+          })
+        : null);
+    } else return this._image;
+  }
+  set image(value: string | Image) {
+    // We don't use backgroundImage of view. Because, it breaks image fill type.
+    if (value instanceof Image) {
+      this._image = value;
+      this.nativeObject.setImageDrawable(value.nativeObject);
+    } else if (typeof value === 'string') {
+      const imageFile = new File({ path: value });
+      this.loadFromFile({ file: imageFile });
+    } else {
+      this._image = null;
+      this.nativeObject.setImageDrawable(null);
+    }
+  }
+
+  get tintColor(): Color {
+    return this._tintColor;
+  }
+  set tintColor(value: Color) {
+    if (!(value instanceof Color)) return;
+    this._tintColor = value;
+    NativeImageCompat.setImageTintList(this.nativeObject, NativeColorStateListUtil.getColorStateListWithValueOf(this._tintColor.nativeObject));
+  }
+
+  get imageFillType(): ImageViewFillTypeIOS | ImageViewFillType {
+    return this._fillType === undefined ? this.nativeObject.getScaleType() : this._fillType;
+  }
+  set imageFillType(value: ImageViewFillTypeIOS | ImageViewFillType) {
+    if (!(value in ImageFillTypeDic)) {
+      value = ImageViewFillType.NORMAL;
+    }
+    this._fillType = value;
+    if (value === ImageViewFillType.ASPECTFILL && !this._adjustViewBounds) {
+      this.nativeObject.setAdjustViewBounds(true);
+      this._adjustViewBounds = true;
+    }
+    this.nativeObject.setScaleType(ImageFillTypeDic[this._fillType]);
+  }
+
+  loadFromUrl(params: {
+    url: string;
+    headers?: { [name: string]: string };
+    placeholder?: Image;
+    fade?: boolean;
+    useHTTPCacheControl?: boolean;
+    onSuccess?: () => void;
+    onFailure?: () => void;
+    android?: {
+      useDiskCache?: boolean;
+      useMemoryCache?: boolean;
+    };
+    ios?: { isRefreshCached?: boolean };
+  }): void {
+    const { url, headers, placeholder, fade, useHTTPCacheControl, onSuccess, onFailure, android } = params;
+    //TODO: Paramters should be object this usage is deprecated
+    if (!url) {
+      onFailure?.();
+      return;
+    }
+    let glideRequestListener = null;
+    if (onFailure || onSuccess) {
+      glideRequestListener = GlideRequestListener.implement({
+        onSuccess: (resource, model, target, dataSource, isFirstResource) => {
+          onSuccess?.();
+        },
+        onFailure: (glideException, model, target, isFirstResource) => {
+          onFailure?.();
+        }
+      });
+    }
+    if (!useHTTPCacheControl) {
+      if (!headers.hasOwnProperty('Cache-Control')) {
+        headers['Cache-Control'] = 'no-cache';
+      }
+    }
+    const loadFromUrlParameters = new LoadFromUrlParameters(
+      AndroidConfig.activity,
+      this.nativeObject,
+      url,
+      placeholder ? placeholder.nativeObject : null,
+      glideRequestListener,
+      fade,
+      headers,
+      useHTTPCacheControl,
+      useHTTPCacheControl ? false : android.useDiskCache,
+      android.useMemoryCache
+    );
+    try {
+      SFGlide.loadFromUrl(loadFromUrlParameters);
+    } catch (error) {
+      onFailure?.();
+    }
+  }
+
+  loadFromFile(params: { file: File; fade?: boolean; width?: number; height?: number; android?: { useMemoryCache?: boolean } }): void {
+    const { file = null, placeholder = null, fade = true, width = -1, height = -1, android: { useMemoryCache: useMemoryCache } = { useMemoryCache: true } } = params;
+
+    if (file instanceof File) {
+      const parameters = new LoadFromFileParameters(AndroidConfig.activity, this.nativeObject, placeholder ? placeholder.nativeObject : null, null, fade, useMemoryCache, width, height);
+      const resolvedPath = file.resolvedPath;
+      if (!AndroidConfig.isEmulator && resolvedPath.type == Path.FILE_TYPE.DRAWABLE) {
+        const resources = AndroidConfig.activity.getResources();
+        const drawableResourceId = resources.getIdentifier(resolvedPath.name, 'drawable', AndroidConfig.packageName);
+        SFGlide.loadByResourceId(drawableResourceId, parameters);
+      } else if (!AndroidConfig.isEmulator && resolvedPath.type == Path.FILE_TYPE.ASSET) {
+        const assetPrefix = 'file:///android_asset/';
+        const assetFilePath = assetPrefix + resolvedPath.name;
+        SFGlide.loadFromAsset(assetFilePath, parameters);
+      } else {
+        SFGlide.loadFromFile(file.nativeObject, parameters);
+      }
+      this._newImageLoaded = true;
+    }
+  }
+
+  fetchFromUrl(params: {
+    url: string;
+    headers?: { [name: string]: string };
+    placeholder?: Image;
+    useHTTPCacheControl?: boolean;
+    onSuccess?: (image: Image, cache: ImageCacheType) => void;
+    onFailure?: () => void;
+    android?: { useDiskCache?: boolean; useMemoryCache?: boolean };
+    ios?: {
+      isRefreshCached?: boolean;
+    };
+  }): void {
+    const self = this;
+    const {
+      url = null,
+      headers = {},
+      placeholder = null,
+      onSuccess = null,
+      onFailure = null,
+      useHTTPCacheControl = false,
+      android: { useDiskCache, useMemoryCache } = { useMemoryCache: true, useDiskCache: true }
+    } = params;
+
+    if (!url) {
+      onFailure?.();
+      return;
+    }
+    let glideTarget = null;
+    if (onSuccess) {
+      const GlideTarget = requireClass('io.smartface.android.sfcore.ui.imageview.listeners.GlideTarget');
+      glideTarget = GlideTarget.implement({
+        onResourceReady(resource, transition) {},
+        onLoadStarted(placeholder) {
+          if (placeholder) {
+            self.nativeObject.setImageDrawable(placeholder);
+          }
+        },
+        onLoadCleared(placeholder) {}
+      });
+    }
+    if (!useHTTPCacheControl) {
+      if (!headers.hasOwnProperty('Cache-Control')) {
+        headers['Cache-Control'] = 'no-cache';
+      }
+    }
+    let glideRequestListener = null;
+    if (onFailure) {
+      const GlideRequestListener = requireClass('io.smartface.android.sfcore.ui.imageview.listeners.GlideRequestListener');
+      glideRequestListener = GlideRequestListener.implement({
+        onSuccess: function (resource, model, target, dataSource, isFirstResource) {
+          const cacheName = dataSource.toString();
+          const cacheType = this.getCacheTypeByName(cacheName);
+          const image = new Image({ drawable: resource });
+          onSuccess?.(image, cacheType);
+        },
+        onFailure: (glideException, model, target, isFirstResource) => {
+          onFailure?.();
+        }
+      });
+    }
+    const parameters = new FetchFromUrlParameters(
+      AndroidConfig.activity,
+      this.nativeObject,
+      url,
+      placeholder ? placeholder.nativeObject : null,
+      glideRequestListener,
+      true,
+      headers,
+      useHTTPCacheControl,
+      useHTTPCacheControl ? false : useDiskCache,
+      useMemoryCache,
+      glideTarget
+    );
+    try {
+      SFGlide.fetchFromUrl(parameters);
+    } catch (error) {
+      onFailure?.();
+    }
+  }
+
+  getCacheTypeByName(cacheName: string) {
+    //TODO : define enum
+    if (['LOCAL', 'DATA_DISK_CACHE', 'RESOURCE_DISK_CACHE'].includes(cacheName)) {
+      return ImageCacheType.DISK;
+    } else if (cacheName === 'MEMORY_CACHE') {
+      return ImageCacheType.MEMORY;
+    } else {
+      // TODO Recheck
+      return ImageCacheType.NETWORK;
+    }
+  }
+
+  toString(): string {
+    return 'ImageView';
+  }
+}
---
+++ ui/imageview/imageview.ios.ts
@@ -0,0 +1,316 @@
+import { IImageView, ImageViewFillType, ImageViewFillTypeIOS } from '.';
+import File from '../../io/file';
+import Color from '../color';
+import Image from '../image';
+import { ImageCacheType } from '../imagecachetype';
+import ViewIOS from '../view/view.ios';
+import { ImageViewEvents } from './imageview-events';
+
+export default class ImageViewIOS<TEvent extends string = ImageViewEvents> extends ViewIOS<TEvent | ImageViewEvents> implements IImageView {
+  private _imageTemplate: Image;
+  private _isSetTintColor: boolean;
+  constructor(params: Partial<IImageView> = {}) {
+    super();
+
+    if (!this.nativeObject) {
+      this._nativeObject = new __SF_UIImageView();
+    }
+
+    if (__SF_UIView.viewAppearanceSemanticContentAttribute() == 3) {
+      this.nativeObject.setValueForKey(3, 'semanticContentAttribute');
+    } else if (__SF_UIView.viewAppearanceSemanticContentAttribute() == 4) {
+      this.nativeObject.setValueForKey(4, 'semanticContentAttribute');
+    }
+
+    this.nativeObject.contentMode = ImageViewFillType.NORMAL;
+    this.touchEnabled = true;
+
+    for (const param in params) {
+      this[param] = params[param];
+    }
+  }
+
+  get image(): string | Image {
+    return this.nativeObject.image ? Image.createFromImage(this.nativeObject.image) : undefined;
+  }
+  set image(value: string | Image) {
+    this._imageTemplate = undefined;
+
+    if (typeof value === 'string') {
+      const image = Image.createFromFile(value);
+      if (this._isSetTintColor) {
+        // TODO Recheck after build
+        image.nativeObject = image.nativeObject.imageWithRenderingMode(2);
+        this._imageTemplate = image.nativeObject;
+      }
+      this.nativeObject.loadImage(image.nativeObject);
+    } else {
+      if (value) {
+        if (this._isSetTintColor) {
+          value.nativeObject = value.nativeObject.imageWithRenderingMode(2);
+          // TODO Recheck after build
+          this._imageTemplate = value.nativeObject;
+        }
+        this.nativeObject.loadImage(value.nativeObject);
+      } else {
+        this.nativeObject.loadImage(undefined);
+      }
+    }
+  }
+
+  get tintColor(): Color {
+    return new Color({
+      color: this.nativeObject.tintColor
+    });
+  }
+  set tintColor(value: Color) {
+    if (this.nativeObject.image) {
+      if (this._imageTemplate) {
+        this.nativeObject.image = this._imageTemplate;
+      } else {
+        this._imageTemplate = this.nativeObject.image.imageWithRenderingMode(2);
+        this.nativeObject.image = this._imageTemplate;
+      }
+    }
+    this._isSetTintColor = true;
+    this.nativeObject.tintColor = value.nativeObject;
+  }
+
+  get imageFillType(): ImageViewFillTypeIOS | ImageViewFillType {
+    return this.nativeObject.contentMode;
+  }
+  set imageFillType(value: ImageViewFillTypeIOS | ImageViewFillType) {
+    this.nativeObject.contentMode = value;
+  }
+
+  loadFromUrl(params: {
+    url: string;
+    headers?: { [name: string]: string };
+    placeholder?: Image;
+    fade?: boolean;
+    useHTTPCacheControl?: boolean;
+    onSuccess?: () => void;
+    onFailure?: () => void;
+    android?: { useDiskCache?: boolean; useMemoryCache?: boolean };
+    ios?: { isRefreshCached?: boolean };
+  }): void {
+    if (typeof params.url === 'string') {
+      // Deprecated: Use loadFromUrl(object);
+      this.nativeObject.loadFromURL(__SF_NSURL.URLWithString(params.url), params.placeholder ? params.placeholder.nativeObject : undefined, params.headers, undefined, () => {
+        if (!params.onFailure) {
+          // TODO Recheck after build
+          if (cache == ImageCacheType.NONE && params.fade !== false) {
+            var alpha = this.nativeObject.alpha;
+            this.nativeObject.alpha = 0;
+            __SF_UIView.animation(
+              0.3,
+              0,
+              () => {
+                this.nativeObject.alpha = alpha;
+              },
+              function () {}
+            );
+          }
+        }
+      });
+    } else if (typeof params.url === 'object') {
+      // Deprecated: Use useHTTPCacheControl option.
+      const options = params.ios && params.ios.isRefreshCached ? SDWebImageOptions.SDWebImageRefreshCached : params.useHTTPCacheControl ? SDWebImageOptions.SDWebImageRefreshCached : undefined;
+
+      const headers = params.headers;
+      this.nativeObject.loadFromURL(__SF_NSURL.URLWithString(params.url), params.placeholder ? params.placeholder.nativeObject : undefined, headers, options ? options : undefined, () => {
+        if (!params.onFailure) {
+          // TODO Recheck after build
+          if (params.cache == ImageCacheType.NONE && params.fade !== false) {
+            const alpha = this.nativeObject.alpha;
+            this.nativeObject.alpha = 0;
+            __SF_UIView.animation(
+              0.3,
+              0,
+              () => {
+                this.nativeObject.alpha = alpha;
+              },
+              function () {}
+            );
+          }
+          if (typeof params.onSuccess === 'function') {
+            __SF_Dispatch.mainAsync(function (innerIndex) {
+              params.onSuccess();
+            });
+          }
+        } else {
+          if (typeof params.onFailure === 'function') {
+            __SF_Dispatch.mainAsync(function (innerIndex) {
+              params.onFailure();
+            });
+          }
+        }
+      }); //onFailure COR-1817
+    }
+  }
+
+  loadFromFile(params: { file: File; fade?: boolean; width?: number; height?: number; android?: { useMemoryCache?: boolean } }): void {
+    if (params.file) {
+      const file = params.file;
+      const filePath = file.nativeObject.getActualPath();
+      const image = Image.createFromFile(filePath);
+      const fade = typeof params.fade === 'boolean' ? params.fade : true;
+
+      if (fade) {
+        this.nativeObject.loadImage(image.nativeObject);
+        const alpha = this.nativeObject.alpha;
+        this.nativeObject.alpha = 0;
+        __SF_UIView.animation(
+          0.3,
+          0,
+          () => {
+            this.nativeObject.alpha = alpha;
+          },
+          function () {}
+        );
+      } else {
+        this.nativeObject.loadImage(image.nativeObject);
+      }
+    }
+  }
+
+  fetchFromUrl(params: {
+    url: string;
+    headers?: { [name: string]: string };
+    placeholder?: Image;
+    useHTTPCacheControl?: boolean;
+    onSuccess?: (image: Image, cache: ImageCacheType) => void;
+    onFailure?: () => void;
+    android?: { useDiskCache?: boolean; useMemoryCache?: boolean };
+    ios?: { isRefreshCached?: boolean };
+  }): void {
+    let options = SDWebImageOptions.SDWebImageAvoidAutoSetImage;
+    params.ios && params.ios.isRefreshCached && (options = options | SDWebImageOptions.SDWebImageRefreshCached); // Deprecated: Use useHTTPCacheControl option.
+    params.useHTTPCacheControl && (options = options | SDWebImageOptions.SDWebImageRefreshCached);
+
+    const headers = params.headers || {};
+    this.nativeObject.loadFromURL(__SF_NSURL.URLWithString(params.url), params.placeholder ? params.placeholder.nativeObject : undefined, headers, options ? options : undefined, () => {
+      if (!params.onFailure) {
+        if (typeof params.onSuccess === 'function') {
+          __SF_Dispatch.mainAsync(function (innerIndex) {
+            // TODO Recheck after build
+            params.onSuccess(Image.createFromImage(image), cache);
+          });
+        }
+      } else {
+        if (typeof params.onFailure === 'function') {
+          __SF_Dispatch.mainAsync(function (innerIndex) {
+            params.onFailure();
+          });
+        }
+      }
+    }); //onFailure COR-1817
+  }
+}
+
+const SDWebImageOptions = {
+  /**
+   * By default, when a URL fail to be downloaded, the URL is blacklisted so the library won't keep trying.
+   * This flag disable this blacklisting.
+   */
+  SDWebImageRetryFailed: 1 << 0,
+
+  /**
+   * By default, image downloads are started during UI interactions, this flags disable this feature,
+   * leading to delayed download on UIScrollView deceleration for instance.
+   */
+  SDWebImageLowPriority: 1 << 1,
+
+  /**
+   * This flag disables on-disk caching after the download finished, only cache in memory
+   */
+  SDWebImageCacheMemoryOnly: 1 << 2,
+
+  /**
+   * This flag enables progressive download, the image is displayed progressively during download as a browser would do.
+   * By default, the image is only displayed once completely downloaded.
+   */
+  SDWebImageProgressiveDownload: 1 << 3,
+
+  /**
+   * Even if the image is cached, respect the HTTP response cache control, and refresh the image from remote location if needed.
+   * The disk caching will be handled by NSURLCache instead of SDWebImage leading to slight performance degradation.
+   * This option helps deal with images changing behind the same request URL, e.g. Facebook graph api profile pics.
+   * If a cached image is refreshed, the completion block is called once with the cached image and again with the final image.
+   *
+   * Use this flag only if you can't make your URLs static with embedded cache busting parameter.
+   */
+  SDWebImageRefreshCached: 1 << 4,
+
+  /**
+   * In iOS 4+, continue the download of the image if the app goes to background. This is achieved by asking the system for
+   * extra time in background to let the request finish. If the background task expires the operation will be cancelled.
+   */
+  SDWebImageContinueInBackground: 1 << 5,
+
+  /**
+   * Handles cookies stored in NSHTTPCookieStore by setting
+   * NSMutableURLRequest.HTTPShouldHandleCookies = YES;
+   */
+  SDWebImageHandleCookies: 1 << 6,
+
+  /**
+   * Enable to allow untrusted SSL certificates.
+   * Useful for testing purposes. Use with caution in production.
+   */
+  SDWebImageAllowInvalidSSLCertificates: 1 << 7,
+
+  /**
+   * By default, images are loaded in the order in which they were queued. This flag moves them to
+   * the front of the queue.
+   */
+  SDWebImageHighPriority: 1 << 8,
+
+  /**
+   * By default, placeholder images are loaded while the image is loading. This flag will delay the loading
+   * of the placeholder image until after the image has finished loading.
+   */
+  SDWebImageDelayPlaceholder: 1 << 9,
+
+  /**
+   * We usually don't call transformDownloadedImage delegate method on animated images,
+   * as most transformation code would mangle it.
+   * Use this flag to transform them anyway.
+   */
+  SDWebImageTransformAnimatedImage: 1 << 10,
+
+  /**
+   * By default, image is added to the imageView after download. But in some cases, we want to
+   * have the hand before setting the image (apply a filter or add it with cross-fade animation for instance)
+   * Use this flag if you want to manually set the image in the completion when success
+   */
+  SDWebImageAvoidAutoSetImage: 1 << 11,
+
+  /**
+   * By default, images are decoded respecting their original size. On iOS, this flag will scale down the
+   * images to a size compatible with the constrained memory of devices.
+   * If `SDWebImageProgressiveDownload` flag is set the scale down is deactivated.
+   */
+  SDWebImageScaleDownLargeImages: 1 << 12,
+
+  /**
+   * By default, we do not query disk data when the image is cached in memory. This mask can force to query disk data at the same time.
+   * This flag is recommend to be used with `SDWebImageQueryDiskSync` to ensure the image is loaded in the same runloop.
+   */
+  SDWebImageQueryDataWhenInMemory: 1 << 13,
+
+  /**
+   * By default, we query the memory cache synchronously, disk cache asynchronously. This mask can force to query disk cache synchronously to ensure that image is loaded in the same runloop.
+   * This flag can avoid flashing during cell reuse if you disable memory cache or in some other cases.
+   */
+  SDWebImageQueryDiskSync: 1 << 14,
+
+  /**
+   * By default, when the cache missed, the image is download from the network. This flag can prevent network to load from cache only.
+   */
+  SDWebImageFromCacheOnly: 1 << 15,
+  /**
+   * By default, when you use `SDWebImageTransition` to do some view transition after the image load finished, this transition is only applied for image download from the network. This mask can force to apply view transition for memory and disk cache as well.
+   */
+  SDWebImageForceTransition: 1 << 16
+};
