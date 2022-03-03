2785ab024c4f519956d1388b0b839d347ac561d1
Berk Baski <baskiberk41@gmail.com>
2022-03-03 18:54:55 +0300

    Update tabbarcontroller to TypeScript

---
+++ ui/tabbarcontroller/index.ts
@@ -0,0 +1,313 @@
+import TabBarItem, { ITabbarItem } from '../tabbaritem';
+import Color from '../color';
+import Page, { AbstractPage, IPage } from '../page';
+import OverScrollMode from '../android/overscrollmode';
+import { TabBarControllerEvents } from './tabbarcontroller-events';
+
+export enum BarTextTransform {
+  AUTO = 0,
+  NONE = 1,
+  UPPERCASE = 2
+}
+export enum LargeTitleDisplayMode {
+  AUTOMATIC = 0,
+  ALWAYS = 1,
+  NEVER = 2
+}
+export enum PresentationStyle {
+  COVERVERTICAL = 0,
+  FLIPHORIZONTAL = 1,
+  CROSSDISSOLVE = 2,
+  PARTIALCURL = 3
+}
+
+export declare interface ITabBarController<TEvent extends string = TabBarControllerEvents, TIOS = {}, TAND = {}> extends IPage<TEvent | TabBarControllerEvents, TIOS, TAND> {
+  /**
+   * Gets the tab bar height of the TabBarController. You can change barHeight on Android, but not iOS.
+   * This property is read-only for iOS.
+   * @property {Number} barHeight
+   * @android
+   * @ios
+   * @since 3.2.0
+   */
+  barHeight: number;
+  /**
+   * Gets/sets the tab bar item array of the TabBarController.
+   * @property {UI.TabBarItem[]} items
+   * @android
+   * @ios
+   * @since 3.2.0
+   */
+  items: TabBarItem[];
+
+  /**
+   * Gets/sets the divider color of the TabBarController.
+   * @property {UI.Color} [dividerColor = UI.Color.BLACK]
+   * @android
+   * @since 3.2.0
+   */
+  dividerColor: Color;
+  /**
+   * Gets/sets the divider padding of the TabBarController.
+   * @property {Number} [dividerPadding = 0]
+   * @android
+   * @since 3.2.0
+   */
+  dividerPadding: number;
+  /**
+   * Gets/sets the divider width of the TabBarController.
+   * @property {Number} [dividerWidth = 0]
+   * @android
+   * @since 3.2.0
+   */
+  dividerWidth: number;
+  /**
+   * Gets/sets the indicator color of the TabBarController.
+   * @property {UI.Color} [indicatorColor = UI.Color.create("#00A1F1")]
+   * @android
+   * @ios
+   * @since 3.2.0
+   */
+  indicatorColor: Color;
+  /**
+   * Gets/sets the auto capitalize title of the items of TabBarController.
+   * @property {Boolean} [autoCapitalize = true]
+   * @android
+   * @ios
+   * @since 3.2.1
+   */
+  autoCapitalize: boolean;
+  /**
+   * Gets/sets the indicator height of the TabBarController.
+   * @property {Number} indicatorHeight
+   * @android
+   * @ios
+   * @since 3.2.0
+   */
+  indicatorHeight: number;
+  /**
+   * Gets/sets over-scroll mode for top tab bar.
+   *
+   * @property {UI.Android.OverScrollMode} [overScrollMode = UI.Android.OverScrollMode.ALWAYS]
+   * @android
+   * @since 3.2.0
+   */
+  overScrollMode: OverScrollMode;
+  /**
+   * Gets/sets bar color of tabs.
+   * @property {UI.Color} [barColor = Color.WHITE]
+   * @android
+   * @ios
+   * @since 3.2.0
+   */
+  barColor: Color;
+  /**
+   * Gets/sets whether to enable scrollable tabs.
+   * @property {Boolean} scrollEnabled
+   * @android
+   * @ios
+   * @since 3.2.0
+   */
+  scrollEnabled: boolean;
+  /**
+   * Gets the selected index of TabBarController.
+   * @property {Number} selectedIndex
+   * @android
+   * @ios
+   * @since 3.2.0
+   */
+  selectedIndex: number;
+  /**
+   * Sets the selected index of TabBarController.
+   * @method setSelectedIndex
+   * @param {Number} index
+   * @param {Boolean} [animated=true]
+   * @android
+   * @ios
+   * @since 3.2.0
+   */
+  setSelectedIndex(index: number, animated: boolean): void;
+  /**
+   * Gets/sets the icon color of the tabs. You can specify text colors for the different states (normal, selected) used for the tabs.
+   * @property {UI.Color|Object} textColor
+   * @android
+   * @ios
+   * @since 3.2.0
+   */
+  iconColor:
+    | {
+        normal: Color;
+        selected: Color;
+      }
+    | Color;
+  /**
+   * Gets/sets the text color of the tabs. You can specify text colors for the different states (normal, selected) used for the tabs.
+   * @property {UI.Color|Object} textColor
+   * @android
+   * @ios
+   * @since 3.2.0
+   */
+  textColor:
+    | {
+        normal: Color;
+        selected: Color;
+      }
+    | Color;
+
+  /**
+   * Enables/Disables paging behavior.
+   *
+   * @property {Boolean} [pagingEnabled = true]
+   * @android
+   * @ios
+   * @since 4.3.2
+   */
+  pagingEnabled: boolean;
+
+  /**
+   * This event called when a tab is chosen by the user.
+   * Returns an {@link UI.Page Page} instance based on index.
+   *
+   * @event onPageCreate
+   * @param index
+   * @deprecated
+   * @return UI.Page
+   * @android
+   * @ios
+   * @since 3.2.0
+   * @example
+   * ````
+   * import TabbarController from '@smartface/native/ui/tabbarcontroller';
+   *
+   * const tabbarController = new TabbarController();
+   * tabbarController.on(TabbarController.Events.PageCreate, (params) => {
+   * 	console.info('onPageCreate', params);
+   * });
+   * ````
+   */
+  onPageCreate: (index: number) => Page;
+  /**
+   * This event called when a tab is chosen by the user.
+   *
+   * @event onSelected
+   * @deprecated
+   * @param index
+   * @android
+   * @ios
+   * @since 3.2.0
+   * @example
+   * ````
+   * import TabbarController from '@smartface/native/ui/tabbarcontroller';
+   *
+   * const tabbarController = new TabbarController();
+   * tabbarController.on(TabbarController.Events.Selected, (params) => {
+   * 	console.info('onSelected', params);
+   * });
+   * ````
+   */
+  onSelected: (index: number) => void;
+}
+
+export declare class AbstractTabBarController<TEvent extends string = TabBarControllerEvents> extends AbstractPage<TEvent> implements ITabBarController<TEvent> {
+  static iOS: {
+    BarTextTransform: typeof BarTextTransform;
+    LargeTitleDisplayMode: typeof LargeTitleDisplayMode;
+    PresentationStyle: typeof PresentationStyle;
+  };
+
+  barHeight: number;
+  items: ITabbarItem[];
+  dividerColor: Color;
+  dividerPadding: number;
+  dividerWidth: number;
+  indicatorColor: Color;
+  autoCapitalize: boolean;
+  indicatorHeight: number;
+  overScrollMode: OverScrollMode;
+  barColor: Color;
+  scrollEnabled: boolean;
+  selectedIndex: number;
+  setSelectedIndex(index: number, animated: boolean): void;
+  iconColor: { normal: Color; selected: Color } | Color;
+  textColor: { normal: Color; selected: Color } | Color;
+  pagingEnabled: boolean;
+  onPageCreate: (index: number) => Page;
+  onSelected: (index: number) => void;
+}
+
+/**
+ * @class UI.TabBarController
+ * @extends UI.Page
+ * @since 3.2.0
+ *
+ * This class extends from {@link UI.Page Page}. But you shouldn't use directly layout of the {@link UI.TabBarController TabBarController}.
+ *
+ *     @example
+ *     import extend = from "js-base/core/extend";
+ *     import TabBarController = from '@smartface/native/ui/tabbarcontroller';
+ *     import Color = from '@smartface/native/ui/color';
+ *     import TabBarItem = from '@smartface/native/ui/tabbaritem';
+ *     import Page = from '@smartface/native/ui/page';
+ *
+ *     const backgroundColors = [Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN, Color.MAGENTA];
+ *     const SamplePage = extend(Page)(
+ *         (_super, params) => {
+ *             _super(this, params);
+ *         }
+ *     );
+ *
+ *     const TabBarController1 = extend(TabBarController)(
+ *         (_super, params) => {
+ *             _super(this, {
+ *                 items: createTabBarItems(5)
+ *             });
+ *
+ *             const pages = [];
+ *             this.onPageCreate = (index: number) => {
+ *                 if (!pages[index]) {
+ *                     pages[index] = new SamplePage({ index: index });
+ *                     pages[index].layout.backgroundColor = backgroundColors[index];
+ *                 }
+ *                 return pages[index];
+ *             };
+ *
+ *             this.onShow = () => {
+ *                 this.headerBar.visible = false;
+ *             };
+ *
+ *             this.onLoad = () => {
+ *                 this.scrollEnabled = true;
+ *                 this.indicatorColor = Color.BLACK;
+ *                 this.indicatorHeight = 5;
+ *                 this.barColor = Color.LIGHTGRAY;
+ *                 this.iconColor = {
+ *                     normal: Color.BLACK,
+ *                     selected: Color.BLUE
+ *                 };
+ *                 this.textColor = {
+ *                     normal: Color.BLACK,
+ *                     selected: Color.BLUE
+ *                 };
+ *             };
+ *
+ *             this.onSelected = (index: number) => {
+ *                 console.log("Selected item index: " + index);
+ *             };
+ *         }
+ *     );
+ *
+ *     function createTabBarItems(itemCount) {
+ *         const items = [];
+ *         for (let i = 0; i < itemCount; i++) {
+ *             items.push(new TabBarItem({
+ *                 title: "Category " + i
+ *             }));
+ *         }
+ *         return items;
+ *     }
+ *
+ */
+const TabBarController: typeof AbstractTabBarController = require(`./tabbarcontroller.${Device.deviceOS.toLowerCase()}`).default;
+type TabBarController = AbstractTabBarController;
+
+export default TabBarController;
---
+++ ui/tabbarcontroller/tabbarcontroller.android.ts
@@ -0,0 +1,326 @@
+import OverScrollMode from 'ui/android/overscrollmode';
+import { ITabBarController } from '.';
+import Application from '../../application';
+import { AndroidConfig, UnitConverter } from '../../util';
+import Color, { AbstractColor } from '../color';
+import Page from '../page';
+import PageAndroid from '../page/page.android';
+import SwipeView from '../swipeview';
+import { ITabbarItem } from '../tabbaritem';
+import { TabBarControllerEvents } from './tabbarcontroller-events';
+
+/* globals requireClass */
+const NativeTabLayout = requireClass('com.google.android.material.tabs.TabLayout');
+const NativeRelativeLayout = requireClass('android.widget.RelativeLayout');
+const NativeYogaNodeFactory = requireClass('com.facebook.yoga.YogaNodeFactory');
+const NativeGradientDrawable = requireClass('android.graphics.drawable.GradientDrawable');
+const PorterDuff = requireClass('android.graphics.PorterDuff');
+
+const ModeSRC_IN = PorterDuff.Mode.SRC_IN;
+
+export default class TabBarControllerAndroid<TEvent extends string = TabBarControllerEvents> extends PageAndroid<TEvent | TabBarControllerEvents> implements ITabBarController {
+  private _onSelectedCallback: (index: number) => void;
+  private _onPageCreateCallback: (index: number) => Page;
+  private _items: ITabbarItem[];
+  private _barColor: Color;
+  private _indicatorColor: Color;
+  private _textColor: Color | { normal: Color; selected: Color };
+  private _iconColor: Color | { normal: Color; selected: Color };
+  private _overScrollMode: OverScrollMode;
+  private _scrollEnabled: boolean = false;
+  private _dividerWidth: number = 0;
+  private _dividerPadding: number = 0;
+  private _dividerColor: Color;
+  private _indicatorHeight: number;
+  private _autoCapitalize: boolean = true;
+  private tabLayout: any;
+  private divider: any;
+  private swipeView: SwipeView;
+  private dividerDrawable: typeof NativeGradientDrawable;
+
+  constructor(params: Partial<ITabBarController> = {}) {
+    super(params);
+
+    this._items = [];
+    this._overScrollMode = OverScrollMode.ALWAYS;
+    this._dividerColor = Color.BLACK;
+    this._indicatorColor = Color.create('#00A1F1');
+    this._barColor = Color.WHITE;
+    this._textColor = Color.BLACK;
+
+    this.tabLayout.nativeObject = new NativeTabLayout(AndroidConfig.activity);
+    this.tabLayout.yogaNode = NativeYogaNodeFactory.create();
+    this.tabLayout.nativeObject.setLayoutParams(new NativeRelativeLayout.LayoutParams(-1, -2));
+    this.divider = this.tabLayout.nativeObject.getChildAt(0);
+    //Todo: When Google fixed its issue (https://issuetracker.google.com/issues/36973591), remove this line.
+    this.tabLayout.nativeObject.setLayoutDirection(Application.LayoutDirection.LEFTTORIGHT);
+
+    if (!this.swipeView) {
+      this.swipeView = new SwipeView({
+        page: this,
+        flexGrow: 1,
+        onPageCreate: (position: number) => {
+          this.emit('pageCreate', position);
+          if (!this.onPageCreate) {
+            return null;
+          }
+          return this.onPageCreate(position);
+        },
+        // TODO: Remove params.items check later version
+        pageCount: params && params.items ? params.items.length : this._items.length
+      });
+    }
+
+    this.tabLayout.nativeObject.setupWithViewPager(this.swipeView.nativeObject);
+    this.layout.addChild(this.tabLayout);
+    this.layout.addChild(this.swipeView);
+
+    const listener = NativeTabLayout.OnTabSelectedListener.implement({
+      onTabSelected: (tab) => {
+        this.onSelected && this.onSelected(tab.getPosition());
+        this.emit('selected', tab.getPosition());
+        if (!this.iconColor) return;
+
+        const selectedColor = this.iconColor instanceof Color ? this.iconColor : this.iconColor.selected;
+        const tabIcon = tab.getIcon();
+        tabIcon && tabIcon.setColorFilter(selectedColor.nativeObject, ModeSRC_IN);
+      },
+      onTabUnselected: (tab) => {
+        if (!this.iconColor) return;
+
+        const normalColor = this.iconColor instanceof Color ? this.iconColor : this.iconColor.normal;
+        const tabIcon = tab.getIcon();
+        tabIcon && tabIcon.setColorFilter(normalColor.nativeObject, ModeSRC_IN);
+      },
+      onTabReselected: (tab) => {}
+    });
+    this.tabLayout.nativeObject.addOnTabSelectedListener(listener);
+
+    const { android, ...restParams } = params;
+    Object.assign(this._android, android, this.androidProps);
+    Object.assign(this, restParams);
+  }
+
+  private get androidProps() {
+    const self = this;
+    return {
+      get dividerWidth(): number {
+        return self._dividerWidth;
+      },
+      set dividerWidth(value: number) {
+        self._dividerWidth = value;
+
+        self.divider.setShowDividers(2); // 2 = LinearLayout.SHOW_DIVIDER_MIDDLE
+        self.dividerDrawable = new NativeGradientDrawable();
+        self.dividerDrawable.setColor(self._dividerColor.nativeObject);
+
+        let px = UnitConverter.dpToPixel(value);
+        self.dividerDrawable.setSize(px, 1);
+        px = UnitConverter.dpToPixel(self._dividerPadding);
+        self.divider.setDividerPadding(px);
+        self.divider.setDividerDrawable(self.dividerDrawable);
+      },
+      get dividerColor(): Color {
+        return self._dividerColor;
+      },
+      set dividerColor(value: Color) {
+        self._dividerColor = value;
+        if (self.dividerDrawable) {
+          self.dividerDrawable.setColor(value.nativeObject);
+        }
+      },
+      get dividerPadding(): number {
+        return self._dividerPadding;
+      },
+      set dividerPadding(value: number) {
+        self._dividerPadding = value;
+        if (self.dividerDrawable) {
+          const px = UnitConverter.dpToPixel(self._dividerPadding);
+          self.divider.setDividerPadding(px);
+        }
+      },
+      get overScrollMode(): OverScrollMode {
+        return self._overScrollMode;
+      },
+      set overScrollMode(value: OverScrollMode) {
+        self._overScrollMode = value;
+        self.swipeView.android.overScrollMode = value;
+        self.tabLayout.nativeObject.setOverScrollvalue(value);
+      }
+    };
+  }
+
+  // TODO Unused fields
+  dividerColor: Color;
+  dividerPadding: number;
+  dividerWidth: number;
+
+  get barHeight(): number {
+    return UnitConverter.pixelToDp(this.tabLayout.nativeObject.getHeight());
+  }
+  set barHeight(value: number) {
+    this.tabLayout.yogaNode.setHeight(UnitConverter.dpToPixel(value));
+  }
+
+  get items(): ITabbarItem[] {
+    return this._items;
+  }
+  set items(value: ITabbarItem[]) {
+    // TODO: We have updated UI.TabBarItem in Router v2.
+    // After it will merge, title and icon must be updated dynamically.
+    this._items = value;
+
+    // TODO: Maybe later, swipeView pageCount can be set dynamically.
+    // After that, use refreshData method like listview.
+    this.swipeView.pageCount = this._items.length;
+    this.swipeView.pagerAdapter.notifyDataSetChanged();
+
+    for (let i = 0; i < this._items.length; i++) {
+      const item = this._items[i];
+      const itemTitle = item._attributedTitleBuilder ? item._attributedTitleBuilder : item.title;
+
+      item.tabBarItemParent = this;
+      item.nativeObject = this.tabLayout.nativeObject.getTabAt(i);
+      item.setProperties({
+        itemTitle,
+        itemIcon: item.icon,
+        systemIcon: item.android.systemIcon
+      });
+    }
+    if (!this.autoCapitalize) this.setAllCaps(this._items, this.tabLayout.nativeObject, this.autoCapitalize);
+  }
+
+  get indicatorColor(): Color {
+    return this._indicatorColor;
+  }
+  set indicatorColor(value: Color) {
+    this._indicatorColor = value;
+    this.tabLayout.nativeObject.setSelectedTabIndicatorColor(value.nativeObject);
+  }
+
+  get autoCapitalize(): boolean {
+    return this._autoCapitalize;
+  }
+  set autoCapitalize(value: boolean) {
+    this._autoCapitalize = value;
+    if (this.items && this.items.length > 0) {
+      // TODO: If you set title or icon later, native tabLayout capitalizes title of tab item.
+      // Call this function after setting title.
+      this.setAllCaps(this.items, this.tabLayout.nativeObject, value);
+    }
+  }
+
+  get indicatorHeight(): number {
+    return this._indicatorHeight;
+  }
+  set indicatorHeight(value: number) {
+    this._indicatorHeight = value;
+    const px = UnitConverter.dpToPixel(value);
+    this.tabLayout.nativeObject.setSelectedTabIndicatorHeight(px);
+  }
+
+  overScrollMode: OverScrollMode;
+
+  get barColor(): Color {
+    return this._barColor;
+  }
+  set barColor(value: Color) {
+    this._barColor = value;
+    this.tabLayout.nativeObject.setBackgroundColor(value.nativeObject);
+  }
+
+  get scrollEnabled(): boolean {
+    return this._scrollEnabled;
+  }
+  set scrollEnabled(value: boolean) {
+    this._scrollEnabled = value;
+    if (value) {
+      this.tabLayout.nativeObject.setTabMode(0); // 0 = TabLayout.MODE_SCROLLABLE
+      this.tabLayout.nativeObject.setLayoutParams(new NativeRelativeLayout.LayoutParams(-2, -2));
+    } else {
+      this.tabLayout.nativeObject.setTabMode(1); // 1 = TabLayout.MODE_FIXED
+      this.tabLayout.nativeObject.setLayoutParams(new NativeRelativeLayout.LayoutParams(-1, -2));
+    }
+  }
+
+  get selectedIndex(): number {
+    return this.swipeView.currentIndex;
+  }
+
+  setSelectedIndex(index: number, animated: boolean): void {
+    this.swipeView.swipeToIndex(index, animated);
+  }
+
+  get iconColor(): Color | { normal: Color; selected: Color } {
+    return this._iconColor;
+  }
+  set iconColor(value: Color | { normal: Color; selected: Color }) {
+    this._iconColor = value;
+    var normalColor = value instanceof Color ? value : value.normal;
+    var selectedColor = value instanceof Color ? value : value.selected;
+    for (let i = 0; i < this._items.length; i++) {
+      const tabIcon = this.tabLayout.nativeObject.getTabAt(i).getIcon();
+      if (i === this.selectedIndex) {
+        tabIcon && tabIcon.setColorFilter(selectedColor.nativeObject, ModeSRC_IN);
+      } else {
+        tabIcon && tabIcon.setColorFilter(normalColor.nativeObject, ModeSRC_IN);
+      }
+    }
+  }
+
+  get textColor(): Color | { normal: Color; selected: Color } {
+    return this._textColor;
+  }
+  set textColor(value: Color | { normal: Color; selected: Color }) {
+    this._textColor = value;
+    const normalColor = value instanceof Color ? value : value.normal;
+    const selectedColor = value instanceof Color ? value : value.selected;
+    this.tabLayout.nativeObject.setTabTextColors(normalColor.nativeObject, selectedColor.nativeObject);
+  }
+
+  get pagingEnabled(): boolean {
+    // TODO Boolean - boolean issue
+    return this.swipeView.pagingEnabled;
+  }
+  set pagingEnabled(value: boolean) {
+    this.swipeView.pagingEnabled = value;
+  }
+
+  get onPageCreate(): (index: number) => Page {
+    return this._onPageCreateCallback;
+  }
+  set onPageCreate(value: (index: number) => Page) {
+    this._onPageCreateCallback = value;
+  }
+
+  get onSelected(): (index: number) => void {
+    return this._onSelectedCallback;
+  }
+  set onSelected(value: (index: number) => void) {
+    this._onSelectedCallback = value;
+  }
+
+  setAllCaps(itemArray: any[], nativeTabLayout: any, autoCapitalize: boolean) {
+    const NativeTextView = requireClass('android.widget.TextView');
+    const viewGroupOfTabLayout = nativeTabLayout.getChildAt(0);
+    const tabsCount = viewGroupOfTabLayout.getChildCount();
+    for (let i = 0; i < tabsCount; i++) {
+      const viewGroupOfTab = viewGroupOfTabLayout.getChildAt(i);
+      const tabChildsCount = viewGroupOfTab.getChildCount();
+      for (let j = 0; j < tabChildsCount; j++) {
+        const tabViewChild = viewGroupOfTab.getChildAt(j);
+        const isAssignableFrom = NativeTextView.isAssignableFrom(tabViewChild.getClass());
+        if (isAssignableFrom) {
+          tabViewChild.setAllCaps(autoCapitalize);
+          itemArray[i].nativeTextView = tabViewChild;
+        }
+      }
+    }
+  }
+
+  createDividerDrawableIfNeeded() {
+    if (!this.dividerDrawable) {
+      this.dividerDrawable = new NativeGradientDrawable();
+    }
+  }
+}
