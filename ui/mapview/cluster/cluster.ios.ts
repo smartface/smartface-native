60c200945829b0b458cd90f6a0748e01498edb8d
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-03-08 13:55:47 +0300

    convert cluster to typescript

---
+++ ui/mapview/cluster/cluster.ios.ts
@@ -0,0 +1,38 @@
+import { ICluster } from '.';
+import MapView from '..';
+import NativeComponent from '../../../core/native-component';
+
+export default class ClusterIOS extends NativeComponent<__SF_Cluster> implements ICluster {
+  constructor(params: Partial<ICluster> = {}, nativeObject?: __SF_Cluster) {
+    super(params);
+    this.nativeObject = nativeObject || __SF_Cluster.createCluster();
+  }
+  get title(): string {
+    return this.nativeObject.title;
+  }
+  set title(value: string) {
+    this.nativeObject.title = value;
+  }
+  get subtitle(): string {
+    return this.nativeObject.subtitle;
+  }
+  set subtitle(value: string) {
+    this.nativeObject.subtitle = value;
+  }
+  get canShowCallout(): boolean {
+    return this.nativeObject.canShowCallout;
+  }
+  set canShowCallout(value: boolean) {
+    this.nativeObject.canShowCallout = value;
+  }
+  get count(): number {
+    return this.nativeObject.count;
+  }
+  set count(value: number) {
+    this.nativeObject.count = value;
+  }
+
+  setDefaultClusterRenderer(mapView: MapView, nativeGoogleMap: any, nativeClusterManager: any) {
+    return; //Android Only
+  }
+}
