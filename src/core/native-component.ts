0bd561ccb9cde36750fb6fa4a77866d2d2aa663e
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2022-03-10 16:33:16 +0300

    fix: NativeComponent

--- src/core/native-component.ts
+++ src/core/native-component.ts
@@ -4,15 +4,15 @@ type NativeInstance = Partial<Record<any, any>>;
 export default abstract class NativeComponent<TNative extends NativeInstance = NativeInstance, TProps extends Record<string, any> = Record<string, any>> {
   protected _nativeObject: any;
   constructor(params?: TProps) {
-    const { android = {}, ios = {}, ...rest } = params;
+    const { android = {}, ios = {}, ...rest } = params || {ios: {}, android: {}};
     rest && Object.assign(this, rest);
   }
 
-  get nativeObject(): any {
+  get nativeObject(): TNative {
     return this._nativeObject;
   }
 
-  set nativeObject(value: any) {
+  set nativeObject(value: TNative) {
     this._nativeObject = value;
   }
 }
