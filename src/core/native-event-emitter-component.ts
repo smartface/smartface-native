2937c6d568b8d47a86b784e8d5a2d0fb5d58220e
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-03-10 13:21:03 +0300

    remove emitterclass and format

--- src/core/native-event-emitter-component.ts
+++ src/core/native-event-emitter-component.ts
@@ -1,7 +1,7 @@
 import { EventEmitter, EventListenerCallback, IEventEmitter } from './eventemitter';
 import { NativeMobileComponent, WithMobileOSProps } from './native-mobile-component';
 
-export default abstract class NativeEventEmitterComponent<TEvent extends string, TNative extends Record<string, any> =  Record<string, any>, TProps extends WithMobileOSProps = WithMobileOSProps>
+export default abstract class NativeEventEmitterComponent<TEvent extends string, TNative extends Record<string, any> = Record<string, any>, TProps extends WithMobileOSProps = WithMobileOSProps>
   extends NativeMobileComponent<TNative, WithMobileOSProps<TProps>>
   implements IEventEmitter<TEvent>
 {
