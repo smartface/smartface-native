7f30a59ce62d070eebd27acebeecfdfa1f10891c
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2022-03-10 11:49:12 +0300

    refactor: Dialog

--- src/ui/dialog/index.ts
+++ src/ui/dialog/index.ts
@@ -1,6 +1,14 @@
-import { AbstractDialog } from './dialog';
+import FlexLayout from '../flexlayout';
+import { AbstractDialog, DialogStyle } from './dialog';
+declare class DialogImpl extends AbstractDialog {
+    setShowListener(): void;
+    get layout(): FlexLayout;
+    hide(): void;
+    show(): void;
+    get android(): Partial<{ themeStyle: DialogStyle; isTransparent: boolean; cancelable: boolean; }>;
+}
 
-const Dialog: typeof AbstractDialog = require(`./dialog.${Device.deviceOS.toLowerCase()}`).default;
+const Dialog: typeof DialogImpl = require(`./dialog.${Device.deviceOS.toLowerCase()}`).default;
 
-type Dialog = AbstractDialog;
+type Dialog = DialogImpl;
 export default Dialog;
