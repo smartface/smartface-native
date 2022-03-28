a4723c4376ee2f1502017a4b1c159f2adbeefe12
Cihat Yeşildağ <cico643@hotmail.com.tr>
2022-03-28 21:44:47 +0300

    fix(data): static methods

--- src/global/data/data.android.ts
+++ src/global/data/data.android.ts
@@ -11,66 +11,66 @@ class DataAndroid implements BaseData {
     }
     return null;
   }
-  getBooleanVariable(key: string) {
+  static getBooleanVariable(key: string) {
     if (TypeUtil.isString(key) && DataAndroid.jsSharedPreferences.contains(key)) {
       return DataAndroid.jsSharedPreferences.getBoolean(key, false);
     }
     return null;
   }
-  getIntVariable(key: string) {
+  static getIntVariable(key: string) {
     if (TypeUtil.isString(key) && DataAndroid.jsSharedPreferences.contains(key)) {
       return DataAndroid.jsSharedPreferences.getInt(key, -1);
     }
     return null;
   }
-  getFloatVariable(key: string) {
+  static getFloatVariable(key: string) {
     if (TypeUtil.isString(key) && DataAndroid.jsSharedPreferences.contains(key)) {
       return DataAndroid.jsSharedPreferences.getFloat(key, -1);
     }
     return null;
   }
-  getLongVariable(key: string) {
+  static getLongVariable(key: string) {
     if (TypeUtil.isString(key) && DataAndroid.jsSharedPreferences.contains(key)) {
       return DataAndroid.jsSharedPreferences.getLong(key, -1);
     }
     return null;
   }
-  setStringVariable(key: string, value: string) {
+  static setStringVariable(key: string, value: string) {
     if (TypeUtil.isString(key) && TypeUtil.isString(value)) {
       DataAndroid.jsSharedPreferences.edit().putString(key, value).commit();
     }
   }
-  setBooleanVariable(key: string, value: boolean) {
+  static setBooleanVariable(key: string, value: boolean) {
     if (TypeUtil.isString(key) && TypeUtil.isBoolean(value)) {
       DataAndroid.jsSharedPreferences.edit().putBoolean(key, value).commit();
     }
   }
-  setIntVariable(key: string, value: number) {
+  static setIntVariable(key: string, value: number) {
     if (TypeUtil.isString(key) && TypeUtil.isNumeric(value)) {
       DataAndroid.jsSharedPreferences.edit().putInt(key, value).commit();
     }
   }
-  setFloatVariable(key: string, value: number) {
+  static setFloatVariable(key: string, value: number) {
     if (TypeUtil.isString(key) && TypeUtil.isNumeric(value)) {
       DataAndroid.jsSharedPreferences.edit().putFloat(key, value).commit();
     }
   }
-  setLongVariable(key: string, value: number) {
+  static setLongVariable(key: string, value: number) {
     if (TypeUtil.isString(key) && TypeUtil.isNumeric(value)) {
       DataAndroid.jsSharedPreferences.edit().putLong(key, long(value)).commit();
     }
   }
-  containsVariable(key: string) {
+  static containsVariable(key: string) {
     if (TypeUtil.isString(key)) {
       return DataAndroid.jsSharedPreferences.contains(key);
     }
   }
-  removeVariable(key: string) {
+  static removeVariable(key: string) {
     if (TypeUtil.isString(key)) {
       DataAndroid.jsSharedPreferences.edit().remove(key).commit();
     }
   }
-  removeAllVariables() {
+  static removeAllVariables() {
     DataAndroid.jsSharedPreferences.edit().clear().commit();
   }
 }
