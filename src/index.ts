a9dbe8842da996efd661f1fb9f69c3bc018eed33
Halit Aksoy <jhalitaksoy@gmail.com>
2022-05-30 16:03:13 +0300

    [NTVE-838] assign AbortController and AbortSignal to global

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import XHR from './net/xhr';
 import FormData from './net/formdata';
 import AlertOverride from './core/alert';
 import * as TimerOverride from './core/timers';
+import { AbortController, AbortSignal } from 'abort-controller';
 
 global.XMLHttpRequest = XHR as any;
 global.FormData = FormData;
+global.AbortController = AbortController as any;
+global.AbortSignal = AbortSignal as any;
 global.process = global.process || {
   env: {
     NODE_ENV: 'production'
