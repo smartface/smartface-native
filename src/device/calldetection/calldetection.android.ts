db4adb736c9cb3876cea4af400f75c82be5fe0c4
Halit Aksoy <jhalitaksoy@gmail.com>
2022-06-09 10:52:53 +0300

    [NTVE-873] use Broadcast Receiver for call detection

--- src/device/calldetection/calldetection.android.ts
+++ src/device/calldetection/calldetection.android.ts
@@ -3,27 +3,39 @@ import { ICallDetection, State } from './calldetection';
 import { CallDetectionEvents } from './calldetection-events';
 import AndroidConfig from '../../util/Android/androidconfig';
 
-const SFPhoneStateListener = requireClass('io.smartface.android.sfcore.device.calldetection.SFPhoneStateListener');
-const TELEPHONY_SERVICE = 'phone';
-const TELEPHONY_MANAGER = 'android.telephony.TelephonyManager';
+const SFPhoneStateBroadcastReceiver = requireClass('io.smartface.android.sfcore.device.calldetection.SFPhoneStateBroadcastReceiver');
+const NativeTelephonyManager = requireClass('android.telephony.TelephonyManager');
+const NativeIntentFilter = requireClass('android.content.IntentFilter');
 
 class CallDetectionAndroid extends NativeEventEmitterComponent<CallDetectionEvents> implements ICallDetection {
+  private phoneStateBroadcastReceiver: any;
   protected createNativeObject() {
     return null;
   }
   onCallStateChanged: (params: { state: State; incomingNumber?: string; observer?: any }) => void;
   constructor() {
     super();
-    const phoneListener = new SFPhoneStateListener((state, incomingNumber) => {
+    this.phoneStateBroadcastReceiver = this.createPhoneStateBroadcastReceiver();
+    this.registerPhoneStateBroadcastReceiver(this.phoneStateBroadcastReceiver);
+  }
+
+  private createPhoneStateBroadcastReceiver() {
+    return new SFPhoneStateBroadcastReceiver((state, incomingNumber) => {
       this.onCallStateChanged?.({
         state: state,
         incomingNumber: incomingNumber
       });
       this.emit(CallDetectionEvents.CallStateChanged, state, incomingNumber);
     });
+  }
 
-    const telephonyManager = AndroidConfig.getSystemService(TELEPHONY_SERVICE, TELEPHONY_MANAGER);
-    telephonyManager.listen(phoneListener, SFPhoneStateListener.LISTEN_CALL_STATE);
+  private registerPhoneStateBroadcastReceiver(broadcastReceiver: any) {
+    const intentFilter = new NativeIntentFilter(NativeTelephonyManager.ACTION_PHONE_STATE_CHANGED);
+    AndroidConfig.activity.registerReceiver(broadcastReceiver, intentFilter);
+  }
+
+  private dispose() {
+    AndroidConfig.activity.unregisterReceiver(this.phoneStateBroadcastReceiver);
   }
 }
 
