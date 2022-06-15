f832dd1ca9279cac2de67cbb8b9a4dfd9c90e27b
Halit Aksoy <jhalitaksoy@gmail.com>
2022-06-16 00:05:15 +0300

    [NTVE-877] fix requireClass usage

--- src/device/calldetection/calldetection.android.ts
+++ src/device/calldetection/calldetection.android.ts
@@ -3,10 +3,8 @@ import { ICallDetection, State } from './calldetection';
 import { CallDetectionEvents } from './calldetection-events';
 import AndroidConfig from '../../util/Android/androidconfig';
 
-const SFPhoneStateListener = requireClass('io.smartface.android.sfcore.device.calldetection.SFPhoneStateListener');
 const TELEPHONY_SERVICE = 'phone';
 const TELEPHONY_MANAGER = 'android.telephony.TelephonyManager';
-const SFCallStateListener = requireClass('io.smartface.android.sfcore.device.calldetection.SFCallStateListener');
 const NativeBuild = requireClass('android.os.Build');
 const NativeContextCompat = requireClass('androidx.core.content.ContextCompat');
 const NativeManifest = requireClass('android.Manifest');
@@ -28,10 +26,12 @@ class CallDetectionAndroid extends NativeEventEmitterComponent<CallDetectionEven
     const telephonyManager = AndroidConfig.getSystemService(TELEPHONY_SERVICE, TELEPHONY_MANAGER);
     if (NativeBuild.VERSION.SDK_INT >= NativeBuild.VERSION_CODES.S) {
       if (this.hasReadPhoneStatePermission()) {
+        const SFCallStateListener = requireClass('io.smartface.android.sfcore.device.calldetection.SFCallStateListener');
         const callStateListener = new SFCallStateListener(callStateChangedCallback);
         telephonyManager.registerTelephonyCallback(NativeContextCompat.getMainExecutor(AndroidConfig.activity), callStateListener);
       }
     } else {
+      const SFPhoneStateListener = requireClass('io.smartface.android.sfcore.device.calldetection.SFPhoneStateListener');
       const phoneListener = new SFPhoneStateListener(callStateChangedCallback);
       telephonyManager.listen(phoneListener, SFPhoneStateListener.LISTEN_CALL_STATE);
     }
