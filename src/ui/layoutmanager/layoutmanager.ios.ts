b8306a69911da1916d28a1cdc5812afe30ee7030
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-07-28 11:31:01 +0300

    add Gridview.layoutmanager default scroll direction

--- src/ui/layoutmanager/layoutmanager.ios.ts
+++ src/ui/layoutmanager/layoutmanager.ios.ts
@@ -37,7 +37,7 @@ export default class LayoutManagerIOS extends AbstractLayoutManager<__SF_UIColle
     this._sectionInset = { bottom: 0, left: 0, right: 0, top: 0 };
     this._contentInset = { bottom: 0, left: 0, right: 0, top: 0 };
     this._onItemLength = () => DEFAULT_ITEM_LENGTH;
-    this._scrollDirection = ScrollDirection.VERTICAL;
+    this.scrollDirection = ScrollDirection.VERTICAL;
     this.collectionView = null;
     super.preConstruct(params);
   }
